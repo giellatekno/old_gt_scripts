84dccf83721b721fd4bdace4db2e87b777770329
Trond Trosterud <trond.trosterud@uit.no>
2002-08-14 09:51:58 +0000

    Added comments to sm of the lines

--- pdfto7bit.sh
+++ pdfto7bit.sh
@@ -5,6 +5,9 @@
 # Usage: go to the gt/script directory. Enter ./pdfto7bit.sh at the commandline
 # This will convert all the pdf files in the CORP directory to the format that 
 # the sme database expects
+# The translated files will be stored in the subdirectory modified, and should 
+# be inspedted and manually moved from there to the parent directory corp.
+
 
 FROMCORP=../sme/corp/original
 TOCORP=../sme/corp/modified
@@ -12,10 +15,10 @@ TOCORP=../sme/corp/modified
 for i in $FROMCORP/sams*.pdf
 do
 	echo "Konverting $i..."
-	pdftotext -raw -enc UTF-8 $i - \
-	| tr -s ' ' \
-	| ./del.pl \
-	| ./utf8.pl \
+	pdftotext -raw -enc UTF-8 $i - \   # invoces the utility pdftotext
+	| tr -s ' ' \       # conflates repeted spaces
+	| ./del.pl \        # invoces del.pl, deleting sm hyphens(?)   
+	| ./utf8.pl \       # invoces utf8.pl, translating sami letters
 	> $TOCORP/`basename $i .pdf`.txt
 	echo "done"
 done
