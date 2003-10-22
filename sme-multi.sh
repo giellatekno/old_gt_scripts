7ba8327b50aa6492dd5627f7dd3d56683a9d6242
Trond Trosterud <trond.trosterud@uit.no>
2003-10-22 11:21:27 +0000

    Added scripts for disamb of sme

---
+++ sme-multi.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# sme-snt.sh
+# This is a shell script for analysing Northern Sámi sentences.
+# It gives the analysis, and the number of the rules used to disambiguate
+
+while [ 1 ]                                 # as long as there is input
+do                                          # run the following loop
+echo -n "Atte cealkaga: "                   # (message to user)
+read sentence                               # next 3 lines is the usual command
+echo $sentence | ~/bin/tokenize ~/gt/sme/bin/tok.fst | \
+lookup -flags mbTT ~/gt/sme/bin/sme.fst | ~/gt/script/lookup2cg
+#vislcg --grammar ~/gt/sme/src/sme-dis.rle --minimal
+
+done                      
+exit 0
