fc9382af5d3d43314aa400ca8428ab56fb473f4f
Tomi Pieski <>
2005-05-02 13:22:10 +0000

    Added conversion regex's to the script.

--- corp-utf8-fix.pl
+++ corp-utf8-fix.pl
@@ -19,10 +19,50 @@ find ( sub {
   my $args;
 
   if ($string =~ /[èÈ¼©]/){
-    $args = ("corp-utf8-2.txt");
+     while(<>) {
+      	s/¹/đ/g
+		s/©/Đ/g
+		s/è/č/g
+		s/È/Č/g
+		s/¼/ž/g
+		s/¿/ŋ/g
+		s/º/š/g
+     }
+#    $args = ("corp-utf8-2.txt");
   } else {
-    $args = ("corp-utf8.txt");
+     while (<>) {
+    		s/„/č/g
+		s/˜/đ/g
+		s/¿/ž/g
+		s/ð/đ/g
+		s/‚/Č/g
+		s/¹/ŋ/g
+		s/ð/đ/g
+		s/‰/Đ/g
+		
+		s/ð/đ/g
+		s/ç/č/g
+		s/Ç/Č/g
+		s/Ó/Š/g
+		s/ó/š/g
+		s/þ/ž/g
+		s/ñ/ŋ/g
+		s/Ñ/Ŋ/g
+		s/ý/ŧ/g
+		
+		s/²/Š/g
+		s/³/š/g
+		s/¢/č/g
+		s/¡/Č/g
+		s/±/ŋ/g
+		s/ð/đ/g
+		s/ç/č/g
+		s/º/ž/g
+		s/¤/đ/g
+     }
+#    $args = ("corp-utf8.txt");
   }
 
-  system ("sed -f $args $File::Find::name");
+#  system ("sed -f $args $File::Find::name");
 }, '.');
+
