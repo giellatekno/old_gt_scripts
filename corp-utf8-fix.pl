525ce3cd73bd87ebfdbfeb7c5119373a30d81bfb
Tomi Pieski <>
2005-05-02 12:04:27 +0000

    Script to fix UTF-8 problems.

---
+++ corp-utf8-fix.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl -w
+
+use strict;
+use encoding 'utf-8';
+use open ':utf8';
+use File::Find;
+
+undef $/;
+
+find ( sub {
+  return if(__FILE__ =~ $_);
+  return if("digr-utf8.txt" =~ $_);
+  return if("digr-utf8-2.txt" =~ $_);
+  open (FILE, "< $File::Find::name") or return;
+  my $string = <FILE>;
+  close (FILE);
+
+  my $args;
+
+  if ($string =~ /[èÈ¼©]/){
+    $args = ("corp-utf8-2.txt");
+  } else {
+    $args = ("corp-utf8.txt");
+  }
+
+  system ("sed -f $args $File::Find::name");
+}, '.');
