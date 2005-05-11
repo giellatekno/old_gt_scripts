ac0a1ea6e2e4d23910f4c93b1270413013a62bc8
Tomi Pieski <>
2005-05-11 13:13:15 +0000

    Script for processing corpus to directory according to the corpus file's metainformation. Usage: cd <rootdir-of-corpus-files> corpus2dir.pl

---
+++ corpus2dir.pl
@@ -0,0 +1,101 @@
+#!/usr/bin/perl -w
+
+use utf8;
+use strict;
+use encoding 'utf-8';
+use open ':utf8';
+use XML::Twig;
+use File::Find;
+use File::Spec;
+#use IO::File;
+#use Getopt::Long;
+
+my $publisher;
+my $person;
+my $year;
+my $title;
+my $TEMP_DIR = "../tmp/";
+my $PROJECT = "gt";
+
+mkdir ("$TEMP_DIR", 0755) unless -d $TEMP_DIR;
+$TEMP_DIR = File::Spec->rel2abs($TEMP_DIR);
+
+find ( sub {
+    return if -d;
+    return unless ($_ =~ /\.int\.xml/);
+
+    my $manual_xsl = File::Spec->rel2abs($_);
+    $manual_xsl =~ s/\.int\.xml/\.xsl/i;
+
+    if (-e $manual_xsl) {
+    	my $temp_file = "$TEMP_DIR/$_";
+
+    	system "xsltproc --novalid \"$manual_xsl\" $_ > \"$temp_file\"";
+    
+	   my $lang;
+	   my $document = XML::Twig->new(twig_handlers =>
+                                  {'document' => sub { $lang = $_->{'att'}->{'xml:lang'}}} );
+                                  
+        $document->parsefile ($temp_file);
+	   $document->purge;
+
+        my $xmlfile = XML::Twig->new(twig_roots =>
+                                 {'document/header' => \&process_header } );
+
+    	$xmlfile->parsefile ($temp_file);
+
+	   my $int = File::Spec->rel2abs($_);
+	   $int =~ s/int\/.*/$lang\/$PROJECT\/$publisher\/$year\/$title\.xml/;
+	   my $intdir;
+	   my $volume;
+	   my $file;
+	   ($volume, $intdir, $file) = File::Spec->splitpath($int);
+	   my @dirs = File::Spec->splitdir($intdir);
+        my $dirpath = "";
+        for my $dir (@dirs) {
+            $dirpath = $dirpath . $dir . "/";
+            print $dirpath . "\n";
+    	   mkdir ("$dirpath", 0755) unless -d $dirpath;
+        }
+
+	   system ("mv \"$_\" \"$int\"");
+    }
+}, '.');
+
+#find ( sub {
+#    return if -d;
+#    return unless ($_ =~ /\.int\.xml/);
+
+#    my $lang;
+#    my $document = XML::Twig->new(twig_handlers =>
+#				  {'document' => sub { $lang = $_->{'att'}->{'xml:lang'}}} );
+#    $document->parsefile ($_);
+#    $document->purge;
+
+#    my $xmlfile = XML::Twig->new(twig_roots =>
+#				 {'document/header' => \&process_header } );
+
+#    $xmlfile->parsefile ($_);
+
+#    my $int = File::Spec->rel2abs($_);
+#    $int =~ s/int\/.*/$lang\/$publisher\/$year\/$title\.xml/;
+
+#    print ("mv $_ $int");
+#    print "\n";
+#}, $TEMP_DIR);
+
+rmdir ("$TEMP_DIR");
+
+sub process_header
+{
+    my ($twig, $header) = @_;
+
+    $publisher = $header->first_child('publChannel')->first_child_text('publisher');
+
+    $person = $header->first_child('author')->first_child('person')->{'att'}->{'name'};
+
+    $year = $header->first_child_text('year');
+    $title = $header->first_child_text('title');
+
+    $twig->purge;
+}
