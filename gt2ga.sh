bf05bd945bb3faafc2b5d268176aa23cc7d5e612
Saara Huhmarniemi <saara.huhmarniemi@helsinki.fi>
2006-02-11 06:09:38 +0000

    added umask and full path to ccat

--- gt2ga.sh
+++ gt2ga.sh
@@ -17,6 +17,8 @@ languages="sme"
 tmpdir="/usr/tmp"
 gadir=/usr/local/share/corp/ga
 
+umask=0112
+
 # Build an up-to-date analyzator and cg and
 # analyze the contents of the corpus-hierarchy.
 # The results are stored under /usr/local/share/corp/ga
@@ -34,11 +36,12 @@ analyze_gt ()
 		do
 			mkdir -p $gadir/$lang/$dir;
 			echo "processing directory $lang/$dir..."
-			ccat -r /usr/local/share/corp/gt/$lang/$dir | \
+			/usr/local/bin/ccat -r /usr/local/share/corp/gt/$lang/$dir | \
 			$tmpdir/gt/script/preprocess --abbr=$tmpdir/gt/$lang/bin/abbr.txt | lookup \
 			-flags mbTT $tmpdir/gt/$lang/bin/$lang.fst | $tmpdir/gt/script/lookup2cg \
 			| vislcg --grammar=$tmpdir/gt/$lang/src/$lang-dis.rle  > $gadir/$lang/$dir/$dir.analyzed
 		done
+		chgrp -R cvs $gadir/$lang
 		chgrp -R cvs $gadir/$lang
 	done
 	return 0
