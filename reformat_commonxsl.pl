de6abba1cb3ea39b2be25b55122de2c893a38252
Saara Huhmarniemi <saara.huhmarniemi@helsinki.fi>
2006-02-17 12:40:30 +0000

    Perl script for adding the version information of the tools to the common.xsl

---
+++ reformat_commonxsl.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/perl
+#
+# reformat_commonxsl.pl
+#
+# Perl script for adding version information of the conversion tools 
+# to the common.xml. Called from copy_corpus_bin.sh
+#
+# $Id$
+
+my $convert="convert2xml.pl";
+my $hyph="add-hyph-tags.pl";
+
+my $convert_version;
+my $hyph_version;
+
+open (FH, "<$convert");
+while(<FH>) {
+	if (/\$Revision(.*?)\$/) {
+		$convert_version = $1;
+		last;
+	}
+}
+close FH;
+
+open (FH, "<$hyph");
+while(<FH>) {
+	if (/\$Revision(.*?)\$/) {
+		$hyph_version = $1;
+		last;
+	}
+}
+close FH;
+
+my $xsl_file="common.xsl";
+
+# Reformat the version information in the new xsl-file.
+open (FH, "+<$xsl_file");
+my @text_array = <FH> ;
+my @result_array;
+foreach my $line (@text_array){
+	if ($line =~ /name\=\"common_version/) { $line =~ s/\$Revision(.*?)\$/$1/g; }
+	if ($line =~ /name\=\"convert2xml_version/) { $line =~ s/(select=\"\').*?(\'\")/$1$convert_version$2/ };
+	if ($line =~ /name\=\"hyph_version/) { $line =~ s/(select=\"\').*?(\'\")/$1$hyph_version$2/ };
+	push @result_array, $line;
+}
+
+seek (FH,0,0);
+print FH @result_array;
+truncate(FH, tell(FH));
+close(FH);
