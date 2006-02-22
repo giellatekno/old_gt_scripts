8308ada473608598026a119d46a88c7e37d9d8ad
Saara Huhmarniemi <saara.huhmarniemi@helsinki.fi>
2006-02-22 11:41:28 +0000

    Added formatting of version info of the XSL-template.

--- reformat_commonxsl.pl
+++ reformat_commonxsl.pl
@@ -15,7 +15,7 @@ my $hyph_version;
 
 open (FH, "<$convert");
 while(<FH>) {
-	if (/\$Revision(.*?)\$/) {
+	if (/\$Revision\:(.*?)\$/) {
 		$convert_version = $1;
 		last;
 	}
@@ -24,17 +24,17 @@ close FH;
 
 open (FH, "<$hyph");
 while(<FH>) {
-	if (/\$Revision(.*?)\$/) {
+	if (/\$Revision\:(.*?)\$/) {
 		$hyph_version = $1;
 		last;
 	}
 }
 close FH;
 
-my $xsl_file="common.xsl";
+my $common_xsl="common.xsl";
 
 # Reformat the version information in the new xsl-file.
-open (FH, "+<$xsl_file");
+open (FH, "+<$common_xsl");
 my @text_array = <FH> ;
 my @result_array;
 foreach my $line (@text_array){
@@ -43,7 +43,24 @@ foreach my $line (@text_array){
 	if ($line =~ /name\=\"hyph_version/) { $line =~ s/(select=\"\').*?(\'\")/$1$hyph_version$2/ };
 	push @result_array, $line;
 }
+seek (FH,0,0);
+print FH @result_array;
+truncate(FH, tell(FH));
+close(FH);
 
+# Do some reformatting also for the template.
+
+my $xsl_template="XSL-template.xsl";
+
+print "ok";
+open (FH, "+<$xsl_template");
+my @text_array = <FH> ;
+my @result_array;
+foreach my $line (@text_array){
+	if ($line =~ /name\=\"current_version/) { $line =~ s/Revision/\$Revision\$/; }
+	if ($line =~ /name\=\"template_version/) { $line =~ s/\$Revision\:(.*?)\$/$1/g; }
+	push @result_array, $line;
+}
 seek (FH,0,0);
 print FH @result_array;
 truncate(FH, tell(FH));
