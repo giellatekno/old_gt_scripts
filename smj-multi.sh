aba6104e74613bf6451bbf5f5c90b575cfca81f0
Trond Trosterud <trond.trosterud@uit.no>
2006-03-14 11:15:31 +0000

    Fix.

--- smj-multi.sh
+++ smj-multi.sh
@@ -9,7 +9,7 @@ do                                          # run the following loop
 echo -n "Atte tjielgga: "                   # (message to user)
 read sentence                               # next 3 lines is the usual command
 echo $sentence | preprocess | lookup -flags mbTT -utf8 ~/gt/smj/bin/smj.fst | ~/gt/script/lookup2cg
-#vislcg --grammar ~/gt/smj/src/smj-dis.rle --minimal
+# vislcg --grammar ~/gt/smj/src/smj-dis.rle --minimal
 
 done                      
 exit 0
