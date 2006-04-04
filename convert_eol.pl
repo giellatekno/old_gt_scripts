5c41f60cd6ee83dc37c55d38d98ad085ec0fc774
Saara Huhmarniemi <saara.huhmarniemi@helsinki.fi>
2006-04-04 16:24:56 +0000

    Added removal of no-break space, which caused trouble in some html-documents.

--- convert_eol.pl
+++ convert_eol.pl
@@ -5,8 +5,12 @@
 # $Id$
 
 use strict;
+use open ':utf8';
 
 my $fdata;
+
+# no-break space
+my $no_break = 160;
 
 # conversion to mac / pc / unix
 my $tostring = "\n";
@@ -35,6 +39,19 @@ foreach my $file(@ARGV){
 		$fdata =~ s/\r\n/\n/g;
 		$fdata =~ s/\r/\n/g;
 		$fdata =~ s/\n/$tostring/g;
+
+		# remove no-break space
+		my @unpacked = unpack("U*", $fdata);
+		my $i=0;
+		while ($unpacked[$i]) {
+			my $byte = $unpacked[$i];
+			if ($byte == $no_break) {
+				splice(@unpacked, $i, 1);
+				next;
+			}
+			$i++;
+		}
+		$fdata = pack("U*", @unpacked);
 		print $fdata;
 	} else {
         warn ("File $file could not be read\n");
