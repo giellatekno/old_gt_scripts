ccccb31b2f175c653fb76db2aafdb51fe118ce83
Saara Huhmarniemi <saara.huhmarniemi@helsinki.fi>
2006-04-08 12:16:47 +0000

    Shell script for changing corpus file permissions and groups according to our specs. E.g after you have moved the files created by apache to their places, run this script to get the permissions and groups right (it will go through all the files in specific lang dir).

---
+++ corpus_chmod.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+#****************************************************************
+#			corpus_chmod.sh
+#			written by Saara Huhmarniemi
+#			Apr 7, 2006
+#
+#           Fix the permissions of the corpus directories
+#
+# $Id$
+#****************************************************************
+
+# Change file group and permissions for different corpus directories.
+
+corpdir=/usr/local/share/corp
+
+# Add different languages here, separated by newline.
+langdirs="sme"
+
+# Original documents in orig-hierarchy
+# xsl-files that are under version control need not to be modified,
+# since they are dependent on the directory settings only.
+orig ()
+{
+	for dir in "$@"
+	do
+	  echo "fixing $corpdir/orig/$dir..."
+	  files=`find $corpdir/orig/$dir -name "*.pdf" -or -name "*.doc" -or -name "*.html" -or -name "*.ptx"` 
+	  for file in $files
+	  do
+		chgrp corpus $file
+		chmod 0660 $file
+	  done
+	done
+}
+
+# xml-files in gtbound
+gtbound () 
+{
+	for dir in "$@"
+	do
+	  echo "fixing $corpdir/gtbound/$dir..."
+	  files=`find $corpdir/gtbound/$dir -name "*.xml"`
+	  for file in $files
+	  do
+		chgrp cvs $file
+		chmod 0660 $file
+	  done
+	 done
+}
+
+# Free xml-files in gtfree
+gtfree () 
+{
+	for dir in "$@"
+	do
+	  echo "fixing $corpdir/gtfree/$dir..."
+	  files=`find $corpdir/gtfree/$dir -name "*.xml"`
+	  for file in $files
+	  do
+		chgrp cvs $file
+		chmod 0664 $file
+	  done
+	done
+}
+
+orig $langdirs
+gtbound $langdirs
+gtfree $langdirs
+
+exit 0
