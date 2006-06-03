e624972fbf2aa4c2a77683b2bcbed92acc2e52a8
Saara Huhmarniemi <saara.huhmarniemi@helsinki.fi>
2006-06-03 09:01:50 +0000

    Added actual mv of the file to the normalized form. Some tests added as well.

--- normalize.pl
+++ normalize.pl
@@ -1,16 +1,33 @@
 #!/usr/bin/perl
 
-# Perl script for converting string to NFC (precomposed unicode characters).
+# Perl script for converting filenames to NFC (precomposed unicode characters).
 # Used in fixing decomposed unicode characters (NFD) produced by mac.
+# Files are given to the script via STDIN.
 
+use strict;
 use open ':locale';
 binmode STDOUT, ":utf8";
+use File::Spec;
 
 use Unicode::Normalize;
 
 while(<>) {
 
-	$NFC_string  = NFC($_);  # Normalization Form C
-	print $NFC_string;
-
+	chomp;
+    my $file = File::Spec->rel2abs($_);
+	if ( -e "$_" ) {
+#		print "Fixing $file..\n";
+		my $newname = NFC($file);  # Normalization Form C
+		
+		if ( "$file" ne "$newname" ) {
+			if ( -e $newname ) {
+				print "Cannot move $file:  $newname exists already\n";
+				next;
+			}
+			print "mv $file $newname\n";
+			my @args=("mv", "$file", "$newname");
+			system(@args) == 0
+				or print "Cannot move $file: $!";
+		}
+	}
 }
