89feae8ee5787668161a1668a56f9055edfb6a73
Linda <32739027+lynnda-hill@users.noreply.github.com>
2006-08-31 12:38:00 +0000

    Added a new script for multiple syntactic analyses equivalent to the section=1 version of the analysis command.

---
+++ sme-multisyn.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# sme-snt.sh
+# This is a shell script for analysing Northern Sámi sentences.
+# It gives the analysis, and the number of the rules used to disambiguate
+
+while [ 1 ]                                 # as long as there is input
+do                                          # run the following loop
+echo -n "Atte cealkaga: "                   # (message to user)
+read sentence                               # next 3 lines is the usual command
+echo $sentence | preprocess --abbr=~/gt/sme/bin/abbr.txt | \
+lookup -flags mbTT -utf8 ~/gt/sme/bin/sme.fst | ~/gt/script/lookup2cg | \
+vislcg --grammar ~/gt/sme/src/sme-dis.rle --minimal --sections=1 
+
+done                      
+exit 0
