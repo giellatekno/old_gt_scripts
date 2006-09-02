fcf5cb73d7741c7a8b89bbedf2a6ab262d841311
Saara Huhmarniemi <saara.huhmarniemi@helsinki.fi>
2006-09-02 11:20:45 +0000

    Added copying of the Makefile to the script.

--- copy_corpus_bin.sh
+++ copy_corpus_bin.sh
@@ -10,12 +10,12 @@
 # $Id$
 #****************************************************************
 
+corpdir=/usr/local/share/corp
 bindir=/usr/local/share/corp/bin
 tmpdir="/usr/tmp"
 
 scripts="convert2xml.pl
 text_cat
-LM
 paratext2xml.pl
 add-hyph-tags.pl
 convert_eol.pl
@@ -24,13 +24,18 @@ common.xsl
 xhtml2corpus.xsl
 docbook2corpus2.xsl"
 
+makefile="corpus.make"
+
 cvs -d /usr/local/cvs/repository checkout -d $tmpdir/gt gt
 
 for file in $scripts
 do
-  cp -R $tmpdir/gt/script/$file $bindir/$file
+  cp $tmpdir/gt/script/$file $bindir/$file
   echo "copying file $bindir/$file"
 done
+
+cp $tmpdir/gt/script/LM/* $bindir/LM/
+cp $tmpdir/gt/script/$makefile $corpdir/Makefile
 
 cd $bindir && perl $tmpdir/gt/script/reformat_commonxsl.pl
 chgrp cvs $bindir/*
