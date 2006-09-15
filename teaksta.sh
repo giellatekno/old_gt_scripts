01522f054cc775554b1e45f5a1018e76b39f5936
Trond Trosterud <trond.trosterud@uit.no>
2006-09-15 08:47:00 +0000

    Made the script work, provided the user adds one of these lines to his/her .bashrc: export GT_HOME=/Users/<username>/ export GT_HOME=/Users/<username>/Documents/ (according to whether the gt/ catalogue is in the home dir or in Documents).
    The script now works with plain text input files, it can be tweaked in different ways.
    
    I also added a plan for replacing this script, and the family of cealkka, sme-dis.sh, smj-multi.sh etc. with one script with several options. The options, their default values, etc. were sketched.
    
    With this sketch the script leaves the realm of my scripting competence, and I hope for input from computer-literates.

--- teaksta.sh
+++ teaksta.sh
@@ -7,12 +7,44 @@
 # usage:
 # teaksta.sh filename
 
-# This one does not work:
-cat  $1 | preprocess --abbr=~/gt/sme/bin/abbr.txt --corr=~/gt/sme/src/typos.txt | \
-lookup -flags mbTT -utf8 ~/gt/sme/bin/sme.fst | ~/gt/script/lookup2cg | \
-vislcg --grammar ~/gt/sme/src/sme-dis.rle | less #--minimal
+# You need to make GT_HOME point to your gt/ directory, normally one of the two following:
+# export GT_HOME=/Users/<your_user_name> 
+# export GT_HOME=/Users/<your_user_name>/Documents
 
-# This one works when I stand in gt/script, but not elsewhere
-#cat  $1 | preprocess --abbr=../sme/bin/abbr.txt --corr=../sme/src/typos.txt | \
-#lookup -flags mbTT -utf8 ../sme/bin/sme.fst | ../script/lookup2cg | \
-#vislcg --grammar ../sme/src/sme-dis.rle | less #--minimal
+cat  $1 | preprocess --abbr=$GT_HOME/gt/sme/bin/abbr.txt --corr=$GT_HOME/gt/sme/src/typos.txt | \
+lookup -flags mbTT -utf8 $GT_HOME/gt/sme/bin/sme.fst | $GT_HOME/gt/script/lookup2cg | \
+vislcg --grammar $GT_HOME/gt/sme/src/sme-dis.rle --minimal
+
+
+# Notes for further development:
+# ==============================
+
+# At the moment, there is a family of scripts for analysis, in addition to this one:
+# cealkka (sentence), sme-dis.sh (sentence, with rule nr), sme-multi.sh (sentence w/o disamb)
+#                     smj-dis.sh (sentence, with rule nr), smj-multi.sh (sentence w/o disamb)
+# With more lgs and more options this will develop into a wildernis.
+
+# What we want:
+# One script, with parametrised options along several paths:
+# What language, 
+# What kind of input (plain text, xml text, evt. other text formats as well)
+# What kind of output (disambiguated text with and without rule numbers, non-disambiguated text with and without syntactic tags
+# What kind of morphological transducers (standard (tolerant) or normative (restricted))
+# 
+# dis.sh (script for disambiguating text)
+# options:
+# -l <lang> what disambiguator to call for (compulsatory)
+# -n        gives the rule number (no rule numbers is default)
+# -m        gives the non-disambiguated output (only section 1 in the .rle file)
+# -norm     uses normative transducers for the morphological analysis,
+#           ie will not recognise any non-conformant spellings
+# -i <type> gives text type:
+#           xml  (default, calls for ccat with the same <lang> as given above)
+#           txt  (takes plain text as input, is basically the script we have now)
+#           doc  (if we bother doing this, it would call for ... | antiword -db | ccat | ...)
+#           odt  (openoffice documents could probably get the same treatment
+#           html (importing relevant code from convert2xml.pl)
+#           pdf  (importing relevant code from convert2xml.pl)
+# -o <type> visl (default is the standard vislcg output format)
+#           xml  (gives xml-tagget output)
+
