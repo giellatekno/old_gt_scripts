ab76337f95cfab8db636e14d0ec6c10ab1656947
Saara Huhmarniemi <saara.huhmarniemi@helsinki.fi>
2006-09-25 14:00:13 +0000

    shell script for calling the jpedal program.

---
+++ corpus_call_jpedal.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+libDir=/home/saara/src/jpedal/additional_jars
+jpedalDir=/home/saara/src/jpedal
+
+if [ -n "$1" -a -n "$2" ]
+then
+	file=$(echo $1 | sed -re "s/.*\///")
+	basename=$(echo $1 | sed -re "s/.*\/(.*)\.pdf/\1/")
+	java -Ddir=$2/$basename -Dxml -cp $libDir/bcprov-jdk14-119.jar:$libDir/jai_core.jar:$libDir/jai_codec.jar:$jpedalDir/os_jpedal.jar org/jpedal/examples/text/ExtractTextInRectangle $1 
+fi 
+
+
