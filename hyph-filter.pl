5bae64651908f301f64c5c23ab43fa3df365e2d0
Sjur N Moshagen <sjurnm@mac.com>
2006-09-26 06:37:45 +0000

    Added hyphenation filter script.

---
+++ hyph-filter.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl -w
+
+# Short script to removed hyphenated words that do not correspond to the input
+# string, barring the hyphenation and word boundary marks.
+# It removes ^# from the input hyphenated string, and compares with the original
+# and then print out the hyphenated one if the comparison is true.
+
+while (<>) {
+  chomp;
+  my ($orig, $hyph) = split (/\s+/);
+  my $cleaned = $hyph;
+     $cleaned =~ s/[#^]//g;
+  if ($orig eq $cleaned) {
+    print "$hyph\n";
+  }
+}
