a4836ed4818c84d919c72e4bc0f8862d9257ba33
Saara Huhmarniemi <saara.huhmarniemi@helsinki.fi>
2006-09-29 11:51:32 +0000

    Added function for adding <s> tags to the xml-structure without analysis, this option is used when the parallel files are preprocessed. It is also possible to analyze an xml-file both with and without the <s>-tags. In the latter case, they are added to the structure.

--- corpus-analyze.pl
+++ corpus-analyze.pl
@@ -8,12 +8,18 @@ use strict;
 # --tags=<file_name>
 # --input=<file_name> (default STDIN)
 # --output=<file_name> (default tmp.out)
+# --add_sentences
+# --only_add_sentences
 #
 # The input to the script consist of start and end tags, e.g 
 # <document> ... </document> and inside them, a set of paragraphs in
 # <p>-tags. Each paragraph is send to complete analysis and the 
-# output stored to a separate file. The sentences are surrounded with
-# <s>-tags. 
+# output stored to a separate file. The <s>-tags are assumed in input,
+# but they can be added as well, with option --add_sentences. 
+#
+# It is possible to add <s>-tags with ids to the structure without
+# analyzing the words, with option --only_add_sentences. 
+# The parallel files are preprocessed with this option.
 #
 # Typical usage: ccat --add-id docu.xml | corpus-analyze.pl
 #
@@ -27,6 +33,21 @@ use POSIX qw(locale_h);
 use Getopt::Long;
 
 my $lang="sme";
+my $s_id=0;
+my $add_sentences=0;
+my $only_add_sentences=0;
+my $tagfile = "/usr/tmp/gt/cwb/korpustags.txt";
+
+my $infile;
+my $outfile="out.tmp";
+GetOptions ("tags=s" => \$tagfile,
+            "input=s" => \$infile,
+			"output=s" => \$outfile,
+			"add_sentences" => \$add_sentences,
+			"only_add_sentences" => \$only_add_sentences,
+			"lang=s" => \$lang,
+);
+
 my $binpath="/opt/smi/$lang/bin";
 
 my $cap = $binpath ."/". "cap-" . $lang;
@@ -35,17 +56,9 @@ my $abbr = $binpath ."/abbr.txt";
 my $rle = $binpath ."/". $lang ."-dis.rle";
 my $preproc = "/usr/local/bin/preprocess";
 my $lookup2cg = "/usr/local/bin/lookup2cg";
-my $tagfile = "/usr/tmp/gt/cwb/korpustags.txt";
 my $corrtypos="/home/trond/bin/corrtypos.pl";
 my $lookup="/opt/sami/xerox/c-fsm/ix86-linux2.6-gcc3.4/bin/lookup";
 my $vislcg="/opt/xerox/bin/vislcg";
-my $s_id=0;
-
-my $infile;
-my $outfile="out.tmp";
-GetOptions ("tags=s" => \$tagfile,
-            "input=s" => \$infile,
-			"output=s" => \$outfile);
 
 my $ifh;
 if ($infile) {
@@ -54,7 +67,8 @@ if ($infile) {
 else { $ifh = 'STDIN'; }
 
 my $analyze = "$preproc --abbr=$abbr --fst=$fst | $corrtypos | $lookup -flags mbTT -utf8 -f $cap | $lookup2cg | $vislcg --grammar=$rle --quiet";
-print STDERR $analyze;
+
+my $preprocess = "$preproc --abbr=$abbr --fst=$fst";
 
 my $SENT_DELIM = qq|.!?|;
 
@@ -71,12 +85,35 @@ while(<$ifh>) {
 
 if(! $string) { exit; }
 
-my $document = XML::Twig->new(twig_handlers => {  'p' => sub { analyze_para(@_);
-														   keep_encoding => 1 } });
+my $document;
 
-if (! $document->safe_parse ($string)) {
-	print STDERR "Couldn't parse file: $@";
+# Do not analyze, only add <s>-tags.
+if ($only_add_sentences) {
+	$document = XML::Twig->new(twig_handlers => {  'p' => sub { add_sentences(@_);
+																keep_encoding => 1 } });
+	if (! $document->safe_parse ($string)) {
+		print STDERR "Couldn't parse file: $@";
+	}
 }
+# Otherwise analyze each para and add sentences
+elsif ( $add_sentences) {
+    print STDERR $analyze;
+	$document = XML::Twig->new(twig_handlers => {  'p' => sub { analyze_block(@_, "para");
+																   keep_encoding => 1 } });	
+	if (! $document->safe_parse ($string)) {
+		print STDERR "Couldn't parse file: $@";
+	}
+}
+# Otherwise analyze each sentence
+else {
+    print STDERR $analyze;
+	$document = XML::Twig->new(twig_handlers => {  's' => sub { analyze_block(@_, "sentence");
+																   keep_encoding => 1 } });
+	if (! $document->safe_parse ($string)) {
+		print STDERR "Couldn't parse file: $@";
+	}
+}
+
 
 open (FH, ">:utf8", "$outfile") or die "Cannot open $!";
 $document->set_pretty_print('record');
@@ -84,19 +121,91 @@ $document->print( \*FH);
 $document->purge;
 close(FH);
 
-sub analyze_para {
+
+sub add_sentences {
 	my ($twig, $para) = @_;
+
+	my @answers;
+
+	$para->set_asis;
+	my $text = $para->text;
+	print "$text\n";
+
+	for my $c ($para->children) {
+		$c->delete;
+	}
+
+	$text =~ s/\n/ /g;
 	
+	my $pid = open2(\*R,\*W, $preprocess); 
+	$pid || die "did not work as expected:$!"; 
+
+	binmode R, ':utf8';
+	binmode W, ':utf8';
+
+	print W "$text\n";
+	close W;
+	while( my $answer = <R>) { push (@answers, $answer); }
+	close R;
+
+	waitpid $pid, 0 ;	
+
+	my $sentence;
+	my @words;
+	my $ans;
+	print @answers;
+  WORDS:
+	for $ans (@answers) {
+		chomp $ans;
+		
+		# ignore empty lines
+		next WORDS if $ans =~ /^\s*$/;
+
+		if (! $sentence) {
+			# create an XML-element for a new sentence.
+			$sentence = XML::Twig::Elt->new('s');
+			$sentence->set_att('id', $s_id++);
+		}
+		
+		push (@words, $ans);
+		push (@words, " ");
+
+		if ($ans =~ /^[$SENT_DELIM]$/) {
+			$sentence->set_content(@words);
+			$sentence->paste('last_child', $para);
+			$sentence->DESTROY;
+			$sentence=undef;
+			@words=undef;
+			pop @words;
+			next WORDS;
+		}
+	}
+	if ($ans) { push (@words, $ans); }
+	if (@words) {
+		if (! $sentence) {
+			# create an XML-element for a new sentence.
+			$sentence = XML::Twig::Elt->new('s');
+			$sentence->set_att('id', $s_id++);
+		}
+		$sentence->set_content(@words);
+		$sentence->paste('last_child', $para);
+	}
+}
+
+sub analyze_block {
+	my ($twig, $block, $type) = @_;
+
 	my @answers;
 
 	my $cohort_rec;
 	my @tokens;
 
-	$para->set_asis;
+	$block->set_asis;
 
-	my $text = $para->text;
+	my $text = $block->text;
+	print "$text\n";
 	$text =~ s/\n/ /g;
-
+	
 	my $pid = open2(\*R,\*W, $analyze); 
 	$pid || die "did not work as expected:$!"; 
 
@@ -138,28 +247,25 @@ sub analyze_para {
 	if ($cohort_rec) {
 		push @tokens, $cohort_rec;
 	}
-	add_sentence(\@tokens, \%tags, $para);
+	add_structure(\@tokens, \%tags, $block, $type);
 }
 
 # Subroutine to add <s> tags and print out the result.
-sub add_sentence {
-	my ($tokens_aref, $tags_href, $para) = @_;
-	
-	my @sentences;
+sub add_structure {
+	my ($tokens_aref, $tags_href, $block, $type) = @_;
 
-	# create an XML-element for a new sentence.
-	my $sentence = XML::Twig::Elt->new('s');
-	$sentence->set_att('id', $s_id++);
+	my @sentences;
+	my @tokens;
+	my $sentence;
 
 	while (my $token_rec = shift @$tokens_aref) {
 
 		# Create a new XML-element for the token
 		my $token = XML::Twig::Elt->new('w');
-		if(! $sentence) { 
+		if($type eq "para" && ! $sentence ) { 
 			$sentence = XML::Twig::Elt->new('s');
 			$sentence->set_att('id', $s_id++);
 		}
-		$token->paste('last_child', $sentence);
 
 		$token_rec->{WORD} =~ s/^\"<(.*)?>\".*$/$1/;
 		chomp $token_rec->{WORD};
@@ -192,18 +298,33 @@ sub add_sentence {
 
 			# Store the reading to child of the token in XML tree
 			$reading->paste('last_child', $token);
-		}  # end while readings
+		} # end while readings
 
-		if ($token_rec->{WORD} =~ /^[$SENT_DELIM]$/) {
+		push (@tokens, $token);
+
+		if ($type eq "para" && $token_rec->{WORD} =~ /^[$SENT_DELIM]$/) {
+			$sentence->set_content(@tokens);
 			push @sentences, $sentence;
-			undef $sentence;
+			$sentence->DESTROY;
+			$sentence=undef;
+			@tokens=undef;
+			pop @tokens;
 		}
+
 	} # end while tokens
 
-	if($sentence) {
-		push @sentences, $sentence;
+	if($type eq "para") {
+		if (@tokens) {
+			$sentence->set_content(@tokens);
+		}		
+		if ($sentence) {
+			push (@sentences, $sentence);
+		}
+		if (@sentences) {
+			$block->set_content(@sentences);
+		}
 	}
-	$para->set_content(@sentences);
+	else { $block->set_content(@tokens); }
 }
 
 # Subroutine to read the morphological tags from a file
