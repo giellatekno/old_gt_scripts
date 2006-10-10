41f9c2940a430f5aee830a704e532bff7527b10c
Saara Huhmarniemi <saara.huhmarniemi@helsinki.fi>
2006-10-10 08:47:35 +0000

    better upper case handling cases with #- seem to go fine, but they are not tested properly.

--- hyph-filter.pl
+++ hyph-filter.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/perl -w
 
 use strict;
+binmode STDOUT, ":utf8";
+binmode STDIN, ":utf8";
 
 # hyph-filter.pl 
 # Perls script for cleaning the hyphenator output.
@@ -17,70 +19,79 @@ use strict;
 $/ = "";
 
 # Read while not eol
-while(<>) {	
+INPUT:
+	while(<>) {	
+		my %forms;
+		
+		my $word;
+		my $hyph;
+		my $compound = 0;
+		my $max_boundary_count = 0;
+		my $min_boundary_count = 5;
+		my $max_comp_rate = 0;
+		my $boundary_count;
+		
+		chomp;
+		my @lines = split(/\n/, $_);
+		
+		for my $line (@lines) {
+			chomp;
+			($word, $hyph) = split(/\t/, $line);
 
-	my @Analyses;
-	my %forms;
-	my %rated;
-	
-	my $word;
-	my $hyph;
-	my $compound = 0;
-	my $max_boundary_count = 0;
-	my $min_boundary_count = 5;
-	my $max_comp_rate = 0;
-	my $boundary_count;
-	
-	chomp;
-	my @lines = split(/\n/, $_);
-	if ($#lines == 0) { print; print "\n\n"; next; }
-	for my $line (@lines) {
-		# store word to a scalar and
-		# analysis (=base form and tags) to an array
-		my $analysis;
-		($word, $hyph) = split(/\t/, $line, 2);
+			if ($#lines == 0 and $word eq $hyph) {
+				print; 
+				print "\n\n"; 
+				next INPUT; 
+			}
 
-		if ($hyph) {
 			#remove word boundary from the beginning of the word.
 			$hyph =~ s/^\#//;
+
+			# take the upper case form.
+			my $uc = ucfirst($hyph);
+			
+			# remove all the other wb:s
 			( my $cleaned = $hyph ) =~ tr/[\#^]//d;
-
-			# take acoount also the lower cased form.
-			(my $lcfirst = $word) =~ tr/A-ZÁČĐŊŠŦŽÅÆØÄÖÅ/a-záčđŋšŧžåæøäöå/;
-			next if ($word ne $cleaned && $lcfirst ne $cleaned);
-			$forms{$hyph} = 1;
-		
-		# Get the min and max boundary counts.
-		my $boundary_count = ( $hyph  =~ tr/\#// );
-		if ($boundary_count > $max_boundary_count) { $max_boundary_count = $boundary_count; }
-		if ( $boundary_count < $min_boundary_count) { $min_boundary_count = $boundary_count; }
+			
+		  TEST: {
+			  if ($word eq $cleaned) { $forms{$hyph} = 1; last TEST; }
+			  
+			  ( my $ucfirst = $uc ) =~ tr/[\#^]//d;
+			  if ($ucfirst eq $word) { $forms{$uc} = 1; last TEST; }
+			  
+			  #If gone this far, then move forward.
+			  next;
+		  }
+			# Get the min and max boundary counts.
+			my $boundary_count = ( $hyph  =~ tr/\#// );
+			if ($boundary_count > $max_boundary_count) { $max_boundary_count = $boundary_count; }
+			if ( $boundary_count < $min_boundary_count) { $min_boundary_count = $boundary_count; }
 		}
-	}
-
-  RATE_COMP: {
-	  # If there were no boundaries
-	  last RATE_COMP if ( $max_boundary_count == 0 );
-
-	  # If the least amount of boundaries was 0,
-	  # All the forms with # are removed.
-	  if( $min_boundary_count == 0) {
-		  for (keys %forms) { if (/\#/) { delete($forms{$_}); } }
-		  last RATE_COMP;
-	  }
-	  # If there were more than one word boundary in each form,
-	  # only preserve the form that has least word boundaries.
-	  for (keys %forms) {
-		  my $boundary_count = tr/\#//;
-		  if ( $boundary_count > $min_boundary_count )
-		  { delete($forms{$_}); }		  
-	  }
-  } #RATE_COMP
-
-	unless( %forms) { print STDERR "No forms for $word\n"; }
- 	
-	# Print the output.
-	for my $key (keys %forms) {
-		print "$word\t$key\n";
-	}
-	print "\n";
-} # while
+		
+	  RATE_COMP: {
+		  # If there were no boundaries
+		  last RATE_COMP if ( $max_boundary_count == 0 );
+		  
+		  # If the least amount of boundaries was 0,
+		  # All the forms with # are removed.
+		  if( $min_boundary_count == 0) {
+			  for (keys %forms) { if (/\#/) { delete($forms{$_}); } }
+			  last RATE_COMP;
+		  }
+		  # If there were more than one word boundary in each form,
+		  # only preserve the form that has least word boundaries.
+		  for (keys %forms) {
+			  my $boundary_count = tr/\#//;
+			  if ( $boundary_count > $min_boundary_count )
+			  { delete($forms{$_}); }		  
+		  }
+	  } #RATE_COMP
+		
+		unless( %forms) { print STDERR "No forms for $word\n"; }
+		
+		# Print the output.
+		for my $key (keys %forms) {
+			print "$word\t$key\n";
+		}
+		print "\n";
+	} # while
