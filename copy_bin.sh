8f56f3bde42b3fa919bd64588639b5d6f99a71a9
Saara Huhmarniemi <saara.huhmarniemi@helsinki.fi>
2006-11-08 05:09:51 +0000

    Shell script for copying the project binaries to /usr/local/bin and some Perl modules to perl hierarchy.

---
+++ copy_bin.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+#****************************************************************
+#			copy_bin.sh
+#			written by Saara Huhmarniemi
+#			Oct 30, 2006
+#
+#           Copy the scripts and binaries that are needed in
+#           corpus- and other linguistic work to the common bin.
+#
+# $Id$
+#****************************************************************
+
+tmpdir=/usr/tmp
+script_dir=$tmpdir/gt/script
+bindir=$tmpdir/gt/bin
+srcdir=$tmpdir/gt/src
+
+perl_libdir=/usr/lib/perl5/5.8.5
+common_bin=/usr/local/bin
+
+own_modules="samiChar
+langTools"
+other_modules="XML::Twig"
+
+binaries="ccat"
+
+scripts="preprocess lookup2cg text_cat hyph-filter.pl cgi-export"
+
+##### start copying
+
+# First check out the lateset version from cvs.
+echo "Check out gt from cvs"
+cvs -d /usr/local/cvs/repository checkout -d $tmpdir/gt gt
+
+# make and copy ccat
+echo "Making and copying ccat.."
+echo "cd $script_dir/samiXMLParser && make"
+cd $script_dir/samiXMLParser && make
+echo "cp $script_dir/samiXMLParser/ccat $common_bin"
+cp $script_dir/samiXMLParser/ccat $common_bin
+
+# copy scripts
+echo "Copying scripts.."
+for s in $scripts
+do 
+  echo "cp $script_dir/$s $common_bin"
+  cp $script_dir/$s $common_bin
+done
+
+# copy perl modules
+echo "Copying Perl modules.."
+for mod in $own_modules
+do
+  echo "cp -r $script_dir/$mod $perl_libdir"
+  cp -r $script_dir/$mod $perl_libdir
+done
+
