130f2575db2a066764c63046291af8c07334a5bb
Saara Huhmarniemi <saara.huhmarniemi@helsinki.fi>
2006-12-05 13:16:58 +0000

    Added handling of two column pdf's.

--- corpus_call_jpedal.sh
+++ corpus_call_jpedal.sh
@@ -3,11 +3,21 @@
 libDir=/home/saara/src/jpedal/additional_jars
 jpedalDir=/home/saara/src/jpedal
 
+mei=$@
+
+a=$(echo "$@" | grep '.*-D.*')
+if [ ! -z "$a" ]
+then 
+	twocol="-Dcol"
+fi
+
+echo $mei
+
 if [ -n "$1" -a -n "$2" ]
 then
 	file=$(echo $1 | sed -re "s/.*\///")
 	basename=$(echo $1 | sed -re "s/.*\/(.*)\.pdf/\1/")
-	java -Ddir=$2/$basename -Dxml -cp $libDir/bcprov-jdk14-119.jar:$libDir/jai_core.jar:$libDir/jai_codec.jar:$jpedalDir/os_jpedal.jar org/jpedal/examples/text/ExtractTextInRectangle $1 
+	java $twocol -Ddir=$2/$basename -Dxml -cp $libDir/bcprov-jdk14-119.jar:$libDir/jai_core.jar:$libDir/jai_codec.jar:$jpedalDir/os_jpedal.jar org/jpedal/examples/text/ExtractTextInRectangle $1
 fi 
 
 
