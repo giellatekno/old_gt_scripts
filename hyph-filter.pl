c64d1b02316e4fc7e7135c8eb3d8a1fa2203907d
Saara Huhmarniemi <saara.huhmarniemi@helsinki.fi>
2006-12-07 15:07:30 +0000

    Optimized by 20%. Removed handling of compounds, since there seems to be an ^ alternative always present. If this changes then the compound handling has to be restrored.

--- hyph-filter.pl
+++ hyph-filter.pl
@@ -21,38 +21,34 @@ use utf8;
 # set reading to paragraph mode.
 $/ = "";
 
-# change this to 1 when used with smi.cgi.
-my $take_only_one=1;
-
 # Read while not eol
 INPUT:
 	while(<>) {	
+	
+		chomp;
+		my %lines;
+		@lines{split(/\n/, $_)} = ();
+		
 		my %forms;
 		
 		my $word;
 		my $hyph;
-		my $compound = 0;
-		my $max_boundary_count = 0;
-		my $min_boundary_count = 5;
-		my $max_comp_rate = 0;
-		my $boundary_count;
-		
-		chomp;
-		my @lines = split(/\n/, $_);
-		
-		for my $line (@lines) {
+
+		for my $line (keys %lines) {
 			chomp;
+
 			($word, $hyph) = split(/\t/, $line);
 			next if (! $hyph);
 
-			if ($#lines == 0 and $word eq $hyph && $line !~ /\+\?/) {
-				print; 
+			if (scalar(keys %lines) == 1) { # and $word eq $hyph && $line !~ /\+\?/) {
+				$line =~ s/\s*\+\?//;
+				print $line; 
 				print "\n\n"; 
 				next INPUT; 
 			}
 
-			#remove word boundary from the beginning of the word.
-			$hyph =~ s/^\#//;
+			#remove word boundaries
+			$hyph =~ tr/\#//;
 
 			# take the upper case and lower case forms.
 			my $uc = ucfirst($hyph);
@@ -60,60 +56,29 @@ INPUT:
 
 			# Take hyphenation point count.
 			my $count = ($hyph =~ tr/^//);
-			#if ($count == 0) {
 
 			# remove all the other wb:s
-			( my $cleaned = $hyph ) =~ tr/[\#^]//d;
+			( my $cleaned = $hyph ) =~ tr/[^]//d;
 			
 		  TEST: {
 			  if ($word eq $cleaned) { $forms{$hyph} = $count; last TEST; }
 			  
-			  ( my $ucfirst = $uc ) =~ tr/[\#^]//d;
+			  ( my $ucfirst = $uc ) =~ tr/[^]//d;
 			  if ($ucfirst eq $word) { $forms{$uc} = $count; last TEST; }
 
-			  ( my $lcfirst = $lc ) =~ tr/[\#^]//d;
+			  ( my $lcfirst = $lc ) =~ tr/[^]//d;
 			  if ($lcfirst eq $word) { $forms{$lc} = $count; last TEST; }
 			  
 			  #If gone this far, then move forward.
 			  next;
 		  }
-			# Get the min and max boundary counts.
-			my $boundary_count = ( $hyph  =~ tr/\#// );
-			if ($boundary_count > $max_boundary_count) { $max_boundary_count = $boundary_count; }
-			if ( $boundary_count < $min_boundary_count) { $min_boundary_count = $boundary_count; }
 		}
-		
-	  RATE_COMP: {
-		  # If there were no boundaries
-		  last RATE_COMP if ( $max_boundary_count == 0 );
-		  
-		  # If the least amount of boundaries was 0,
-		  # All the forms with # are removed.
-		  if( $min_boundary_count == 0) {
-			  for (keys %forms) { if (/\#/) { delete($forms{$_}); } }
-			  last RATE_COMP;
-		  }
-		  # If there were more than one word boundary in each form,
-		  # only preserve the form that has least word boundaries.
-		  for (keys %forms) {
-			  my $boundary_count = tr/\#//;
-			  if ( $boundary_count > $min_boundary_count )
-			  { delete($forms{$_}); }		  
-		  }
-	  } #RATE_COMP
-		
 		unless( %forms) { print STDERR "No forms for $word\n"; }
 		
 		# Print the output.
-		if($take_only_one) {
-			for my $key (sort { $forms{$b} <=> $forms{$a}} keys %forms) {
-				print "$word\t$key\n";
-				last;
-			}
-		} else {
-			for my $key (keys %forms) {
-				print "$word\t$key\n";
-			}
+		for my $key (sort { $forms{$b} <=> $forms{$a}} keys %forms) {
+			print "$word\t$key\n";
+			last;
 		}
 		print "\n";
 	} # while
