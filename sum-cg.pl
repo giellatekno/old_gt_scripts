2f421c6ef9f96af4179fa3ec487b2cfb33f356d6
Saara Huhmarniemi <saara.huhmarniemi@helsinki.fi>
2006-12-11 10:47:52 +0000

    Fixed the memory problem. Added option --dir to process directories recursively. To collect statistics from all ga-files: gt/script/sum-cg.pl --words --dir=zcorp/ga/sme/ > statistics.file Or from one file: gt/script/sum-cg.pl --words zcorp/ga/sme/bible/bible.analyzed > statistics.file There are some error messages due to the text source, but nothing fatal I hope.

--- sum-cg.pl
+++ sum-cg.pl
@@ -13,18 +13,25 @@ use locale;
 # 
 # $Id$
 
+binmode STDERR, ":utf8";
+
 # permit named arguments
 use Getopt::Long;
+use File::Find;
+use File::Basename;
+use Carp qw(cluck);
 
 my $help;
 my $grammar;
 my $print_words;
 my $string="";
+my $dir;
 
 GetOptions ("help" => \$help,
 			"grammar" => \$grammar,
 			"words" => \$print_words,
 			"string=s" => \$string,
+			"dir=s" => \$dir,
 			) ;
 
 if ($help) {
@@ -33,10 +40,6 @@ if ($help) {
 }
 
 my $anal_count = 0;
-
-# hash of hashes base -> analyses
-my %analyses;
-
 my %cohorts;
 my %count;
 
@@ -45,45 +48,16 @@ my %count;
 # Read while not eol
 my $word;
 my $whole;
-	my $base;
+my $base;
 
-LINE : while(<>) {	
-	next if (/^\s*$/);
-	READING : {
-		last READING if (/^\"</);
-		if(/(\".*?\")(\s+.*)$/) {
-			$base = $1;
-			my $analysis = $2;
-			$anal_count += 1;
-			$analyses{$base}{$analysis} = 1;
-		}
-		else { print "Line not recognized: $word, $_\n"; }
-		next LINE;
-	} # READING
+# Search the files in the directory $dir and process each one of them.
+if ($dir) {
+	if (-d $dir) { find (\&process_file, $dir) }
+	else { cluck "Directory did not exist."; }
+}
 
-	my %analyses_2;
-	for my $ba (keys %analyses) {
-		foreach my $key (sort keys %{$analyses{$ba}}) {
-			$analyses_2{$ba}{$key} = 1;
-			$whole .= $key;
-		}
-	}
-
-	$count{$whole} += 1;
-	$cohorts{$whole} = { %analyses_2 };
-	
-	if ($whole && $anal_count == 1) {
-		delete($cohorts{$whole});
-		delete($count{$whole});
-	}
-
-	if (/^\"<.*?>\".*$/) {
-		# Start with the new word
-		$whole = $_;
-		%analyses = ();
-		$anal_count = 0;
-	}
-} # LINE
+# Process the file given in command line.
+process_file ($ARGV[$#ARGV]) if -f $ARGV[$#ARGV];
 
 if ($grammar) {
 	my %tags;
@@ -95,7 +69,7 @@ if ($grammar) {
 				$amb .= "\n$anal";
 				$count_an += 1;
 			}
-# The statistics are count for different cohorts.
+            # The statistics are count for different cohorts.
 			if ($count_an > 1) {
 				$tags{$amb}{'number'} += $count{$cohort};
 				my $word = $cohort;
@@ -148,10 +122,85 @@ else {
 	}
 }
 
+
+sub process_file {
+
+    my $file = $_;
+    $file = shift (@_) if (!$file);
+
+	my $no_decode_this_time = 0;
+
+	return if (! -f $file);
+	return if (-z $file);
+
+	print STDERR "Processing file: $file\n";
+
+	open(FH, $file);
+	
+	my $line;
+	while(<FH>) {
+
+		return if eof(FH);
+		
+		$line = $_;
+		return if (! $line);
+
+		next if ($line =~ /^\s*$/);
+
+		# hash of hashes base -> analyses
+		my %analyses;
+
+		LINE :
+			while ($line && $line !~ /^\"</) {
+				if($line =~ /(\".*?\")(\s+.*)$/) {
+					$base = $1;
+					my $analysis = $2;
+					$anal_count += 1;
+					$analyses{$base}{$analysis} = 1;
+				}
+				else { print STDERR "Line not recognized: $word, $_\n"; }
+				
+				while(<FH>) {
+					$line = $_;
+					next if ($line =~ /^\s*$/);
+					last LINE if eof(FH);
+					next LINE;
+				}
+			} #end of LINE
+		
+		my %analyses_2;
+		for my $ba (keys %analyses) {
+			foreach my $key (sort keys %{$analyses{$ba}}) {
+				$analyses_2{$ba}{$key} = 1;
+				$whole .= $key;
+			}
+		}
+		
+		$count{$whole} += 1;
+		$cohorts{$whole} = { %analyses_2 };
+		
+		if ($whole && $anal_count == 1) {
+			delete($cohorts{$whole});
+			delete($count{$whole});
+		}
+
+		if ($line =~ /^\"<.*?>\".*$/) {
+			# Start with the new word
+			$whole = $line;
+			$anal_count = 0;
+		} else { cluck "Error, $line in wrong place."; }
+
+	} # LINE
+
+	close FH;
+}
+
+
 sub print_usage {
 	print "Usage: sum-cg [OPTIONS] FILE\n";
 	print "Summarize cg-output.\n";
 	print "Options\n";
+	print "--dir=<dir>     Search files from directory <dir>.\n";
 	print "--grammar       Compare only grammatical analyzes.\n";
 	print "--words         Print words associated with analyzes.\n";
     print "--help          Print the help text and exit.\n";
