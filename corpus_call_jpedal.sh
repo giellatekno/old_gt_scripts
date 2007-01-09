cfba37fb1a464daa53576c9c80228e17b55634ca
Saara Huhmarniemi <saara.huhmarniemi@helsinki.fi>
2007-01-09 13:22:24 +0000

    Modified so that the jpedal call can include lowering of the text area and exclusion of some pages.

--- corpus_call_jpedal.sh
+++ corpus_call_jpedal.sh
@@ -5,19 +5,13 @@ jpedalDir=/home/saara/src/jpedal
 
 mei=$@
 
-a=$(echo "$@" | grep '.*-D.*')
-if [ ! -z "$a" ]
-then 
-	twocol="-Dcol"
-fi
-
-echo $mei
+a=$(echo "$@" | perl -pe "s/(.*?)\-D/-D/")
 
 if [ -n "$1" -a -n "$2" ]
 then
 	file=$(echo $1 | sed -re "s/.*\///")
 	basename=$(echo $1 | sed -re "s/.*\/(.*)\.pdf/\1/")
-	java $twocol -Ddir=$2/$basename -Dxml -cp $libDir/bcprov-jdk14-119.jar:$libDir/jai_core.jar:$libDir/jai_codec.jar:$jpedalDir/os_jpedal.jar org/jpedal/examples/text/ExtractTextInRectangle $1
+	java $a -Ddir=$2/$basename -Dxml -cp $libDir/bcprov-jdk14-119.jar:$libDir/jai_core.jar:$libDir/jai_codec.jar:$jpedalDir/os_jpedal.jar org/jpedal/examples/text/ExtractTextInRectangle $1
 fi 
 
 
