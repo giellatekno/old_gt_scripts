9d06da781b2c06d88d6221d3fb637305349c3967
Saara Huhmarniemi <saara.huhmarniemi@helsinki.fi>
2007-03-13 11:57:24 +0000

    fixed the errors which were caused by cvs ci too early.

--- normalize.pl
+++ normalize.pl
@@ -17,9 +17,8 @@ while(<>) {
     my $file = File::Spec->rel2abs($_);
 	if ( -e "$_" ) {
 #		print "Fixing $file..\n";
-#		my $newname = NFC($file);  # Normalization Form C
+		my $newname = NFC($file);  # Normalization Form C
 
-		my $newname = 
 		if ( "$file" ne "$newname" ) {
 			if ( -e $newname ) {
 				print "Cannot move $file:  $newname exists already\n";
@@ -27,8 +26,8 @@ while(<>) {
 			}
 			print "mv $file $newname\n";
 			my @args=("mv", "$file", "$newname");
-#			system(@args) == 0
-#				or print "Cannot move $file: $!";
+			system(@args) == 0
+				or print "Cannot move $file: $!";
 		}
 	}
 }
