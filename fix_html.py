dfaa4c16c0bf8dcbb8d9775eddce79de88499863
Børre Gaup <albbas@gmail.com>
2007-03-28 22:13:24 +0000

    A short script to fix files fetched from minaigi.no. The files from minaigi.no contains a lot of unimportant text that we don't need. This script extracts only the article, and saves it to another file.

---
+++ fix_html.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+
+def fix_file(origname):
+    try:
+        origfile = open(origname)
+        newfile = open(origname + '.html', 'w')
+        inside_article = 0
+        
+        newfile.write('<html>\n<body>\n')
+        
+        for line in origfile:
+            if line.find('<table class="nyhet3"') != -1:
+                inside_article = 1
+            
+            if inside_article == 1:
+                if line.find('</table') != -1 :
+                    inside_article = inside_article - 1
+                newfile.write(line)
+    
+        origfile.close()
+        newfile.write('</body>\n</html>\n')
+        newfile.close()
+        
+    except IOError:
+        print "oops, couldn't open file " + origname
+        sys.exit(2)
+            
+    
+def usage():
+    print "This script fixes Min Áigi files."
+    print "It takes a file fetched from http://minaigi.no/ "
+    print "by the script fetchdailyminaigi.sh and extracts the article"
+    print "This is printed to the original filename + the .html extension"
+
+def main():
+    
+    if len(sys.argv) != 2:
+        # print help information and exit:
+        usage()
+        sys.exit(2)
+    else:
+        fix_file(sys.argv[1])
+
+   
+if __name__ == "__main__":
+    main()
+    
+    
