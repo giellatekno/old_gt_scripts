ca244138702554d9c446ba1420247269459c0892
Børre Gaup <albbas@gmail.com>
2007-04-12 12:46:36 +0000

    Added generation of numerals as specified in http://divvun.no/doc/admin/weekly/2007/Meeting_2007-04-10.html#Numbers

--- generate-plx-numerals.py
+++ generate-plx-numerals.py
@@ -6,40 +6,27 @@ import shutil
 import string
 import sys
 
-def generate_nums(lang,max_limit):
-	plxfile = open(os.path.join(os.path.expanduser("~"),"gt/" + lang + "/polderland/generated_nums-" + lang + "-plx.txt"),"w")
+def generate_nums(max_limit):
+	plxfile = open(os.path.join(os.path.expanduser("~"),"gt/common/polderland/generated_nums-plx.txt"),"w")
 	for x in range(1,max_limit + 1):
-		plxfile.write(`x` + '\t' + 'UI\n')
-		plxfile.write(`x` + '-' + '\t' + 'UL\n')
+		plxfile.write(`x` + '\t' + 'UILH\n')
 
 def usage():
 	print "This script generates numerals."
-	print "It needs two arguments, the language, which can be sme or smj"
-	print "and the upper limit for the numerals"
-	print "The result is printed to the file $HOME/gt/<lang>/polderland/generated_nums-<lang>-plx.txt"
-	print "Examples:"
-	print '\t', "generate-plx-numerals.py sme 1000"
+	print "It needs one arguments, the upper limit for the numerals"
+	print "The result is printed to the file $HOME/gt/common/polderland/generated_nums-plx.txt"
+	print "Example:"
 	print '\t', "generate-plx-numerals.py smj 1000000"
 
 
 def main():
-	if (len(sys.argv) != 3):
+	if (len(sys.argv) != 2):
 		# print help information and exit:
 		print "\nToo few arguments\n"
 		usage()
 		sys.exit(2)
-	if (sys.argv[1] != 'sme'):
-		# print help information and exit:
-		print "\nWrong language, has to be either sme or smj\n"
-		usage()
-		sys.exit(2)
-	if (sys.argv[1] != 'smj'):
-		# print help information and exit:
-		print "\nWrong language, has to be either sme or smj\n"
-		usage()
-		sys.exit(2)
 	else:
-		generate_nums(sys.argv[1], string.atoi(sys.argv[2]))
+		generate_nums(string.atoi(sys.argv[1]))
    
 if __name__ == "__main__":
     main()
