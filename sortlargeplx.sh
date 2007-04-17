89226ee7ad9ff2367f8583add14da364729b3c8b
Trond Trosterud <trond.trosterud@uit.no>
2007-04-17 09:11:16 +0000

    Correct file names and LANG=.

--- sortlargeplx.sh
+++ sortlargeplx.sh
@@ -2,10 +2,10 @@
 gunzip -c $1 | split -l 25000000 - large
 for i in large*
 do
-	sort -ru $i > $i-sorted
+	LANG= sort -ru $i > $i-sorted
 done
-sort -mru *-sorted > finishedlargefile
+LANG= sort -mru *-sorted > $2
 
 
-
+# Tomis alterntative
 # find ~/gt/smj/polderland -name 'large??' -print0 | xargs -0 -n1 sort -ru
