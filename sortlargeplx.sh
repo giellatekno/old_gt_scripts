ab1166ea8dbd485c423df4b47c16bb6976a3ba82
Sjur N Moshagen <sjurnm@mac.com>
2007-04-18 18:39:40 +0000

    Fixed a logical blunder (we were soring the num file after each number was added, whereas we only need to sort it once in the end).
    Added a lot of progress feedback comments.

--- sortlargeplx.sh
+++ sortlargeplx.sh
@@ -5,35 +5,51 @@
 # $3 = sorted output
 
 # 1 get the initial chars from sigma
+echo
+echo "*** Creating sigma-script: ***"
+echo
 printf "load stack < $2/bin/spellernonrec-$2.fst \n \
 sigma \n \
 quit \n " > tmp/sigma-script
 
 # 2 grep with the initial chars and the numbers, get the lines from bigfile, store to separate files
 # 3 for each separate file, sort, and store as -sorted
+echo
+echo "*** Grepping and sorting: ***"
+echo
 for i in `xfst -utf8 -f tmp/sigma-script -q | tr ' ' '\n' | grep '^.$' | egrep '[[:alpha:]]' | LANG= sort -ru` 
 do
+    echo "*** Grepping $i ***"
 	grep ^$i $1 > $2/int/$i-init.plx
+    echo "*** Sorting $i ***"
 	sort -ru $2/int/$i-init.plx > $2/int/$i-init-sorted.plx
 	rm -f $2/int/$i-init.plx
 done
 
 for i in 9 8 7 6 5 4 3 2 1 0
 do
+    echo "*** Grepping $i ***"
 	grep ^$i $1 > $2/int/num-init.plx
-	sort -ru $2/int/num-init.plx > $2/int/num-init-sorted.plx
 done
+echo "*** Sorting numbers ***"
+sort -ru $2/int/num-init.plx > $2/int/num-init-sorted.plx
 
 # Be sure to delete old garbage
 rm -f tmp/large-$2-sorted.plx
 
 # 4 concatenate the files, in sorted order, and store as total
+echo
+echo "*** Concatenating ***"
+echo
 for i in `xfst -utf8 -f tmp/sigma-script -q | tr ' ' '\n' | grep '^.$' | egrep '[[:alpha:]]' | LANG= sort -ru` 
 do
 	cat $2/int/$i-init-sorted.plx >> tmp/large-$2-sorted.plx
 done
 
 cat $2/int/num-init-sorted.plx >> tmp/large-$2-sorted.plx
+echo
+echo "*** File sorted! ***"
+echo
 
 
 # Børre pseudo:
