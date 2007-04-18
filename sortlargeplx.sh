28d854eab35bac007faf36216cd8afa351bd7def
Trond Trosterud <trond.trosterud@uit.no>
2007-04-18 15:21:36 +0000

    Rewrote the shellscript completely. The former version did split-sort-merge, i.e. at random points; the final merge showed the same errors in sorting as the old sort-it-all option. Now we sort according to initial symbol, and then concatenate the resulting subfiles, thereby avoiding sorting all lines in one operation.

--- sortlargeplx.sh
+++ sortlargeplx.sh
@@ -1,12 +1,38 @@
 #!/bin/sh
-split -l 25000000 $1 $2-large
-for i in $2-large*
+
+# $1 = large input
+# $2 = target
+# $3 = sorted output
+
+# 1 get the initial chars from sigma
+print "load stack < $2/bin/spellernonrec-$2.fst \n \
+sigma \n \
+quit \n " > tmp/sigma-script
+
+# 2 grep with the initial chars and the numbers, get the lines from bigfile, store to separate files
+# 3 for each separate file, sort, and store as -sorted
+for i in `xfst -utf8 -f tmp/sigma-script -q | tr ' ' '\n' | grep '^.$' | egrep '[[:alpha:]]' | LANG= sort -ru` 
 do
-	LANG= sort -ru $i > $2/int/$i-sorted
-	rm -f $i
+	grep ^$i $1 > $2/int/$i-init.plx
+	sort -ru $2/int/$i-init.plx > $2/int/$i-init-sorted.plx
+	rm -f $2/int/$i-init.plx
 done
-LANG= sort -mru $2/int/*-sorted > $3
+
+for i in 9 8 7 6 5 4 3 2 1 0
+do
+	grep ^$i $1 > $2/int/num-init.plx
+	sort -ru $2/int/num-init.plx > $2/int/num-init-sorted.plx
+done
+
+# Be sure to delete old garbage
+rm -f tmp/large-$2-sorted.plx
+
+# 4 concatenate the files, in sorted order, and store as total
+for i in `xfst -utf8 -f tmp/sigma-script -q | tr ' ' '\n' | grep '^.$' | egrep '[[:alpha:]]' | LANG= sort -ru` 
+do
+	cat $2/int/$i-init-sorted.plx >> tmp/large-$2-sorted.plx
+done
+
+cat $2/int/num-init-sorted.plx >> tmp/large-$2-sorted.plx
 
 
-# Tomis alterntative
-# find ~/gt/smj/polderland -name 'large??' -print0 | xargs -0 -n1 sort -ru
