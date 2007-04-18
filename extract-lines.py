22302f5361bacf8010652c41ded59ebf7e4c3dfb
Børre Gaup <albbas@gmail.com>
2007-04-18 19:16:54 +0000

    Python script to split huge plx files into letter based files

---
+++ extract-lines.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os,string,sys
+
+def main():
+    foundChars = []
+    openFiles = {}
+    sortFiles = {}
+    largefile = open(sys.argv[1])
+    
+    for line in largefile:
+        line = unicode(line, 'utf8')
+        if line[0] not in foundChars:
+            foundChars = foundChars + [line[0]]
+            #openFiles = 
+            openFiles[line[0]] = open(line[0] + '-init.plx', 'w')
+            sortFiles[line[0]] = open(line[0] + '-sorted-init.plx', 'w')
+        openFiles[line[0]].write(line.encode('utf8'))
+            
+     # rev, uniq sort the files, delete the old files
+     # add the file to the large file in the correct order
+    
+    foundChars.sort()
+    foundChars.reverse()
+    
+    charfile = open("charfile",'w')
+    for i in foundChars:
+        charfile.write( unicode(i + " ").encode('utf8'))
+
+
+if __name__ == "__main__":
+    main()
