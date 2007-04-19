401a2ef1e8ff6313a1679598c360f189022f630d
Børre Gaup <albbas@gmail.com>
2007-04-19 08:49:54 +0000

    Mac OS X is case insensitive, write files according to that. Also write out the sorted set of files to a file, to be used by mklexSami[Nort|Lule]

--- extract-lines.py
+++ extract-lines.py
@@ -7,26 +7,34 @@ def main():
     foundChars = []
     openFiles = {}
     largefile = open(sys.argv[1])
+    firstchar = ''
     
     for line in largefile:
         line = unicode(line, 'utf8')
-        if line[0] not in foundChars:
-            foundChars = foundChars + [line[0]]
-            #openFiles = 
-            openFiles[line[0]] = open(line[0] + '-init.plx', 'w')
+        firstChar = line[0]
+        
+        #
+         #   firstChar = firstChar.lower() + '_'
+            
+        if firstChar not in foundChars:
+            foundChars = foundChars + [firstChar]
+            if firstChar != firstChar.lower():
+                openFiles[firstChar] = open(firstChar.lower() + '_' + '-init.plx', 'w')
+            else:
+                openFiles[firstChar] = open(firstChar + '-init.plx', 'w')
             print line.encode('utf8')
+        
+        openFiles[firstChar].write(line.encode('utf8'))
             
-        openFiles[line[0]].write(line.encode('utf8'))
-            
-     # rev, uniq sort the files, delete the old files
-     # add the file to the large file in the correct order
-    
     foundChars.sort()
     foundChars.reverse()
     
-    charfile = open("charfile",'w')
+    filefile = open("filefile",'w')
     for i in foundChars:
-        charfile.write( unicode(i + " ").encode('utf8'))
+    	if i != i.lower():
+            filefile.write( unicode(i.lower() + "_-sorted-init.plx ").encode('utf8'))
+        else:
+            filefile.write( unicode(i + "_-sorted-init.plx ").encode('utf8'))
 
 
 if __name__ == "__main__":
