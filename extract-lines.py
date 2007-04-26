c18368cc800d58a3161c279da4f9b5ae3cbad22b
Børre Gaup <albbas@gmail.com>
2007-04-26 20:36:36 +0000

    this script now opens a list of files, and knows how to open gzipped files, too. this considerably lessens the complexity of the make target ms-win-speller.

--- extract-lines.py
+++ extract-lines.py
@@ -1,42 +1,46 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os,string,sys
+import os,string,sys,gzip
+
 
 def main():
-    foundChars = []
-    openFiles = {}
-    if (sys.argv[1] == '-'):
-	largefile = sys.stdin
-    else:
-    	largefile = open(sys.argv[1])
+	foundChars = []
+	openFiles = {}
+	for fname in sys.argv[1:]:
+		if (fname == '-'):
+			largefile = sys.stdin
+		elif (fname[-2:] == 'gz'):
+			largefile = gzip.GzipFile(fname)
+		else:
+			largefile = open(fname)
+		
+		firstchar = ''
+		
+		for line in largefile:
+			line = unicode(line, 'utf8')
+			firstChar = line[0]
+			
+			if firstChar not in foundChars:
+				foundChars = foundChars + [firstChar]
+				if firstChar != firstChar.lower():
+					openFiles[firstChar] = open('tmp/' + firstChar.lower() + '_' + '-init.plx', 'w')
+				else:
+					openFiles[firstChar] = open('tmp/' + firstChar + '-init.plx', 'w')
+				#print line.encode('utf8')
+			
+			openFiles[firstChar].write(line.encode('utf8'))
+			
+	foundChars.sort()
+	foundChars.reverse()
 	
-    firstchar = ''
-    
-    for line in largefile:
-        line = unicode(line, 'utf8')
-        firstChar = line[0]
-        
-        if firstChar not in foundChars:
-            foundChars = foundChars + [firstChar]
-            if firstChar != firstChar.lower():
-                openFiles[firstChar] = open(firstChar.lower() + '_' + '-init.plx', 'w')
-            else:
-                openFiles[firstChar] = open(firstChar + '-init.plx', 'w')
-            print line.encode('utf8')
-        
-        openFiles[firstChar].write(line.encode('utf8'))
-            
-    foundChars.sort()
-    foundChars.reverse()
-    
-    charfile = open("charfile",'w')
-    for i in foundChars:
-    	if i != i.lower():
-            charfile.write( unicode(i.lower() + '_ ').encode('utf8'))
-        else:
-            charfile.write( unicode(i + ' ').encode('utf8'))
+	charfile = open("charfile",'w')
+	for i in foundChars:
+		if i != i.lower():
+			charfile.write( unicode(i.lower() + '_ ').encode('utf8'))
+		else:
+			charfile.write( unicode(i + ' ').encode('utf8'))
 
 
 if __name__ == "__main__":
-    main()
+	main()
