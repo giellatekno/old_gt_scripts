a3607017aafcb892766ca10e92b220ec49ffcdb0
Sjur N Moshagen <sjurnm@mac.com>
2007-04-27 07:40:13 +0000

    Removed bare numbers from the generation - these are ignored by the spellers.

--- generate-plx-numerals.py
+++ generate-plx-numerals.py
@@ -10,11 +10,11 @@ import sys
 def generate_nums(max_limit):
 	plxfile = open(os.path.join(os.getcwd(),"common/polderland/generated_nums-plx.txt"),"w")
 	for x in range(1,max_limit + 1):
-		plxfile.write(`x` + '\t' + 'UIL\n')
+		# plxfile.write(`x` + '\t' + 'UIL\n')
 		plxfile.write(`x` + '--\t' + 'NL\n')
 
 def usage():
-	print "This script generates numerals."
+	print "This script generates numerals for compounding, like '10-feet'."
 	print "It needs one arguments, the upper limit for the numerals"
 	print "The result is printed to the file $HOME/gt/common/polderland/generated_nums-plx.txt"
 	print "Example:"
