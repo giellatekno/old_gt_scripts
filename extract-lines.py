2788478eb61e2ef735202203410fdf16819cddbd
Børre Gaup <albbas@gmail.com>
2007-04-29 20:37:00 +0000

    put lines from the big files into files according to the two first characters in each line. this way we will get smaller files, but the number of files will increase. the sorting should work better, though.

--- extract-lines.py
+++ extract-lines.py
@@ -18,23 +18,24 @@ def main():
 		else:
 			largefile = open(fname)
 		
-		firstchar = ''
+		firstChars = ''
 		count = 0
 		
 		for line in largefile:
 			count += 1
 			line = unicode(line, 'utf8')
-			firstChar = line[0]
+			firstChars = line[0:2]
+			print firstChars
 			
-			if firstChar not in foundChars:
-				foundChars = foundChars + [firstChar]
-				if firstChar != firstChar.lower():
-					openFiles[firstChar] = open('tmp/' + firstChar.lower() + '_' + '-init.plx', 'w')
+			if firstChars not in foundChars:
+				foundChars += [firstChars]
+				if firstChars[0] != firstChars[0].lower():
+					openFiles[firstChars] = open('tmp/' + firstChars[0].lower() + '_' + firstChars[1] + '-init.plx', 'w')
 				else:
-					openFiles[firstChar] = open('tmp/' + firstChar + '-init.plx', 'w')
+					openFiles[firstChars] = open('tmp/' + firstChars + '-init.plx', 'w')
 				#print line.encode('utf8')
 			
-			openFiles[firstChar].write(line.encode('utf8'))
+			openFiles[firstChars].write(line.encode('utf8'))
 			
 	foundChars.sort()
 	foundChars.reverse()
@@ -43,8 +44,8 @@ def main():
 	for i in foundChars:
 		if i == '-':
 			pass
-		elif i != i.lower():
-			charfile.write( unicode(i.lower() + '_ ').encode('utf8'))
+		elif i[0] != i[0].lower():
+			charfile.write( unicode(i[0].lower() + '_ ' + i[1]).encode('utf8'))
 		else:
 			charfile.write( unicode(i + ' ').encode('utf8'))
 
