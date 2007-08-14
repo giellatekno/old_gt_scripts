394cbee0f9bba346d9d1376c0cea2a9d320bc9a5
Saara Huhmarniemi <saara.huhmarniemi@helsinki.fi>
2007-08-14 06:15:42 +0000

    modified flag resolution and redirected jpedal output to /dev/null

--- corpus_call_jpedal.sh
+++ corpus_call_jpedal.sh
@@ -5,13 +5,12 @@ jpedalDir=/home/saara/src/jpedal
 
 mei=$@
 
-a=$(echo "$@" | perl -pe "s/(.*?)\-D/-D/")
+a=$(echo "$@" | perl -pe "if (/\-D/) { s/(.*?)\-D/-D/; } else {s/.*//; }")
 
 if [ -n "$1" -a -n "$2" ]
 then
 	file=$(echo $1 | sed -re "s/.*\///")
 	basename=$(echo $1 | sed -re "s/.*\/(.*)\.pdf/\1/")
-	java $a -Ddir=$2/$basename -Dxml -cp $libDir/bcprov-jdk14-119.jar:$libDir/jai_core.jar:$libDir/jai_codec.jar:$jpedalDir/os_jpedal.jar org/jpedal/examples/text/ExtractTextInRectangle $1
+	echo java $a -Ddir=$2/$basename -Dxml -cp $libDir/bcprov-jdk14-119.jar:$libDir/jai_core.jar:$libDir/jai_codec.jar:$jpedalDir/os_jpedal.jar org/jpedal/examples/text/ExtractTextInRectangle $1
+	java $a -Ddir=$2/$basename -Dxml -cp $libDir/bcprov-jdk14-119.jar:$libDir/jai_core.jar:$libDir/jai_codec.jar:$jpedalDir/os_jpedal.jar org/jpedal/examples/text/ExtractTextInRectangle $1 > /dev/null
 fi 
-
-
