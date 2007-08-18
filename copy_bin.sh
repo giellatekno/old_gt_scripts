95f1c2c9ca64f74e755dd22a5c86d5c7771041bc
Saara Huhmarniemi <saara.huhmarniemi@helsinki.fi>
2007-08-18 06:10:40 +0000

    changed the order of commands.

--- copy_bin.sh
+++ copy_bin.sh
@@ -13,12 +13,12 @@
 
 host=$(hostname)
 # Set variables according to the current host.
-if [ "$host" == "Hoavdas-Power-Mac-G5" ]
+if [ "$host" == "victorio.uit.no" ]
 then
+    tmpdir=/usr/tmp
+else
 	group=staff
 	tmpdir=/tmp
-else
-    tmpdir=/usr/tmp
 fi
 script_dir=$tmpdir/gt/script
 bindir=$tmpdir/gt/bin
@@ -35,15 +35,15 @@ binaries="ccat"
 scripts="preprocess lookup2cg text_cat cgi-export"
 
 # If we are in G5
-if [ "$host" == "Hoavdas-Power-Mac-G5" ]
+if [ "$host" == "victorio.uit.no" ]
 then 
-	perl_libdir=/Library/Perl/5.8.6
-	perl_libdir2=/sw/lib/perl5/5.8.6
-	cd $tmpdir && cvs -d :ext:victorio.uit.no:/usr/local/cvs/repository checkout gt
-else
 	perl_libdir=/usr/lib/perl5/5.8.8
 	echo "Check out gt from cvs"
 	cvs -d /usr/local/cvs/repository checkout -d $tmpdir/gt gt
+else
+	perl_libdir=/Library/Perl/5.8.6
+	perl_libdir2=/sw/lib/perl5/5.8.6
+	cd $tmpdir && cvs -d :ext:victorio.uit.no:/usr/local/cvs/repository checkout gt
 fi
 
 
@@ -72,8 +72,10 @@ for mod in $own_modules
 do
   echo "cp -r $script_dir/$mod $perl_libdir"
   sudo cp -r $script_dir/$mod $perl_libdir
-  if [ "$host" == "Hoavdas-Power-Mac-G5" ]
+  if [ "$host" == "victorio.uit.no" ]
 	  then 
+	  echo
+  else
 	  echo "cp -r $script_dir/$mod $perl_libdir2"
 	  sudo cp -r $script_dir/$mod $perl_libdir2
   fi
