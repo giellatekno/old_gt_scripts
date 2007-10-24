6e9039f5fb69ca1cf5f4a1d8d3643631ac048c40
Tomi Pieski <>
2007-10-24 11:13:07 +0000

    Commented out all but one case. Numbers in PLX format are now generated with java tool.

--- generate-plx-numerals.py
+++ generate-plx-numerals.py
@@ -9,27 +9,28 @@ import sys
 
 def generate_nums(lang, max_limit):
 	plxfile = open(os.path.join(os.getcwd(),lang + "/polderland/generated_nums-plx.txt"),"w")
-	if (lang == "sme"):
-		for x in range(1,max_limit + 1):
-			plxfile.write(`x` + '--\t' + 'NL\n')
-			plxfile.write(`x` + ':i' + '\t' + 'UI\n')
-			plxfile.write(`x` + ':s' + '\t' + 'UI\n')
-			plxfile.write(`x` + ':in' + '\t' + 'UI\n')
-			plxfile.write(`x` + ':n' + '\t' + 'UI\n')
-			plxfile.write(`x` + ':id' + '\t' + 'UI\n')
-			plxfile.write(`x` + ':ide' + '\t' + 'UI\n')
-			plxfile.write(`x` + ':iguin' + '\t' + 'UI\n')
-	else:
-		for x in range(1,max_limit + 1):
-			plxfile.write(`x` + '--\t' + 'NL\n')
-			plxfile.write(`x` + ':n' + '\t' + 'UI\n')
-			plxfile.write(`x` + ':v' + '\t' + 'UI\n')
-			plxfile.write(`x` + ':jv' + '\t' + 'UI\n')
-			plxfile.write(`x` + ':jt' + '\t' + 'UI\n')
-			plxfile.write(`x` + ':j' + '\t' + 'UI\n')
-			plxfile.write(`x` + ':jda' + '\t' + 'UI\n')
-			plxfile.write(`x` + ':js' + '\t' + 'UI\n')
-			plxfile.write(`x` + ':jn' + '\t' + 'UI\n')
+
+#	if (lang == "sme"):
+	for x in range(1,max_limit + 1):
+		plxfile.write(`x` + '--\t' + 'NL\n')
+#			plxfile.write(`x` + ':i' + '\t' + 'UI\n')
+#			plxfile.write(`x` + ':s' + '\t' + 'UI\n')
+#			plxfile.write(`x` + ':in' + '\t' + 'UI\n')
+#			plxfile.write(`x` + ':n' + '\t' + 'UI\n')
+#			plxfile.write(`x` + ':id' + '\t' + 'UI\n')
+#			plxfile.write(`x` + ':ide' + '\t' + 'UI\n')
+#			plxfile.write(`x` + ':iguin' + '\t' + 'UI\n')
+#	else:
+#		for x in range(1,max_limit + 1):
+#			plxfile.write(`x` + '--\t' + 'NL\n')
+#			plxfile.write(`x` + ':n' + '\t' + 'UI\n')
+#			plxfile.write(`x` + ':v' + '\t' + 'UI\n')
+#			plxfile.write(`x` + ':jv' + '\t' + 'UI\n')
+#			plxfile.write(`x` + ':jt' + '\t' + 'UI\n')
+#			plxfile.write(`x` + ':j' + '\t' + 'UI\n')
+#			plxfile.write(`x` + ':jda' + '\t' + 'UI\n')
+#			plxfile.write(`x` + ':js' + '\t' + 'UI\n')
+#			plxfile.write(`x` + ':jn' + '\t' + 'UI\n')
 
 
 def usage():
