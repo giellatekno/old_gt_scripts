de56f8b8e3e91cf0fcb70646413b647af28e1680
Sjur N Moshagen <sjurnm@mac.com>
2007-11-16 12:01:51 +0000

    Added some context to the replacement rule - the previous version over-applied.

--- exist11to12.pl
+++ exist11to12.pl
@@ -3,7 +3,7 @@
 # Perl script to change from eXist 1.1 to eXist 1.2
 while (<>) {
     if ( m/xi\:include/ ) {
-        s/\#/\" xpointer=\"/g ;
+        s/xml\#xpointer/xml\" xpointer=\"xpointer/g ;
     }
     print ;
 }
