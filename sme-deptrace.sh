dd986ce0f2e15afb8ee34f3609f4de5a8523a024
Trond Trosterud <trond.trosterud@uit.no>
2008-03-29 09:27:36 +0000

    Shell scripts for dependency analysis.

---
+++ sme-deptrace.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# cealkka3
+# This is a shell script for analysing Northern Sámi sentences.
+# It gives the analysis, but not the number of the rules used to disambiguate
+# It uses the vislcg3 parser.
+
+while [ 1 ]                                 # as long as there is input
+do                                          # run the following loop
+echo -n "Atte cealkaga (ctrl-C = STOP): "          # (message to user)
+read sentence                               # next 3 lines is the usual command
+echo $sentence | preprocess --abbr=~/gt/sme/bin/abbr.txt | \
+lookup -flags mbTT -utf8 ~/gt/sme/bin/sme.fst | ~/gt/script/lookup2cg | \
+vislcg3 --grammar $HOME/gt/sme/src/sme-dis.rle -C UTF-8 | \
+vislcg3 --grammar $HOME/gt/sme/src/sme-dep.rle -C UTF-8 --trace
+
+done                      
+exit 0
