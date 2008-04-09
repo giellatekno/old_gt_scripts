831e13bed2c4e34a4bdfa5d6128b274b5c556c0d
Trond Trosterud <trond.trosterud@uit.no>
2008-04-09 13:02:38 +0000

    Perl file to number disambiguation rules, thanks to Sjur for assistance.

---
+++ disrulecount.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl -w
+use encoding 'utf-8';
+
+my $i = 1 ;
+
+while (<>){
+
+if ( /^(MAP|ADD|SELECT|REMOVE) (.*)/ ) {
+	$_ =~ s/^(MAP|ADD|SELECT|REMOVE) (.*)/$1:r$i $2/;
+	$i++;
+	}
+print;
+}
+
+
+# File to add index to dis rules.
+# At present, it
+# does not name already named rules
