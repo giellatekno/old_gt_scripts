4dacea03312b5494cd67f09218c31502f6effa8f
Trond Trosterud <trond.trosterud@uit.no>
2008-04-23 19:26:49 +0000

    No abbr for sma.

--- sma-dis.sh
+++ sma-dis.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
 
 # sma-snt.sh
-# This is a shell script for analysing Northern Sámi sentences.
+# This is a shell script for analysing Southern Sámi sentences.
 # It gives the analysis, and the number of the rules used to disambiguate
 
 while [ 1 ]                                 # as long as there is input
 do                                          # run the following loop
-echo -n "Atte cealkaga: "                   # (message to user)
+echo -n "Skriv setning: "                   # (message to user)
 read sentence                               # next 3 lines is the usual command
-echo $sentence | preprocess --abbr=~/gt/sma/bin/abbr.txt | \
+echo $sentence | preprocess | \
 lookup -flags mbTT -utf8 ~/gt/sma/bin/sma.fst | ~/gt/script/lookup2cg | \
- vislcg3 -g ~/gt/sma/src/sma-dis.rle --trace  # no m4
-#dis.sh --grammar ~/gt/sma/bin/sma-dis.rle --minimal  # m4
+ vislcg3 -g ~/gt/sma/src/sma-dis.rle #--trace
+
 
 done                      
 exit 0
