4982c4f0f3fe7845c72501bd31d21c3c9969c6c4
Trond Trosterud <trond.trosterud@uit.no>
2008-09-20 16:05:07 +0000

    Obsolete

--- cgi-scripts/os-detect-test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#export HTTP_USER_AGENT="Linux"
-#./detect-os.pl
-# > test-input.out
-export QUERY_STRING="text=válddán in čuoččun"
-#export QUERY_STRING="in oainne"
-/usr/lib/cgi-bin/smi/utf8-sme-lookup.cgi
