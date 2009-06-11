83c66df39629237efaa7d6bc155e354ac5652453
Sjur N Moshagen <sjurnm@mac.com>
2009-06-11 06:28:10 +0000

    Several changes to complete the setup of the BIG working copy.

--- shfunctions.d/gtsetupbiggies.sh
+++ shfunctions.d/gtsetupbiggies.sh
@@ -2,6 +2,16 @@
 # GT setup functions to set up the big repository.
 # $Id$
 
+big_command_csh () {
+    BIGCMD="\
+setenv GTBIG $GTBIG"
+}
+
+big_command_sh () {
+    BIGCMD="\
+export GTBIG=$GTBIG"
+}
+
 msg_choose_big () {
     echo The Giellatekno code base also contain some rather big 
     echo files that are not required in most cases. They are helpful
@@ -12,27 +22,24 @@ msg_choose_big () {
     echo code block for you? It is about 500 Mb downloaded data,
     echo and will occupy roughly 1 Gb on your disk.
     echo The default is to NOT check out this part.
+    echo
+    echo NB!!! This checkout will take a LOT of TIME.
+    echo Just leave the computer running. A new dialog will
+    echo pop up when the checkout is finished. You can
+    echo use your computer for other tasks in the meantime,
+    echo "just don't turn it off or put it to sleep."
+    echo
     case $ONCONSOLE in
         YES)
     echo you can answer \\\"No\\\" here  
     echo and do it later manually.
     ;;
 	NO)
-    echo "you can answer \"No\" here and do it later manually."
+    echo "You can answer \"No\" here and do it later manually."
     ;;
     esac
     echo
     /bin/echo -n Continue\?
-}
-
-big_command_csh () {
-    BIGCMD="\
-setenv GTBIG $GTBIG"
-}
-
-big_command_sh () {
-    BIGCMD="\
-export GTBIG=$GTBIG"
 }
 
 display_choose_big () {
@@ -62,7 +69,7 @@ EOF
 }
 
 link_biggies () {
-	ln -s $GTBIG/gt/sme/corp $GTHOME/gt/sme/
+	ln -sf $GTBIG/gt/sme/corp $GTHOME/gt/sme/zcorp
 }
 
 display_choose_big_do () {
@@ -76,16 +83,26 @@ display_choose_big_do () {
 	    ;;
 	esac
     if [ "$answer" == "YES" ]; then
-#		svnco=`cd "$GTPARENT" && svn co https://victorio.uit.no/biggies/trunk big`
-		link_biggies
-		echo "" >> $HOME/$RC
-		echo "$BIGCMD" >> $HOME/$RC
-		if [ svnco == 0 ] ; then
-		    Result="\n The Biggies part of the Giellatekno resources \
+		if `cd $GTPARENT && svn co https://victorio.uit.no/biggies/trunk big` ; then
+    		link_biggies
+    		echo "" >> $HOME/$RC
+    		echo "$BIGCMD" >> $HOME/$RC
+            do_login_test
+            if grep GTBIG $TMPFILE >/dev/null 2>&1 ; then
+    		    Result="\n The Biggies part of the Giellatekno resources \
 have been checked out in $GTPARENT/big.\n\
 \n\
 I also added symbolic links within some language dirs to corpus \
 resources for testing purposes. Check out gt/LANG/zcorp/."
+            else
+                Result="The biggies part of the Giellatekno resources
+have been checked out in $GTBIG, but something went wrong when setting up \$GTBIG.
+
+Please add text equivalent to the
+following to your $RC file:
+
+export GTBIG=$GTBIG"
+            fi
 		else
 		    Result="\n
 Something went wrong when checking out the biggies
@@ -100,3 +117,96 @@ cd "$GTPARENT" && svn co https://victorio.uit.no/biggies/trunk big\n
     display_result
 }
 
+setup_big () {
+    if [ "$BIG_EXISTS" == "YES" ]; then
+        confirm_big_do
+    else
+        display_choose_big_do
+    fi
+}
+
+msg_confirm_big () {
+    echo It seems you already have checked out the big
+    echo repository at:
+    echo
+    echo "$GTBIG"
+    echo
+    echo Do you want me to set this path as the value of
+    echo the environmental variable \$GTBIG?
+    echo
+    case $ONCONSOLE in
+        YES)
+    echo You can answer \\\"No\\\" here  
+    echo and do it later manually.
+    ;;
+    NO)
+    echo "You can answer \"No\" here and do it later manually."
+    ;;
+    esac
+    echo
+    /bin/echo -n Continue\?
+}
+
+display_confirm_big () {
+    case $ONCONSOLE in
+        YES)
+# display choice popup
+   osascript <<-EOF
+   tell application "Finder"
+      activate
+      try
+        set dd to display dialog "`msg_title`\n\n`msg_confirm_big`\n" ¬
+        buttons {"No, thanks", "YES"} ¬
+        default button "YES" cancel button "No, thanks" ¬
+        giving up after 30
+        set UserResponse to button returned of dd
+      end try
+   end tell
+EOF
+   ;;
+    NO)
+# display choice dialog
+    msg_title; echo ""; echo ""
+    msg_confirm_big
+    /bin/echo -n " [Y/n] "
+    read answer
+    answer=`echo $answer | sed 's/^[yY].*$/y/'`
+    if [ ! -z "$answer" -a "x$answer" != "xy" ]; then
+       answer="NO"
+    fi
+    ;;
+    esac
+}
+
+
+confirm_big_do () {
+# propose to add existing priv dir as GTPRIV:
+    case $ONCONSOLE in
+        YES)
+        answer=`display_confirm_big`
+        ;;
+        NO)
+        display_confirm_big
+        ;;
+    esac
+    if [ "$answer" == "YES" ]; then
+        link_biggies
+        echo "$BIGCMD" >> $HOME/$RC
+        . $HOME/$RC
+        do_login_test
+        if grep GTBIG $TMPFILE >/dev/null 2>&1 ; then
+            Result="The big section of the Giellatekno
+            setup should be fine now."
+        else
+            Result="Something went wrong when setting up \$GTPRIV.
+
+Please add text equivalent to the
+following to your $RC file:
+        
+export GTBIG=$GTBIG"
+        fi
+    else
+        Result="OK, as you wish.\nYou are on your own. Good luck\n" 
+    fi
+    display_result
+}
