7e57441fb52ecffd4707576663fe54fd5db18df2
Sjur N Moshagen <sjurnm@mac.com>
2009-07-01 08:29:36 +0000

    Even more dialog tweaks.

--- shfunctions.d/gtsetupbiggies.sh
+++ shfunctions.d/gtsetupbiggies.sh
@@ -95,8 +95,9 @@ display_choose_big_do () {
     		    Result="\n The Biggies part of the Giellatekno resources
 have been checked out in $GTPARENT/big.\n\
 \n\
-I also added symbolic links within some language dirs to corpus \
-resources for testing purposes. Check out gt/LANG/zcorp/.\n\n"
+I also added symbolic links within some language
+dirs to corpus resources for testing purposes.
+Check out gt/LANG/zcorp/.\n\n"
             else
                 Result="\nThe biggies part of the Giellatekno resources
 have been checked out in $GTBIG,
