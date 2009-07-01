38fc2e84b62e36abe53f8771c88188eb65378bd1
Sjur N Moshagen <sjurnm@mac.com>
2009-07-01 09:01:28 +0000

    And one more...

--- shfunctions.d/gtsetupcommondialogs.sh
+++ shfunctions.d/gtsetupcommondialogs.sh
@@ -213,8 +213,8 @@ msg_all_done () {
     echo makeall.sh - compile all transducers
     echo "preprocess textfile.txt | usme | less"
     echo "\t   - analyse textfile.txt with the North Sámi transducer"
-    echo "svnup\t - update all Giellatekno svn"
-    echo "\tworking copies in one batch, from whereever you are."
+    echo "svnup\t   - update all Giellatekno svn"
+    echo "\t\tworking copies in one batch, from whereever you are."
     echo
 }
 
