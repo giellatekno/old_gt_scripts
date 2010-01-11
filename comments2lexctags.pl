ee4515b764450bc77d5eb12aa5c315ffc0a6801d
Tomi Pieski <>
2010-01-11 10:21:55 +0000

    Initial start for converting comment tags into real lexc tags. Basically this is just a copy/paste from tag-inclusion.pl script.

---
+++ comments2lexctags.pl
@@ -0,0 +1,83 @@
+#!/usr/bin/perl -w
+
+use strict;
+use utf8;
+
+
+# comments2lexctags.pl
+# Perl script for parsing lexicon files for tags.
+# Usage: comments2lexctags.pl lexicon_file > output_file
+#
+# Transform tags in comments to the entry:
+# abskis0sa:abskis'sa GOAHTI "absciss N" ; !+SgNomCmp +SgGenCmp +PlGenCmp
+# +SgNomCmp+SgGenCmp+PlGenCmpabskis0sa:abskis'sa GOAHTI "absciss N" ; !+SgNomCmp +SgGenCmp +PlGenCmp
+#
+# $Id$
+
+my $file_name = $ARGV[$#ARGV];
+my $inroot = 0;
+if ($file_name =~ /\/sm[aej]-lex./) { $inroot = 1; }
+
+my $root_comments = "";
+
+if ( $inroot ) {
+    # Skip the definitions in the beginning of the file
+    # Start processing the first lexicon
+    while(<>) {
+        print;
+        last if (/LEXICON/);
+    }
+}
+
+while (<>) {
+
+	#Pitäisi hypätä tyhjien rivien yli
+    if (/^\s*$/) { print; next; }
+
+	#Pitäisi hypätä rivien yli, jotka alkaa '!' -merkillä
+	# but allow whitespace chars in front of the !
+    if (/^\s*\!/) { print; next; }
+
+	if (/LEXICON/ && $inroot) {
+		if (! /\!.*\+/) { $root_comments = ""; print; next; }
+		else {
+			my ($entry, $comments) = split (/\!/, $_);
+			my @tagset = process_comments($comments);
+			$root_comments = join ("",@tagset);
+			print;
+			next;
+		}
+	}
+
+
+	if ((! /\!.*\+/) && ($root_comments eq "")) { print; next; }
+	chomp;
+	
+	my ($entry, $comments) = split (/\;/, $_);
+	my @tagset = process_comments($comments);
+	my $new_tags = join ("",@tagset);
+    $entry =~ s/^\s*//;
+	my $new_line = $root_comments . $new_tags . $entry . ";" . $comments . "\n";
+	print $new_line;
+}
+
+
+sub process_comments {
+	my $new_comments;
+	
+	if($inroot) {
+		($new_comments = @_) =~ s/\+/\+/g;
+	}
+	else {
+		($new_comments = @_) =~ s/\!//g;
+	}
+
+	my @strings = split(/\s+/,$new_comments);
+#	print "jee @strings";
+	my @tagset;
+	for my $t (@strings) {
+		if ($t =~ /^\+/) { push @tagset, $t; }
+	}
+	
+	return @tagset;
+}
