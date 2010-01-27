bd86efac3680fe200c49bb23cd6e5f67db27ce4d
Sjur N Moshagen <sjurnm@mac.com>
2010-01-27 11:18:00 +0000

    Added comments to clarify a bit what the code is. The code is presently incomplete, and won't work. Its origin is Odense, I have asked for the rest of the code.

--- niceline.pl
+++ niceline.pl
@@ -1,13 +1,20 @@
 #!/usr/bin/perl
 
+# From http://beta.visl.sdu.dk/cg3_howto.pdf:
+# condenses output to one-line cohorts
+
+# NB! Presently this script is useless, since we are missing the *.subs file.
+# Sjur has written to Eckhard Bick and asked about getting the missing parts.
+
+# $bindir = dir of niceline.pl
 my ($bindir) = $0 =~ /^(.*)\/.*/;
 
-eval(require "$bindir/niceline.perl.subs");
+eval(require "$bindir/niceline.pl.subs");
 if ($@) {
-    print "Couldn't load niceline.perl.subs!\n";
+    print "Couldn't load niceline.pl.subs!\n";
     return -1;
 }
 
-while(<STDIN>) {
+while(<>) {
     print Niceline($_);
 }
