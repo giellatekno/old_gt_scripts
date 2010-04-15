c68dabcebc3f31d80b73680e57d3b7bb0851ecb0
Sjur N Moshagen <sjurnm@mac.com>
2010-04-15 09:49:33 +0000

    Added debug printouts.

--- convert2xml.pl
+++ convert2xml.pl
@@ -131,6 +131,8 @@ if(! $tmpdir || ! -d $tmpdir) {
 my($dev, $ino, $mode, $nlink, $uid, $gid, $rdev,
     $size, $atime, $mtime, $ctime, $blksize, $blocks)
       = stat($tmpdir);
+print "gid er $gid\n";
+print "gt_gid er $gt_gid\n";
 if (! $gid == $gt_gid) {
     my $cnt = "chown -f :$gt_gid $tmpdir";
     my $comreturn = exec_com($cnt,"");
