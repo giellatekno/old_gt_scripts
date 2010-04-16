af129cc65f8778ca804e138258a0ba1afec96ede
Sjur N Moshagen <sjurnm@mac.com>
2010-04-16 10:48:19 +0000

    Debug statements.

--- langTools/Corpus.pm
+++ langTools/Corpus.pm
@@ -52,6 +52,7 @@ sub add_error_markup {
 			# No nested errors, no parentheses
 			if ($text =~ s/^([^$sep]*\s)?(?:\()?($plainerr)(?:\))?(?=$|\n|\s|\p{P})//) {
 				if($1) { push @new_content, $1; }
+				print STDERR "Plain error: $2\n";
 				get_error($2, \@new_content);
 			}
 
@@ -59,11 +60,13 @@ sub add_error_markup {
 				if ($1) { push @new_content, $1; }
 				my $tmp = $2;
 				(my $error = $tmp) =~ s/[\(\)]//g;
+				print STDERR "Complex error: $error\n";
 				get_error($error, \@new_content);
 				my $last_err = pop @new_content;
 				if ($text =~ s/^([$sep](?:\()?[^$sep\\(\\)]+?)(?:\))?(?=$|\n|\s)//) {
 					my $tmp = $1;
 					(my $error = $tmp) =~ s/[\(\)]//g;
+					print STDERR "Another complex error: $error\n";
 					get_error($error, \@new_content, $last_err);
 				}
 			}
