49cdc70666f3a83a4c2d449f541c4f86a79e9fbe
Ciprian Gerstenberger <ciprian.gerstenberger@gmail.com>
2010-04-18 22:32:46 +0000

    renamed sent-dis to loop-sent-disamb

--- loop-sent-disamb.sh
+++ loop-sent-disamb.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# sent-dis.sh
+# loop-sent-disamb.sh
 # This is a shell script for analysing (at the moment, only) Northern Sámi sentences with the vislcg3 parser.
 # It gives the analysis, and optionally the number of the disambiguation rules.
 
@@ -13,30 +13,12 @@
 # export GT_HOME=/Users/<your_user_name>/Documents
 
 # todo: parametize for language!
-
-if [ `hostname` == 'victorio.uit.no' ]
-then
-    LOOKUP=/opt/sami/xerox/c-fsm/ix86-linux2.6-gcc3.4/bin/lookup
-else
-    LOOKUP=`which lookup`
-fi
-
-if [ "$1" = "-t" ]
-then
-    TRACE="--trace"
-else
-    TRACE=""
-fi
-
+# possible todo: this can be merged with sent-dep.sh they are almost the same
 
 while [ 1 ]                                 # as long as there is input
 do                                          # run the following loop
-echo "Atte cealkaga: "                   # (message to user)
-read sentence                               # next 3 lines is the usual command
-echo $sentence | preprocess --abbr=$GTHOME/gt/sme/bin/abbr.txt | \
-$LOOKUP -flags mbTT -utf8 $GTHOME/gt/sme/bin/sme.fst | $GTHOME/gt/script/lookup2cg | \
- vislcg3 -g $GTHOME/gt/sme/src/sme-dis.rle $TRACE  # no m4
-#dis.sh --grammar $GTHOME/gt/sme/bin/sme-dis.rle --minimal  # m4
-
+echo "Atte cealkaga: "                      # (message to user)
+read SENTENCE                               # next line calls the usual command which is the script sent-disamb.sh
+./sent-disamb.sh "${SENTENCE}" $1
 done                      
 exit 0
