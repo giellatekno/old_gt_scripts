427371606c2a6cf625be40b2baa99272d6aac673
Ciprian Gerstenberger <ciprian.gerstenberger@gmail.com>
2010-04-22 22:07:15 +0000

    script parametrized for processing step; changed name accordingly

--- sent-proc.sh
+++ sent-proc.sh
@@ -9,8 +9,7 @@
 # to output the number of disambiguation rules, too, use the parameter '-t'
 # parametized for language (sme as default)
 # input sentence either coming from the pipe or at the end in quotation marks
-
-# todo: finish parametrization for processing step: -s=pos, -s=dis, -s=dep
+# parametrized for processing step: -s=pos, -s=dis, -s=dep
 
 if [ `hostname` == 'victorio.uit.no' ]
 then
@@ -20,10 +19,6 @@ else
     LOOKUP=`which lookup`
     HLOOKUP='/opt/local/bin/hfst-optimized-lookup'
 fi
-
-
-
-
 
 # -l=sme|sma|fao|etc. => default=sme
 fl=$(echo "$@" | grep '\-l\=')
@@ -76,13 +71,7 @@ else
     abbr=""
 fi
 
-last_fl=$(echo "${@:${#@}}" | perl -ne 'if (/-l=.../) {print;}')
-last_fs=$(echo "${@:${#@}}" | perl -ne 'if (/-s=.../) {print;}')
-last_ft=$(echo "${@:${#@}}" | perl -ne 'if (/-t/) {print;}')
-
-
 # omorfi or not omorfi
-
 if [[ "$l" == fin ]]
 then 
 	MORPH="$HLOOKUP $GTHOME/kt/fin/bin/share/omorfi/mor-omorfi.cg.hfst.ol"
@@ -90,6 +79,9 @@ else
 	MORPH="$LOOKUP -flags mbTT -utf8 $GTHOME/$lg/$l/bin/$l.fst"
 fi
 
+last_fl=$(echo "${@:${#@}}" | perl -ne 'if (/-l=.../) {print;}')
+last_fs=$(echo "${@:${#@}}" | perl -ne 'if (/-s=.../) {print;}')
+last_ft=$(echo "${@:${#@}}" | perl -ne 'if (/-t/) {print;}')
 
 # if no params or last param is a flag then take input from the left (cat, echo, etc.)
 # else take the last param from the right
@@ -99,7 +91,6 @@ then
     sentence=$(cat -)
 else
     if [[ ! -z "$last_fl" ]] || [[ ! -z "$last_fs" ]] || [[  ! -z "$last_ft" ]] 
-
     then
 	sentence=$(cat -)
     else
@@ -107,14 +98,23 @@ else
     fi
 fi
 
-echo "$sentence" | \
-preprocess $abbr | \
-$MORPH | \
-$GTHOME/gt/script/lookup2cg | \
-vislcg3 -g $GTHOME/$lg/$l/src/$l-dis.rle $t
+pos_cmd="echo $sentence | preprocess $abbr | $MORPH | $GTHOME/gt/script/lookup2cg"
+dis_cmd=$pos_cmd" | vislcg3 -g $GTHOME/$lg/$l/src/$l-dis.rle $t"
+dep_cmd=$dis_cmd" | vislcg3 -g $GTHOME/gt/sme/src/sme-dep.rle $t"
 
-
-
-
-
+# processing step
+case $s in
+    pos) 
+	echo "... pos tagging ..."
+	echo $(echo $pos_cmd) | sh
+	;;
+    dis)
+	echo "... disambiguating ..."
+	echo $(echo $dis_cmd) | sh
+	;;
+    dep)
+	echo "... dependency parsing ..."
+	echo $(echo $dep_cmd) | sh
+	;;
+esac
 
