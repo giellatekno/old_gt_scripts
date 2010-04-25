5024ae3f2f89f3cc2a1b7c12eb9abc54ed06f85a
Ciprian Gerstenberger <ciprian.gerstenberger@gmail.com>
2010-04-25 21:54:56 +0000

    removed teaksta; added teaksta-notes to the main script

--- sent-proc.sh
+++ sent-proc.sh
@@ -144,3 +144,43 @@ case $s in
 	echo $(echo $dep_cmd) | sh
 	;;
 esac
+
+# Notes for further development:
+# ==============================
+
+# At the moment, there is a family of scripts for analysis, in addition to this one:
+# cealkka (sentence), sme-dis.sh (sentence, with rule nr), sme-multi.sh (sentence w/o disamb)
+#                     smj-dis.sh (sentence, with rule nr), smj-multi.sh (sentence w/o disamb)
+# With more lgs and more options this will develop into a wildernis.
+
+# What we want:
+# One script, with parametrised options along several paths:
+# What language, 
+# What kind of input (plain text, xml text, evt. other text formats as well)
+# What kind of output (disambiguated text with and without rule numbers, non-disambiguated text with and without syntactic tags
+# What kind of morphological transducers (standard (tolerant) or normative (restricted))
+
+# dis.sh (script for disambiguating sentences or text)
+# If given as
+# dis.sh filename
+# the script expects a text
+# If given as 
+# dis.sh 
+# (i.e., without file name), the script expects a sentence, and answers:
+# "Write a sentence and press ENTER. Terminate by pressing ctrl-C."
+
+# options:
+# -l <lang> what disambiguator to call for (sme as default?)
+# -n        gives the rule number (no rule numbers is default)
+# -m        gives the non-disambiguated output (only section 1 in the .rle file)
+# -norm     uses normative transducers for the morphological analysis,
+#           ie will not recognise any non-conformant spellings
+# -i <type> gives text type:
+#           xml  (default, calls for ccat with the same <lang> as given above)
+#           txt  (takes plain text as input, is basically the script we have now)
+#           doc  (if we bother doing this, it would call for ... | antiword -db | ccat | ...)
+#           odt  (openoffice documents could probably get the same treatment
+#           html (importing relevant code from convert2xml.pl)
+#           pdf  (importing relevant code from convert2xml.pl)
+# -o <type> visl (default is the standard vislcg output format)
+#           xml  (gives xml-tagget output)
