4be27f28a1054e32d02ba8bd4de5dde69b5a53a2
Børre Gaup <albbas@gmail.com>
2010-05-04 10:49:17 +0000

    Only one -jar is needed

--- tca2.sh
+++ tca2.sh
@@ -2,4 +2,4 @@
 
 # sentence alignment tool
 # source code in $GTHOME/tools/alignment-tools
-java -Xms512m -Xmx1024m -jar -jar /usr/local/share/tca2/alignment.jar $1 $2 $3 $4
+java -Xms512m -Xmx1024m -jar /usr/local/share/tca2/alignment.jar $1 $2 $3 $4
