9f61b88ad3a8df02fef42091d0104b7e6bdc9050
Børre Gaup <albbas@gmail.com>
2010-06-09 00:13:14 +0000

    A script that sets values in an corpus xsl file by giving them as commandline arguments

---
+++ change_xsl_generic.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Written by Børre Gaup <borre.gaup@samediggi.no>
+
+import sys
+import fileinput
+
+def usage():
+	print 'This is a script that changes empty values in an corpus xsl file'
+	print 'Call the program like this: change_xsl.py variable-value-pairs filename'
+	print 'This requires a odd number of args to the script'
+	print 'If a value contains a space, use "-chars around'
+	print 'e.g. change_xsl_generic.py sub_name "Jens Kristensen" sub_email jens.kristensen@samediggi.no kraken.html.xsl'
+
+# check that there is exactly one argument given to the program
+if ( len(sys.argv) % 2 ) != 0:
+	usage()
+	sys.exit()
+
+
+# Initiate an empty dict
+change_variables = {}
+
+# read in the variable-value pairs, add them in the dict
+for index in range(1, len(sys.argv) - 1, 2):
+	change_variables[sys.argv[index]] = sys.argv[index + 1]
+
+xsl_filename = sys.argv[len(sys.argv) - 1]
+	
+# Do an inline replacement of lines in the xsl file
+for line in fileinput.FileInput(xsl_filename, inplace = 1):
+	for key, value in change_variables.iteritems():
+		if line.find('"' + key + '"') != -1:
+			line = line.replace('\'\'', '\'' + value + '\'')
+	print line.rstrip()
