2f5edbfc25fffe546eafea517ee213d645a3ee76
Ciprian Gerstenberger <ciprian.gerstenberger@gmail.com>
2010-06-19 07:55:52 +0000

    refined the coloring for the non-disambiguated analysis trying to keep the same coloring

--- color_a.pl
+++ color_a.pl
@@ -1,17 +1,15 @@
 #!/usr/bin/perl -w
 $|=1; # output autoflush 
 
-$lang = $ARGV[0];
-
 while (<STDIN>) 
 {
-    # word form
-    s/^([^\s]+)([\t ]+)([^\+]+)(.+)$/<font color="red">$1<\/font>$2<font color="maroon">$3<\/font>$4/; 
-    if ($lang eq "sme") {
-      s/ (N|A|Adv|V|Pron|CS|CC|Adp|Po|Pr|Interj|Pcle|Num|Coll|Prop|Pers|Dem|Interr|Refl|Recipr|Rel|Indef|CLB)( [^\@]*)/ <font color="black"><b>$1<\/b>$2<\/font>/ ;
-
-
-    s/(\+)/<font color="grey">$1<\/font>/g; 
-  }
+    # no analysis
+    if (/[^ \s]+\+\?$/) {
+       s/^([^\s]+)([\t ]+)([^\+ \s]+)(\s+)(\+)(\?)$/<font color="red">$1<\/font>$2<font color="maroon">$3<\/font>$4<font color="grey">$5<\/font><font color="black"><b>$6<\/b><\/font>/; 
+    } else {
+    # analysis 
+       s/^([^\s]+)([\t ]+)([^\+]+)(\+)([^\+]+)(.*)$/<font color="red">$1<\/font>$2<font color="maroon">$3<\/font>$4<font color="black"><b>$5<\/b><\/font>$6/;
+       s/(\+)/<font color="grey">$1<\/font>/g;
+    }
     print;
 }
