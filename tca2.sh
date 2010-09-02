5caa0866ad6aca92b85b35bca95727ba71445944
Trond Trosterud <trond.trosterud@uit.no>
2010-09-02 20:08:25 +0000

    comments

--- tca2.sh
+++ tca2.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# $1 = -a 
+# $2 = anchor file
+# $3 = first input file
+# $4 = second input file
+
 # sentence alignment tool
 # source code in $GTHOME/tools/alignment-tools
 java -Xms512m -Xmx1024m -jar /usr/local/share/tca2/alignment.jar $1 $2 $3 $4
