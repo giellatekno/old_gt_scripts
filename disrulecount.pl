d275be9113461ad8b28f73d8313bb021606c1e99
Trond Trosterud <trond.trosterud@uit.no>
2010-09-02 19:29:26 +0000

    discussing

--- disrulecount.pl
+++ disrulecount.pl
@@ -6,7 +6,9 @@ my $i = 1 ;
 while (<>){
 
 if ( /^(\"<.*>\" )?(MAP|ADD|SELECT|REMOVE|SUBSTITUTE|IFF) (.*)/ ) {
-	$_ =~ s/^(\"<.*>\" )?(MAP|ADD|SELECT|REMOVE|SUBSTITUTE|IFF) (.*)/$1:r$i $2/;
+	$_ =~ s/^(\"<.*>\" )?(MAP|ADD|SELECT|REMOVE|SUBSTITUTE|IFF) (.*)/$2:r$i $3/;
+#	$_ =~ s/^(\"<.*>\" )?(MAP|ADD|SELECT|REMOVE|SUBSTITUTE|IFF) (.*)/$1$2:r$i $3/;
+##	$_ =~ s/^(\"<.*>\" )?(MAP|ADD|SELECT|REMOVE|SUBSTITUTE|IFF) (.*)/$1:r$i $2/;
 	$i++;
 	}
 print;
@@ -16,3 +18,6 @@ print;
 # File to add index to dis rules.
 # At present, it
 # does not name already named rules
+
+# and at present, it also does not include first part ($1)
+
