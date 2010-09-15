e6087dfb4593e5540b98a542af9a6b7b880be6bf
Børre Gaup <albbas@gmail.com>
2010-09-15 13:04:22 +0000

    Create an empty file before trying to append content to it

--- analyse-corpus.sh
+++ analyse-corpus.sh
@@ -9,6 +9,7 @@ mkdir $ANALYZE_DIR
 
 for LANG in sma sme smj
 do
+    touch $ANALYSED_DIR/$LANG-dep.txt
     for CORPUS in boundcorpus freecorpus
     do
         ccat -l $LANG -a -r /Users/hoavda/Public/corp/$CORPUS/converted/$LANG/admin/depts | preprocess --abbr=$GTHOME/gt/$LANG/bin/abbr.txt | lookup -flabs mbTT $GTHOME/gt/$LANG/bin/$LANG.fst | lookup2cg | vislcg3 -g $GTHOME/gt/$LANG/bin/$LANG-dis.bin | vislcg3 -g $GTHOME/gt/smi/bin/smi-dep.bin >> $ANALYSED_DIR/$LANG-dep.txt
