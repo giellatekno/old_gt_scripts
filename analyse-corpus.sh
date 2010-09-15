883cc788b922d26f02401134c6d500d8027d9452
Børre Gaup <albbas@gmail.com>
2010-09-15 12:36:35 +0000

    Script to analyse corpus on divvun.no

---
+++ analyse-corpus.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd $GTHOME
+SVNREVISION=`svn info | grep Revision | cut -f1 -d" "`
+
+ANALYSED_DIR="/Users/hoavda/Public/corp/analysed/`date +%Y-%m-%d`"
+
+mkdir $ANALYZE_DIR
+
+for LANG in sma sme smj
+do
+    for CORPUS in boundcorpus freecorpus
+    do
+        ccat -l $LANG -a -r /Users/hoavda/Public/corp/$CORPUS/converted/$LANG/admin/depts | preprocess --abbr=gt/$LANG/bin/abbr.txt | lookup -flabs mbTT gt/$LANG/bin/$LANG.fst | lookup2cg | vislcg3 -g gt/$LANG/bin/$LANG-dis.bin | vislcg3 -g gt/smi/bin/smi-dep.bin >> $ANALYSED_DIR/$LANG-dep.txt
+    done
+done
