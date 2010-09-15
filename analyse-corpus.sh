726faf3580bfe6a7ccffe147e8d33821360a6b1d
Børre Gaup <albbas@gmail.com>
2010-09-15 12:40:45 +0000

    Use $GTHOME

--- analyse-corpus.sh
+++ analyse-corpus.sh
@@ -11,6 +11,6 @@ for LANG in sma sme smj
 do
     for CORPUS in boundcorpus freecorpus
     do
-        ccat -l $LANG -a -r /Users/hoavda/Public/corp/$CORPUS/converted/$LANG/admin/depts | preprocess --abbr=gt/$LANG/bin/abbr.txt | lookup -flabs mbTT gt/$LANG/bin/$LANG.fst | lookup2cg | vislcg3 -g gt/$LANG/bin/$LANG-dis.bin | vislcg3 -g gt/smi/bin/smi-dep.bin >> $ANALYSED_DIR/$LANG-dep.txt
+        ccat -l $LANG -a -r /Users/hoavda/Public/corp/$CORPUS/converted/$LANG/admin/depts | preprocess --abbr=$GTHOME/gt/$LANG/bin/abbr.txt | lookup -flabs mbTT $GTHOME/gt/$LANG/bin/$LANG.fst | lookup2cg | vislcg3 -g $GTHOME/gt/$LANG/bin/$LANG-dis.bin | vislcg3 -g $GTHOME/gt/smi/bin/smi-dep.bin >> $ANALYSED_DIR/$LANG-dep.txt
     done
 done
