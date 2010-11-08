0399dce1c1b1751b43bce389a8e905592c0312c4
Børre Gaup <albbas@gmail.com>
2010-11-08 13:17:11 +0000

    Added translated-from

--- corpus/analyse-corpus.sh
+++ corpus/analyse-corpus.sh
@@ -6,10 +6,48 @@ SVNREVISION=`svn info | grep Revision | cut -f1 -d" "`
 
 ANALYSED_DIR="/Users/hoavda/Public/corp/analysed/`date +%Y-%m-%d`"
 
-mkdir $ANALYSED_DIR
+if [ -d $ANALYSED_DIR ]
+then
+	echo "removing content in $ANALYSED_DIR"
+	rm $ANALYSED_DIR/*
+else
+	mkdir $ANALYSED_DIR
+fi
 
 for SMILANG in sma sme smj
 do
+	# First ccat all texts
+	echo "ccat'ing all $SMILANG xml files"
+    for CORPUS in boundcorpus freecorpus
+    do
+        
+        cd /Users/hoavda/Public/corp/$CORPUS/converted/$SMILANG
+        for GENREDIR in `ls`
+        do
+#             touch $ANALYSED_DIR/$SMILANG-$GENREDIR-dep.txt
+            for f in `find $GENREDIR -type f`
+            do
+				# ccat makes nonvalid utf8 of some files, ignore them
+				ccat -a $f | iconv -f utf-8 -t utf-8 > /dev/null
+				if [ $? -eq 0 ]
+				then
+					TRANSLATED_FROM=`grep translated_from $f | cut -f2 -d'"'`
+					if [ "$TRANSLATED_FROM" == "" ]
+					then
+						CCAT_FILE="$ANALYSED_DIR/$SMILANG-$GENREDIR.ccat.txt"
+					else
+						CCAT_FILE="$ANALYSED_DIR/$SMILANG-$TRANSLATED_FROM-$GENREDIR.ccat.txt"
+					fi
+					touch $CCAT_FILE
+					echo "ccat'ing $f into $CCAT_FILE"
+					ccat -l $SMILANG -a $f >> $CCAT_FILE
+				else
+					echo "ccat made invalid utf8, ignoring $f"
+				fi
+			done
+        done
+    done
+
     cd $GTHOME/gt
     make TARGET=$SMILANG
     if [ $SMILANG == "sma" ]
@@ -19,23 +57,9 @@ do
         PREPROCESS="preprocess --abbr=$GTHOME/gt/$SMILANG/bin/abbr.txt"
     fi
 
-    for CORPUS in boundcorpus freecorpus
+	echo "analysing $SMILANG"
+    for i in $ANALYSED_DIR/$SMILANG*.ccat.txt
     do
-        # ccat makes nonvalid utf8 of some files, remove them
-        for f in `find /Users/hoavda/Public/corp/$CORPUS/converted -type f`
-        do
-            ccat -a $f | iconv -f utf-8 -t utf-8 > /dev/null
-            if [ ! $? -eq 0 ]
-            then
-                rm -v $f
-            fi
-        done
-        
-        for GENREDIR in `ls /Users/hoavda/Public/corp/$CORPUS/converted/$SMILANG`
-        do
-            touch $ANALYSED_DIR/$SMILANG-$GENREDIR-dep.txt
-            time ccat -l $SMILANG -a -r /Users/hoavda/Public/corp/$CORPUS/converted/$SMILANG/$GENREDIR > $ANALYSED_DIR/$SMILANG-$GENREDIR.ccat.txt
-            time cat $ANALYSED_DIR/$SMILANG-$GENREDIR.ccat.txt | $PREPROCESS | lookup -flags mbTT $GTHOME/gt/$SMILANG/bin/$SMILANG.fst | lookup2cg | vislcg3 -g $GTHOME/gt/$SMILANG/bin/$SMILANG-dis.bin | vislcg3 -g $GTHOME/gt/smi/bin/smi-dep.bin >> $ANALYSED_DIR/$SMILANG-$GENREDIR-dep.txt
-        done
-    done
+		time cat $i | $PREPROCESS 2> /dev/null | lookup -flags mbTT $GTHOME/gt/$SMILANG/bin/$SMILANG.fst | lookup2cg | vislcg3 -g $GTHOME/gt/$SMILANG/bin/$SMILANG-dis.bin | vislcg3 -g $GTHOME/gt/smi/bin/smi-dep.bin >> $ANALYSED_DIR/`basename $i .ccat.txt`.dep.txt
+	done
 done
