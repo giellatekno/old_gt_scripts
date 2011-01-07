b2421920a51b833053c6a50c2cefeb65e4da7495
Børre Gaup <albbas@gmail.com>
2011-01-07 23:39:29 +0000

    paratext2xml.pl makes bible.xml, add the .bible.xml conversion to the process

--- langTools/ParatextConverter.pm
+++ langTools/ParatextConverter.pm
@@ -6,8 +6,11 @@ use langTools::Preconverter;
 sub convert2intermediate {
 	my( $self ) = @_;
 
-	my $command = $self->{_corpus_script} . "/paratext2xml.pl --out \"" . $self->gettmp1() . "\" \"" . $self->getOrig() . "\"";
-	die("Wasn't able to convert " . $self->getOrig() . " to intermediate xml format") if $self->exec_com($command);
+	my $command = $self->{_corpus_script} . "/paratext2xml.pl --out \"" . $self->gettmp2() . "\" \"" . $self->getOrig() . "\" > /dev/null" ;
+	die("Wasn't able to convert " . $self->getOrig() . " to bible.xml format") if $self->exec_com($command);
+	
+	my $command = "bible2xml.pl --out \"" . $self->gettmp1() . "\" \"" . $self->gettmp2() . "\"";
+	die("Wasn't able to convert " . $self->gettmp2() . " to intermediate xml format") if $self->exec_com($command);
 	
 	return $self->gettmp1();
 }
