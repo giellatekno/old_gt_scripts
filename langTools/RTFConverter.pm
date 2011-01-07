c0bcc4d666b21d3b35067e1308e9569b8f944678
Børre Gaup <albbas@gmail.com>
2011-01-07 23:38:51 +0000

    Class that converts an RTF file to a basic giellatekno xml file

---
+++ langTools/RTFConverter.pm
@@ -0,0 +1,34 @@
+package langTools::RTFConverter;
+
+use langTools::Preconverter;
+@ISA = ("langTools::Preconverter");
+
+sub new {
+	my ($class, $filename, $test) = @_;
+
+	my $self = $class->SUPER::new($filename, $test);
+	$self->{ _converter_xsl } = $self->{_corpus_script} . "/xhtml2corpus.xsl";
+
+	bless $self, $class;
+	return $self;
+}
+
+sub getXsl {
+	my( $self ) = @_;
+	return $self->{_converter_xsl};
+}
+
+sub convert2intermediate {
+	my( $self ) = @_;
+
+	my $command = "unrtf --nopict --html " . $self->getOrig() . " | tidy -config " . $self->{_bindir} . "/tidy-config.txt -utf8 -asxml -quiet > " . $self->gettmp2();
+# 	die("Couldn't convert rtf doc to html") if 
+	$self->exec_com($command);
+	
+	$command = "xsltproc \"" . $self->getXsl() . "\" \"" . $self->gettmp2() . "\" > \"" . $self->gettmp() . "\"";
+	die("Wasn't able to convert " . $self->getOrig() . " to intermediate xml format") if $self->exec_com($command);
+	
+	return $self->gettmp1();
+}
+
+1;
