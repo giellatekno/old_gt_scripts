8c66b13adeb46f3202436e18eafb5988e8a366f7
Børre Gaup <albbas@gmail.com>
2011-01-20 12:12:40 +0000

    Add a check for the programs this lib depends on

--- langTools/Preconverter.pm
+++ langTools/Preconverter.pm
@@ -87,4 +87,92 @@ sub exec_com {
 	return $result;
 }
 
+sub check_dependencies {
+	my ($self) = @_;
+	
+	my $invalid_setup = 0;
+
+	if (qx{which antiword} eq "") {
+		print "Didn't find antiword\n";
+		print "Install it on Mac by issuing the command\n\n";
+		print "sudo port install antiword\n\n";
+		print "For Linux, issue one of these commands:\n";
+		print "Ubuntu/Debian: sudo apt-get install antiword\n";
+		print "Fedora/Red Hat/CentOS: sudo yum install antiword\n";
+		print "SUSE: sudo zypper install antiword\n\n";
+		$invalid_setup = 1;
+	}
+	if (qx{which xsltproc} eq "") {
+		print "Didn't find xsltproc\n";
+		print "Install it on Mac by issuing the command\n\n";
+		print "sudo port install libxslt\n\n";
+		print "For Linux, issue one of these commands:\n";
+		print "Ubuntu/Debian: sudo apt-get install xsltproc\n";
+		print "Fedora/Red Hat/CentOS: sudo yum install libxslt\n";
+		print "SUSE: sudo zypper install libxslt\n\n";
+		$invalid_setup = 1;
+	}
+	if (qx{which tidy} eq "") {
+		print "Didn't find tidy\n";
+		print "Install it on Mac by issuing the command\n\n";
+		print "sudo port install tidy\n\n";
+		print "For Linux, issue one of these commands:\n";
+		print "Ubuntu/Debian: sudo apt-get install tidy\n";
+		print "Fedora/Red Hat/CentOS: sudo yum install tidy\n";
+		print "SUSE: sudo zypper install tidy\n\n";
+		$invalid_setup = 1;
+	}
+	if (qx{which pdftotext} eq "") {
+		print "Didn't find pdftotext\n";
+		print "Install it on Mac by issuing the command\n\n";
+		print "sudo port install poppler\n\n";
+		print "For Linux, issue one of these commands:\n";
+		print "Ubuntu/Debian: sudo apt-get install poppler-utils\n";
+		print "Fedora/Red Hat/CentOS: sudo yum poppler-utils\n";
+		print "SUSE: sudo zypper install poppler-utils\n\n";
+		$invalid_setup = 1;
+	}
+	if (qx{which xmllint} eq "") {
+		print "Didn't find xmllint\n";
+		print "Install it on Mac by issuing the command\n\n";
+		print "sudo port install libxml2\n\n";
+		print "For Linux, issue one of these commands:\n";
+		print "Ubuntu/Debian: sudo apt-get install libxml2-utils\n";
+		print "Fedora/Red Hat/CentOS: sudo yum install libxml2\n";
+		print "SUSE: sudo zypper install libxml2\n\n";
+		$invalid_setup = 1;
+	}
+	if (qx{which unrtf} eq "") {
+		print "Didn't find unrtf\n";
+		print "Install it on Mac by issuing the command\n\n";
+		print "sudo port install unrtf\n\n";
+		print "For Linux, issue one of these commands:\n";
+		print "Ubuntu/Debian: sudo apt-get install unrtf\n";
+		print "Fedora/Red Hat/CentOS: sudo yum install unrtf\n";
+		print "SUSE: sudo zypper install unrtf\n\n";
+		$invalid_setup = 1;
+	}
+
+	if ("$ENV{'GTHOME'}" eq "") {
+		print "The environment variable GTHOME isn't set\n";
+		print "Run the script gtsetup.sh found in the same\n";
+		print "directory as this script.";
+		$invalid_setup = 1;
+	}
+
+	if (!-f "/bin/readlink") {
+		#This is not a Linux system, check for a usable readlink
+		if(!-f "/opt/local/bin/greadlink") {
+			print "You don't have the correct version of readlink.\n";
+			print "Install it issuing the command:\n\n";
+			print "sudo port install coreutils\n\n";
+			$invalid_setup = 1;
+		}
+	}
+
+	if ($invalid_setup) {
+		exit(-1);
+	}
+}
+
 1;
--- langTools/Preconverter.t
+++ langTools/Preconverter.t
@@ -12,21 +12,11 @@ BEGIN {
 }
 require_ok('langTools::Preconverter');
 
-#
-# Set a file name, try to make an instance of our object
-#
 my $doc_name = "$ENV{'GTBOUND'}/orig/sme/facta/RidduRiđđu-aviissat/Riddu_Riddu_avis_TXT.200910.svg";
 ok(my $converter = langTools::Preconverter->new($doc_name, 0));
 
+is($converter->check_dependencies(), "0", "Check if all the dependencies are there");
 isnt($converter->getDoclang(), "", "Check if doclang is empty");
-
-#
-# Test if the original is the given file name
-#
-is($converter->getOrig(), Encode::decode_utf8(Cwd::abs_path($doc_name)));
-
-#
-# Check if the random file name is made and is of the expected length
-#
-is(length($converter->getTmpFilebase()), '8');
+is($converter->getOrig(), Encode::decode_utf8(Cwd::abs_path($doc_name)), "Test if the original is the given file name");
+is(length($converter->getTmpFilebase()), '8', "Check if the random file name is made and is of the expected length");
 
