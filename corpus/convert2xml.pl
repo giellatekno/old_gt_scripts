03035ffaf6c08d6b700cd7fcb4fce053d5994848
Børre Gaup <albbas@gmail.com>
2011-01-31 09:43:27 +0000

    Add some statistics to find out where most of the errors occur

--- corpus/convert2xml.pl
+++ corpus/convert2xml.pl
@@ -18,6 +18,17 @@ GetOptions ("debug" => \$debug,
 my $counter = 0;
 my $errors = 0;
 
+# error hash
+my %error_hash = ( 
+	"xsl" => 0,
+	"intermediate" => 0,
+	"convert2xml" => 0,
+	"checklang" => 0,
+	"checkxml_after_checklang" => 0,
+	"character_encoding" => 0,
+	"faulty_chars" => 0,
+	"checkxml_after_faulty" => 0 );
+
 main(\@ARGV, $debug);
 
 sub main {
@@ -32,8 +43,7 @@ sub main {
 			convertdoc($tmp);
 		}
 	}
-	print " $counter \nProcessing finished\n";
-	print "$counter files processed, $errors errors among them\n\n";
+	conclusion();
 }
 
 sub convertdoc2 {
@@ -55,27 +65,35 @@ sub convertdoc {
 			if ($converter->makeXslFile()) {
 				print STDERR "Conversion failed: Couldn't use " . $converter->getOrig() . ".xsl\n";
 				$error = 1;
+				$error_hash{"xsl"}++;
 			} elsif ($converter->convert2intermediatexml()) {
 				print STDERR "Conversion failed: Couldn't convert " . $converter->getOrig() . " to intermediate xml format\n";
 				$error = 1;
+				$error_hash{"intermediate"}++;
 			} elsif ($converter->convert2xml()) {
 				print STDERR "Conversion failed: Couldn't combine " . $converter->getOrig() . " and " . $converter->getOrig() . ".xsl\n";
 				$error = 1;
+				$error_hash{"convert2xml"}++;
 			} elsif ($converter->checklang()) {
 				print STDERR "Conversion failed: Couldn't set the lang of " . $converter->getOrig() . "\n";
 				$error = 1;
+				$error_hash{"checklang"}++;
 			} elsif ($converter->checkxml()) {
 				print STDERR "Conversion failed: Wasn't able to make valid xml out of " . $converter->getOrig() . "\n";
 				$error = 1;
+				$error_hash{"checkxml_after_checklang"}++;
 			} elsif ($converter->character_encoding()) {
 				print STDERR "Conversion failed: Wasn't able to set correct encoding of " . $converter->getOrig() . "\n";
 				$error = 1;
+				$error_hash{"character_encoding"}++;
 			} elsif ($converter->search_for_faulty_characters($converter->getInt())) {
 				print STDERR "Conversion failed: Found faulty chars in " . $converter->getInt() . "(derived from " . $converter->getOrig() . ")\n";
 				$error = 1;
+				$error_hash{"faulty_chars"}++;
 			} elsif ($converter->checkxml()) {
 				print STDERR "Conversion failed: Wasn't able to make valid xml out of " . $converter->getOrig() . "\n";
 				$error = 1;
+				$error_hash{"checkxml_after_faulty"}++;
 			} else {
 				$converter->move_int_to_converted();
 				if (! $debug ) {
@@ -100,3 +118,15 @@ sub convertdoc {
 	}
 }
 
+sub conclusion {
+	print " $counter \nProcessing finished\n";
+	print "$counter files processed, $errors errors among them\n";
+	print "The errors were distributed like this:\n";
+	foreach my $key (keys %error_hash) {
+		print "$key $error_hash{$key} ";
+		my $percents = sprintf "%.0f", $error_hash{$key}/$errors*100;
+		print $percents, "% of errors\n";
+	}
+	print "To find which files caused the errors, do the command\n";
+	print "grep \"Conversion failed\" tmp/*.log\n";
+}
