ddcf2e0eba6d53c58c91c01b117cb5f8890ff6ef
Børre Gaup <albbas@gmail.com>
2011-02-01 15:43:32 +0000

    Give a heads up if a file is empty. Give a clue where error messages come from

--- langTools/PlaintextConverter.pm
+++ langTools/PlaintextConverter.pm
@@ -9,26 +9,31 @@ sub convert2intermediate {
 	my( $self ) = @_;
 
 	my $error = 0;
-	my $encoding = &guess_text_encoding($self->getOrig(), $self->gettmp1(), $self->getDoclang());
 	
-	if ($encoding == -1) {
-		print STDERR "Couldn't guess encoding\n";
+	if (-s $self->getOrig() == 0) {
+		print STDERR "PlaintextConverter::convert2intermediate: " . File::Basename::basename($self->getOrig()) . " is an empty file\n";
 		$error = 1;
-	} elsif ($encoding) {
-		if (&decode_text_file($self->getOrig(), $encoding, $self->gettmp2())) {
-			print STDERR "Couldn't decode " . $self->getOrig() . " with encoding $encoding\n";
-			$error = 1;
-		} else {
-			langTools::Corpus::txtclean($self->gettmp2(), $self->gettmp1(), $self->getDoclang());
-		}
 	} else {
-		langTools::Corpus::txtclean($self->getOrig(), $self->gettmp1(), $self->getDoclang());
+		my $encoding = &guess_text_encoding($self->getOrig(), $self->gettmp1(), $self->getDoclang());
+		
+		if ($encoding == -1) {
+			print STDERR "Couldn't guess encoding\n";
+			$error = 1;
+		} elsif ($encoding) {
+			if (&decode_text_file($self->getOrig(), $encoding, $self->gettmp2())) {
+				print STDERR "Couldn't decode " . $self->getOrig() . " with encoding $encoding\n";
+				$error = 1;
+			} else {
+				langTools::Corpus::txtclean($self->gettmp2(), $self->gettmp1(), $self->getDoclang());
+			}
+		} else {
+			langTools::Corpus::txtclean($self->getOrig(), $self->gettmp1(), $self->getDoclang());
+		}
+		
+		unless ( $error ) {
+			$self->clean_doc();
+		}
 	}
-	
-	unless ( $error ) {
-		$self->clean_doc();
-	}
-	
 	return $error;
 }
 
@@ -40,11 +45,11 @@ sub clean_doc {
 		"\x1" => "",
 		"\x14" => "");
 	
-	open(FH, "<:encoding(utf8)", $self->gettmp1()) or die "Cannot open " . $self->gettmp1() . "$!";
+	open(FH, "<:encoding(utf8)", $self->gettmp1()) or die "clean_doc: Cannot open " . $self->gettmp1() . "$!";
 	my @file = <FH>;
 	close(FH);
 
-	open(FH, ">:encoding(utf8)", $self->gettmp1()) or die "Cannot open " . $self->gettmp1() . "$!";
+	open(FH, ">:encoding(utf8)", $self->gettmp1()) or die "clean_doc: Cannot open " . $self->gettmp1() . "$!";
 	foreach my $string (@file) {
 		foreach my $a (keys %replacements) {
 			my $ii = Encode::decode_utf8($a);
