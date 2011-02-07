def772340016b83157015a2b73088d04d0532692
Børre Gaup <albbas@gmail.com>
2011-02-07 13:51:07 +0000

    Moved check_dependencies to the main script Added a usage text if there are no arguments given

--- corpus/convert2xml.pl
+++ corpus/convert2xml.pl
@@ -37,16 +37,26 @@ main(\@ARGV, $debug);
 sub main {
 	my ($ref_to_argv, $debug) = @_;
 	my $numArgs = $#{$ref_to_argv} + 1;
-	print "Processing files\n";
-	foreach my $argnum (0 .. $#{$ref_to_argv}) {
-		my $tmp = ${$ref_to_argv}[$argnum];
-		if ( -d $tmp ) {
-			File::Find::find( \&convertdoc2, $tmp );
+	if ($numArgs > 1) { 
+		print "Processing files\n";
+
+		if (sanity_check()) {
+			print "Your dependencies aren't setup correctly.\n";
+			print "Follow the instructions above to be able to use this program\n";
 		} else {
-			convertdoc($tmp);
+			foreach my $argnum (0 .. $#{$ref_to_argv}) {
+				my $tmp = ${$ref_to_argv}[$argnum];
+				if ( -d $tmp ) {
+					File::Find::find( \&convertdoc2, $tmp );
+				} else {
+					convertdoc($tmp);
+				}
+			}
+			conclusion();
 		}
+	} else {
+		print_help();
 	}
-	conclusion();
 }
 
 sub convertdoc2 {
@@ -129,6 +139,92 @@ sub convertdoc {
 	}
 }
 
+sub sanity_check {
+	my ($self) = @_;
+	
+	my $invalid_setup = 0;
+
+	# This is the most important thing to set, exit right away if unset
+	if ("$ENV{'GTHOME'}" eq "") {
+		print "The environment variable GTHOME isn't set\n";
+		print "Run the script gtsetup.sh found in the same\n";
+		print "directory as this script.";
+		$invalid_setup = 1;
+	} else {
+		if (qx{which antiword} eq "") {
+			print "Didn't find antiword\n";
+			print "Install it on Mac by issuing the command\n\n";
+			print "sudo port install antiword\n\n";
+			print "For Linux, issue one of these commands:\n";
+			print "Ubuntu/Debian: sudo apt-get install antiword\n";
+			print "Fedora/Red Hat/CentOS: sudo yum install antiword\n";
+			print "SUSE: sudo zypper install antiword\n\n";
+			$invalid_setup = 1;
+		}
+		if (qx{which xsltproc} eq "") {
+			print "Didn't find xsltproc\n";
+			print "Install it on Mac by issuing the command\n\n";
+			print "sudo port install libxslt\n\n";
+			print "For Linux, issue one of these commands:\n";
+			print "Ubuntu/Debian: sudo apt-get install xsltproc\n";
+			print "Fedora/Red Hat/CentOS: sudo yum install libxslt\n";
+			print "SUSE: sudo zypper install libxslt\n\n";
+			$invalid_setup = 1;
+		}
+		if (qx{which tidy} eq "") {
+			print "Didn't find tidy\n";
+			print "Install it on Mac by issuing the command\n\n";
+			print "sudo port install tidy\n\n";
+			print "For Linux, issue one of these commands:\n";
+			print "Ubuntu/Debian: sudo apt-get install tidy\n";
+			print "Fedora/Red Hat/CentOS: sudo yum install tidy\n";
+			print "SUSE: sudo zypper install tidy\n\n";
+			$invalid_setup = 1;
+		}
+		if (qx{which pdftotext} eq "") {
+			print "Didn't find pdftotext\n";
+			print "Install it on Mac by issuing the command\n\n";
+			print "sudo port install poppler\n\n";
+			print "For Linux, issue one of these commands:\n";
+			print "Ubuntu/Debian: sudo apt-get install poppler-utils\n";
+			print "Fedora/Red Hat/CentOS: sudo yum poppler-utils\n";
+			print "SUSE: sudo zypper install poppler-utils\n\n";
+			$invalid_setup = 1;
+		}
+		if (qx{which xmllint} eq "") {
+			print "Didn't find xmllint\n";
+			print "Install it on Mac by issuing the command\n\n";
+			print "sudo port install libxml2\n\n";
+			print "For Linux, issue one of these commands:\n";
+			print "Ubuntu/Debian: sudo apt-get install libxml2-utils\n";
+			print "Fedora/Red Hat/CentOS: sudo yum install libxml2\n";
+			print "SUSE: sudo zypper install libxml2\n\n";
+			$invalid_setup = 1;
+		}
+		if (qx{which unrtf} eq "") {
+			print "Didn't find unrtf\n";
+			print "Install it on Mac by issuing the command\n\n";
+			print "sudo port install unrtf\n\n";
+			print "For Linux, issue one of these commands:\n";
+			print "Ubuntu/Debian: sudo apt-get install unrtf\n";
+			print "Fedora/Red Hat/CentOS: sudo yum install unrtf\n";
+			print "SUSE: sudo zypper install unrtf\n\n";
+			$invalid_setup = 1;
+		}
+
+		if (!-f "/bin/readlink") {
+			#This is not a Linux system, check for a usable readlink
+			if(!-f "/opt/local/bin/greadlink") {
+				print "You don't have the correct version of readlink.\n";
+				print "Install it issuing the command:\n\n";
+				print "sudo port install coreutils\n\n";
+				$invalid_setup = 1;
+			}
+		}
+	}
+	return $invalid_setup;
+}
+
 sub conclusion {
 	print " $counter \nProcessing finished\n";
 	print "$counter files processed, "; 
@@ -146,4 +242,11 @@ sub conclusion {
 	} else {
 		print "no errors encountered\n";
 	}
+}
+
+sub print_help {
+	print "Usage: convert2xml.pl [OPTIONS] [FILES|DIRS]\n";
+	print "The available options:\n";
+	print "    --debug    Print all the operations that are done when converting files to stderr\n";
+	print "    --shallow  Convert only files that haven't been converted before\n";
 }
