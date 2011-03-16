c0491d7926264290a4b5cf48366a51e0614ea4fb
Sjur N Moshagen <sjurnm@mac.com>
2011-03-16 19:34:56 +0000

    *This* is the correct symbol for the empty string.

--- regex2relabel.pl
+++ regex2relabel.pl
@@ -3,7 +3,7 @@ while (<>) {
   chomp;
   my ($target,$operator,$source)=split / /;
   $source =~ s/[,;%]//g;
-  $target =~ s/^0$/@ 0@/;
+  $target =~ s/^0$/\@_EPSILON_SYMBOL_@/;
   $target =~ s/[ %]//;
   print "$source\t$target\n";
 }
