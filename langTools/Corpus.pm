ed181a959ae4ae95f7f5307abc5e4998f6c24cdc
Børre Gaup <albbas@gmail.com>
2011-06-17 17:48:35 +0000

    Error markup parsing which handles nested markup

--- langTools/Corpus.pm
+++ langTools/Corpus.pm
@@ -28,13 +28,6 @@ our %types = ("\$" => "errorort",
 			   "§" => "error");
 
 our $sep = quotemeta("€§£\$¥");
-our $str = "[^$sep\\s\\(\\)]+?";
-our $str_par = "\\([^$sep\\(\\)]+?\\)";
-our $plainerr = "($str|$str_par)[$sep]($str|$str_par)";
-our $dummy1 = "($str|$str_par)( [$sep]|[$sep] | [$sep] )($str|$str_par)";
-our $dummy2 = "( [$sep]|[$sep] | [$sep] )($str|$str_par)";
-our $dummy3 = "( [$sep]|[$sep] | [$sep] )";
-# our $dummy4 = "[$sep]";
 
 my $test = $main::test;
 
@@ -45,192 +38,132 @@ sub add_error_markup {
 	my @new_content;
 	for my $c ($para->children) {
 		my $text = $c->text;
-        # separator: either §, $, €, ¥ or £
-		while ($text && $text =~ /[$sep]/) {
-			# No nested errors, no parentheses
-# 			if ($text =~ s/^([^$sep]*\s)?(?:\()?($dummy1)(?:\))?(?=$|\n|\s|\p{P})//) {
-# 				if ($1) {
-# 					push @new_content, $1;
-# 				}
-# 				if ($2) {
-# 					push @new_content, $2;
-# 				}
-# 			} elsif ($text =~ s/^([^$sep]*\s)?(?:\()?($dummy2)(?:\))?(?=$|\n|\s|\p{P})//) {
-# 				if ($1) {
-# 					push @new_content, $1;
-# 				}
-# 				if ($2) {
-# 					push @new_content, $2;
-# 				}
-# 			} elsif ($text =~ s/^([^$sep]*\s)?(?:\()?($dummy3)(?:\))?(?=$|\n|\s|\p{P})//) {
-# 				if ($1) {
-# 					print "dummy3 1, $1\n";
-# 					push @new_content, $1;
-# 				}
-# 				if ($2) {
-# 					print "dummy3 2, $1\n";
-# 					push @new_content, $2;
-# 				}
-# 			} elsif ($text =~ s/^([^$sep]*\s)?(?:\()?($dummy4)(?:\))?(?=$|\n|\s|\p{P})//) {
-# 				print "hit dummy4\n";
-# 				if($1) {
-# 					print "dummy4 1, $1\n";
-# 					push @new_content, $1;
-# 				}
-# 				if ($2) {
-# 					print "dummy4 2, $2\n";
-# 					push @new_content, $2;
-# 				}
-# 			} els
-			if ($text =~ s/^([^$sep]*\s)?(?:\()?($plainerr)(?:\))?(?=$|\n|\s|\p{P})//) {
-				if($1) { push @new_content, $1; }
-				if ($test) { print STDERR "Plain error: $2\n"; } # Debug print-out
-				get_error($2, \@new_content);
-			} elsif ($text =~ s/^([^$sep\(\)]*\s)?(?:\()($plainerr)(?=[$sep])//) {
-				if ($1) { push @new_content, $1; }
-				my $tmp = $2;
-				(my $error = $tmp) =~ s/[\(\)]//g;
-				if ($test) { print STDERR "Complex error: $error\n"; } # More debug output
-				get_error($error, \@new_content);
-				my $last_err = pop @new_content;
-				if ($text =~ s/^([$sep](?:\()?[^$sep\\(\\)]+?)(?:\))?(?=$|\n|\s)//) {
-					my $tmp = $1;
-					(my $error = $tmp) =~ s/[\(\)]//g;
-					if ($test) { print STDERR "Another complex error: $error\n"; } # Debug
-					get_error($error, \@new_content, $last_err);
-				}
-			} else {
-# 				print "\n***\n*** WARNING - NO MATCH: $text\n***\n\n";
-				push @new_content, $text;
-				$text ="";
-			}
-		}
-		if ($text) { push @new_content, $text; }
-		
+		@new_content = error_parser($text);
 	}
 	$para->set_content(@new_content);
 }
 
 
 sub get_error {
-	my ($error, $separator, $correct, $cont_ref, $first_err) = @_;
+	my ($error, $separator, $correct) = @_;
 
-		
-		# look for extended attributes:
-		my $extatt = 0;
-		my $attlist = "";
-		if ($correct =~ /\|/ ) {
-			$extatt = 1;
-			($attlist, $correct) = split(/\|/, $correct);
-			$attlist =~ s/\s//g;
-			if ($test) { print STDERR "Attribute list is: $attlist.\n"; }
+	# look for extended attributes:
+	my $extatt = 0;
+	my $attlist = "";
+	if ($correct =~ /\|/ ) {
+		$extatt = 1;
+		($attlist, $correct) = split(/\|/, $correct);
+		$attlist =~ s/\s//g;
+		if ($test) { print STDERR "Attribute list is: $attlist.\n"; }
 #			my $fieldnum = ($pos, errtype, teacher) = split(/,/, $attlist);
-		}
-
-		my $error_elt;
-		my $error_elt_name = "error";
-		if ($types{$separator}) { $error_elt_name = $types{$separator}; }
-		if ($first_err && ! $error) {
-			$error_elt = XML::Twig::Elt->new($error_elt_name=>{correct=>$correct});
-			$first_err->paste('last_child', $error_elt);
-		}
-		else {
-			$error_elt = XML::Twig::Elt->new($error_elt_name=>{correct=>$correct}, $error);
-		}
-		# Add extra attributes if found:
-		if ( $extatt ) {
-			# Add attributes for orthographical errors:
-			if ( $types{$separator} eq 'errorort') {
-				my ($pos, $errtype, $teacher) = split(/,/, $attlist);
-				if ( $errtype eq 'yes' || $errtype eq 'no' ) {
-					$teacher = $errtype;
-					$errtype = "";
-				} elsif ( $pos eq 'yes' || $pos eq 'no' ) {
-					$teacher = $pos;
-					$pos = "";
-					$errtype = "";
-				}
-				if ($pos)     { $error_elt->set_att('pos',     $pos); }
-				if ($errtype) { $error_elt->set_att('errtype', $errtype); }
-				if ($teacher) { $error_elt->set_att('teacher', $teacher); }
+	}
+	my $error_elt;
+	my $error_elt_name = "error";
+	if ($types{$separator}) { $error_elt_name = $types{$separator}; }
+	if (ref($error) eq 'XML::Twig::Elt') {
+		$error_elt = $error;
+		$error_elt->set_tag($error_elt_name);
+		$error_elt->set_att(correct => $correct);
+	} else {
+		$error_elt = XML::Twig::Elt->new($error_elt_name=>{correct=>$correct}, $error);
+	}
+	# Add extra attributes if found:
+	if ( $extatt ) {
+		# Add attributes for orthographical errors:
+		if ( $types{$separator} eq 'errorort') {
+			my ($pos, $errtype, $teacher) = split(/,/, $attlist);
+			if ( $errtype eq 'yes' || $errtype eq 'no' ) {
+				$teacher = $errtype;
+				$errtype = "";
+			} elsif ( $pos eq 'yes' || $pos eq 'no' ) {
+				$teacher = $pos;
+				$pos = "";
+				$errtype = "";
 			}
-			# Add attributes for lexical errors:
-			if ( $types{$separator} eq 'errorlex') {
-				my ($pos, $origpos, $errtype, $teacher) = split(/,/, $attlist);
-				if ( $errtype eq 'yes' || $errtype eq 'no' ) {
-					$teacher = $errtype;
-					$errtype = "";
-				} elsif ( $origpos eq 'yes' || $origpos eq 'no' ) {
-					$teacher = $origpos;
-					$origpos = "";
-					$errtype = "";
-				} elsif ( $pos eq 'yes' || $pos eq 'no' ) {
-					$teacher = $pos;
-					$pos = "";
-					$origpos = "";
-					$errtype = "";
-				}
-				if ($pos)     { $error_elt->set_att('pos',     $pos); }
-				if ($errtype) { $error_elt->set_att('errtype', $errtype); }
-				if ($teacher) { $error_elt->set_att('teacher', $teacher); }
-			}
-			# Add attributes for morphosyntactic errors:
-			if ( $types{$separator} eq 'errormorphsyn') {
-				my ($pos, $const, $cat, $orig, $errtype, $teacher) = split(/,/, $attlist);
-				if ( $errtype eq 'yes' || $errtype eq 'no' ) {
-					$teacher = $errtype;
-					$errtype = "";
-				} elsif ( $orig eq 'yes' || $orig eq 'no' ) {
-					$teacher = $orig;
-					$orig = "";
-					$errtype = "";
-				} elsif ( $cat eq 'yes' || $cat eq 'no' ) {
-					$teacher = $cat;
-					$cat = "";
-					$orig = "";
-					$errtype = "";
-				} elsif ( $const eq 'yes' || $const eq 'no' ) {
-					$teacher = $const;
-					$const = "";
-					$cat = "";
-					$orig = "";
-					$errtype = "";
-				} elsif ( $pos eq 'yes' || $pos eq 'no' ) {
-					$teacher = $pos;
-					$pos = "";
-					$const = "";
-					$cat = "";
-					$orig = "";
-					$errtype = "";
-				}
-				if ($pos)     { $error_elt->set_att('pos',     $pos); }
-				if ($const)   { $error_elt->set_att('const',   $const); }
-				if ($cat)     { $error_elt->set_att('cat',     $cat); }
-				if ($orig)    { $error_elt->set_att('orig',    $orig); }
-				if ($errtype) { $error_elt->set_att('errtype', $errtype); }
-				if ($teacher) { $error_elt->set_att('teacher', $teacher); }
-			}
-			# Add attributes for syntactic errors:
-			if ( $types{$separator} eq 'errorsyn') {
-				my ($pos, $errtype, $teacher) = split(/,/, $attlist);
-				if ( $errtype eq 'yes' || $errtype eq 'no' ) {
-					$teacher = $errtype;
-					$errtype = "";
-				} elsif ( $pos eq 'yes' || $pos eq 'no' ) {
-					$teacher = $pos;
-					$pos = "";
-					$errtype = "";
-				}
-				if ($pos)     { $error_elt->set_att('pos',     $pos); }
-				if ($errtype) { $error_elt->set_att('errtype', $errtype); }
-				if ($teacher) { $error_elt->set_att('teacher', $teacher); }
-			}
+			if ($pos)     { $error_elt->set_att('pos',     $pos); }
+			if ($errtype) { $error_elt->set_att('errtype', $errtype); }
+			if ($teacher) { $error_elt->set_att('teacher', $teacher); }
 		}
-		push (@$cont_ref, $error_elt);
-		print "$error_elt\n";
-	#else { print "NOT MATCH get_error: $text\n"; }
+		# Add attributes for lexical errors:
+		if ( $types{$separator} eq 'errorlex') {
+			my ($pos, $origpos, $errtype, $teacher) = split(/,/, $attlist);
+			if ( $errtype eq 'yes' || $errtype eq 'no' ) {
+				$teacher = $errtype;
+				$errtype = "";
+			} elsif ( $origpos eq 'yes' || $origpos eq 'no' ) {
+				$teacher = $origpos;
+				$origpos = "";
+				$errtype = "";
+			} elsif ( $pos eq 'yes' || $pos eq 'no' ) {
+				$teacher = $pos;
+				$pos = "";
+				$origpos = "";
+				$errtype = "";
+			}
+			if ($pos)     { $error_elt->set_att('pos',     $pos); }
+			if ($errtype) { $error_elt->set_att('errtype', $errtype); }
+			if ($teacher) { $error_elt->set_att('teacher', $teacher); }
+		}
+		# Add attributes for morphosyntactic errors:
+		if ( $types{$separator} eq 'errormorphsyn') {
+			my ($pos, $const, $cat, $orig, $errtype, $teacher) = split(/,/, $attlist);
+			if ( $errtype eq 'yes' || $errtype eq 'no' ) {
+				$teacher = $errtype;
+				$errtype = "";
+			} elsif ( $orig eq 'yes' || $orig eq 'no' ) {
+				$teacher = $orig;
+				$orig = "";
+				$errtype = "";
+			} elsif ( $cat eq 'yes' || $cat eq 'no' ) {
+				$teacher = $cat;
+				$cat = "";
+				$orig = "";
+				$errtype = "";
+			} elsif ( $const eq 'yes' || $const eq 'no' ) {
+				$teacher = $const;
+				$const = "";
+				$cat = "";
+				$orig = "";
+				$errtype = "";
+			} elsif ( $pos eq 'yes' || $pos eq 'no' ) {
+				$teacher = $pos;
+				$pos = "";
+				$const = "";
+				$cat = "";
+				$orig = "";
+				$errtype = "";
+			}
+			if ($pos)     { $error_elt->set_att('pos',     $pos); }
+			if ($const)   { $error_elt->set_att('const',   $const); }
+			if ($cat)     { $error_elt->set_att('cat',     $cat); }
+			if ($orig)    { $error_elt->set_att('orig',    $orig); }
+			if ($errtype) { $error_elt->set_att('errtype', $errtype); }
+			if ($teacher) { $error_elt->set_att('teacher', $teacher); }
+		}
+		# Add attributes for syntactic errors:
+		if ( $types{$separator} eq 'errorsyn') {
+			my ($pos, $errtype, $teacher) = split(/,/, $attlist);
+			if ( $errtype eq 'yes' || $errtype eq 'no' ) {
+				$teacher = $errtype;
+				$errtype = "";
+			} elsif ( $pos eq 'yes' || $pos eq 'no' ) {
+				$teacher = $pos;
+				$pos = "";
+				$errtype = "";
+			}
+			if ($pos)     { $error_elt->set_att('pos',     $pos); }
+			if ($errtype) { $error_elt->set_att('errtype', $errtype); }
+			if ($teacher) { $error_elt->set_att('teacher', $teacher); }
+		}
+	}
+	if ($test) {
+		print "160 ";
+		$error_elt->print;
+		print "\n";
+	}
+	
+	return $error_elt;
 }
-
 
 # Clean the output of an extracted pdf-file
 sub pdfclean {
@@ -457,85 +390,62 @@ sub txtclean {
 	close $FH1;
 }
 
-sub error_tester {
-	my ($expr) = @_;
-	my @new_content = undef;
-	my $continue_correction = 0;
-	my $error = undef;
-	my $separator = undef;
-	my $correction = undef;
-	my $parsing_finished = 0;
-	
-# 	print "expr $expr\n";
-	chomp($expr);
-	for my $tok (split('\s', $expr)) {
-		# Errors with word$sepword
-		if ($tok =~ m/(.*\w)([$sep])(\w.*)/) {
-			print "t1 $tok\n";
-			$error = $1;
-			$separator = $2;
-			$correction = $3;
-			$parsing_finished = 1;
-# 			get_error($error, $separator, $correct, \@new_content);
-			
-		}
-		# Errors with word$sep(expression)
-		elsif ($tok =~ m/(^\w.*\w)([$sep])(\(\w.*\w\))/) {
-			print "t2 $tok\n";
-			$error = $1;
-			$separator = $2;
-			$correction = $3;
-			$parsing_finished = 1;
-		}
-		# Errors with word$(start of expression end of expression)
-		elsif ($tok =~ m/(^\w.*\w)([$sep])(\(\w.*\w$)/) {
-			print "t3 $tok\n";
-			$error = $1;
-			$separator = $2;
-			$correction = $3;
-			$continue_correction = 1;
-			$parsing_finished = 0;
-		}
-		# continuation of the above
-		elsif ($continue_correction) {
-			$correction = $correction . " " . $tok;
-			if ($tok =~ m/(\w.*\))/) {
-				$continue_correction = 0;
-				print "t4 $correction\n";
-				$parsing_finished = 1;
-			}
-		} 
-		# Errors of this form 
-		# (álggahuvvon 1991) £ (num,advl,locsg,nomsg,case|álggahuvvon 1991:s)
-		elsif ($tok =~ m/(.*\))([$sep])(\(\w.*)/) {
-			print "t6 $tok\n";
-			$error = $1;
-			$separator = $2;
-			$correction = $3;
+sub error_parser {
+	my ($text) = @_;
 
-			my $tmp;
-			do {
-				$tmp = pop(@new_content); 
-				$error = $tmp . " " . $error;
-			} while ( $tmp !~ /^\(/);
-			$continue_correction = 1;
-			$parsing_finished = 0;
-		}
-		# A usual word
-		else {
-			print "t5 $tok\n";
-			push(@new_content, $tok);
-		}
-		if ($parsing_finished) {
-			push(@new_content, $error);
-			push(@new_content, $separator);
-			push(@new_content, $correction);
-			$parsing_finished = 0;
-		}
+	my $error;
+	my $separator;
+	my $correct;
+	my $rest;
+	my $error_elt;
+	my @new_content;
+	
+	if ($test) {
+		print "404 $text\n";
 	}
+
+	while ($text =~ m/\S[$sep]\S/) {
+		if ($text =~ s/(\S+|\([^\(]*\))([$sep])(\([^\)]*\))(.*)//s || $text =~ s/(\S+|\([^\(]*\))([$sep])(\S+)(.*)//s) {
+			$error = $1;
+			$separator = $2;
+			$correct = $3;
+			$rest = $4;
+			$error =~ s/\)//;
+			$error =~ s/\(//;
+			$correct =~ s/\)//;
+			$correct =~ s/\(//;
+			$error_elt = get_error($error, $separator, $correct);
+		}
+		
+		my @part1;
+		if ($rest =~ s/^\)//) {
+			$text =~ s/(\([^\)]*$)//;
+			my $e = $1;
+			$e =~ s/\(//;
+			push(@part1, $e);
+		}
+		
+		if ($rest =~ m/^[$sep]/) {
+			push(@part1, $error_elt);
+			
+			$error_elt = XML::Twig::Elt->new('dummy');
+			$error_elt->set_content(@part1);
+			while ($rest =~ s/(^[$sep])(\([^\)]*\))//) {
+				$separator = $1;
+				$correct = $2;
+				$correct =~ s/\)//;
+				$correct =~ s/\(//;
+				$error_elt = get_error($error_elt, $separator, $correct);
+			}
+		}
+		push(@new_content, $text);
+		push(@new_content, $error_elt);
+		$text = $rest;
+	}
+	push(@new_content, $text);
+	
 	return @new_content;
 }
-
 
 1;
 
