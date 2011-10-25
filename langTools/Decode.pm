5cd99c58cf0ed71c5684a7986a7b5ee4e50b88a6
Børre Gaup <albbas@gmail.com>
2011-10-25 16:24:39 +0000

    Don't react if Ä is found

--- langTools/Decode.pm
+++ langTools/Decode.pm
@@ -294,8 +294,8 @@ sub guess_encoding () {
 						$count++;
                     } elsif ($type eq "type09" && "," !~ $key) {
 						$count++;
-                    } elsif ($type eq "type08" && "[Šš]" !~ $key) {
-#                         print "default type $type, hit $key\n";
+                    } elsif ($type eq "type08" && "[ĐŠš]" !~ $key) {
+                        print "default type $type, hit $key\n";
                         $count++;
 					} elsif ($type eq "type01" || $type eq "type05" || $type eq "type07" || $type eq "type10" || $type eq "type11") {
 # 						print "default type $type, hit $key\n";
