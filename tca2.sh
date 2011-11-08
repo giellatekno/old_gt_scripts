bc7fa3fb8e2094e155ab0b6013c5f760f1f0b57d
Børre Gaup <albbas@gmail.com>
2011-11-08 14:27:54 +0000

    Make tca2 usable for more casual users than the real diehards

--- tca2.sh
+++ tca2.sh
@@ -6,4 +6,4 @@
 
 # sentence alignment tool
 # source code in $GTHOME/tools/alignment-tools
-java -Xms512m -Xmx1024m -jar /usr/local/share/tca2/alignment.jar -cli -anchor=$1 -in1=$2 -in2=$3
+java -Xms512m -Xmx1024m -jar $GTHOME/tools/alignment-tools/tca2/dist/lib/alignment.jar -cli -anchor=$1 -in1=$2 -in2=$3
