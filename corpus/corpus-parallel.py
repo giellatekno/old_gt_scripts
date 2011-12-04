1a9f1ca6b6a58dcd6d35b109c9561971990bfa08
Børre Gaup <albbas@gmail.com>
2011-12-04 11:21:02 +0000

    Doesn't need the usage bit

--- corpus/corpus-parallel.py
+++ corpus/corpus-parallel.py
@@ -9,17 +9,10 @@ sys.path.append(os.getenv('GTHOME') + '/gt/script/langTools')
 import parallelize
 
 
-def usage():
-    print 'This is a script that changes empty values in a corpus xsl file'
-    print 'Call the program like this: change_xsl.py variable-value-pairs filename'
-    print 'This requires an odd number of args to the script'
-    print 'If a value contains a space, use "-chars around it.'
-    print 'e.g. change_xsl_generic.py sub_name "Jens Kristensen" sub_email jens.kristensen@samediggi.no kraken.html.xsl'
-
 def parse_options():
-    parser = argparse.ArgumentParser(description='Sentence align two files. Input is the document containing the main language, and language to parallelize it with.')
-    parser.add_argument('input_file', help="The input file")
-    parser.add_argument('-p', '--parallel_language', dest='parallel_language', help="The language to parallelize the input document with", required = True)
+    parser = argparse.ArgumentParser(description = 'Sentence align two files. Input is the document containing the main language, and language to parallelize it with.')
+    parser.add_argument('input_file', help = "The input file")
+    parser.add_argument('-p', '--parallel_language', dest = 'parallel_language', help = "The language to parallelize the input document with", required = True)
     
     args = parser.parse_args()
     return args
