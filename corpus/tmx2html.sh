c2e9f8ad4b22f8df246399902f13f9926e1fff35
Berit Merete Nystad Eskonsipo <berit.nystad.eskonsipo@uit.no>
2012-02-10 09:50:24 +0000

    Fix the behaviour of this script

--- corpus/tmx2html.sh
+++ corpus/tmx2html.sh
@@ -1,4 +1,4 @@
-for file in `find $GTFREE/prestable/tmx -name *.tmx`
+for file in `find $GTFREE/prestable/tmx -name \*.tmx`
 do
     xsltproc $GTHOME/gt/script/corpus/tmx2html.xsl $file > $file.html
 done
