f2f7905c2789949b60069cc7963340c3d6c349d3
Sjur N Moshagen <sjurnm@mac.com>
2012-02-27 21:09:35 +0000

    Perl script to sort the analysis lines of CG-formatted cohorts. This can be used to ensure that the output of (partially) ambiguous morphological analysis stays consistent across invocations, versions and tools, and should be used to compare reference output with actual output, e.g. in regression and stability tests.

---
+++ sort-cg-cohort.pl
@@ -0,0 +1,60 @@
+#!/usr/bin/perl
+use strict;
+
+use utf8;
+
+# sort-cohort.pl
+# Perl script for sorting the analyses in a CG stream cohort. The main
+# purpose is to guarantee a consistent ordering of the analyses across
+# invocations, versions and tools, so that it is possible to make meaningful
+# diffs of the output of morphological analysis.
+#
+# This script differs from sort-cohort.pl in the type of input it takes:
+# instead of Xerox lookup-type input, where the cohorts are separated
+# by empty lines, and all analyses are preceeded with the lemma, and the
+# lemma only, it takes a stream of cohorts separated by non-blank first
+# characters.
+#
+# Input: 
+# "<Iđđes>"
+# 	 "iđđes" Adv
+# "<dii.>"
+# 	 "dii" N ABBR Nom
+# 	 "dii" N ABBR Acc
+# 	 "dii" N ABBR Attr
+# 	 "dii" N ABBR Gen
+#
+# Output:
+# "<Iđđes>"
+# 	 "iđđes" Adv
+# "<dii.>"
+# 	 "dii" N ABBR Acc
+# 	 "dii" N ABBR Attr
+# 	 "dii" N ABBR Gen
+# 	 "dii" N ABBR Nom
+
+# Set the record separator to newline + "
+$/ = "\n\"";
+
+# variable to identify the first cohort:
+my $counter = 0;
+
+# Read while not eol
+while(<>) {
+	chomp ;
+	my $cohort = "";
+	# Only add an initial " to the non-first cohort - since the first
+	# one has the initial " that is otherwise part of the separator, and
+	# gets removed; without this conditiona, the first cohort will get
+	# an initial double " - not nice:
+	if ($counter)
+		{ $cohort = "\"" . $_ ; }
+	else
+		{ $cohort = $_ ; }
+	my ($lemma, $lines) = split(/\n/, $cohort, 2);
+	my @lines = split(/\n/, $lines);
+	my @newlines = sort @lines;
+	my $sortlines = join("\n", @newlines);
+	print "$lemma\n$sortlines\n";
+	++$counter ;
+}
