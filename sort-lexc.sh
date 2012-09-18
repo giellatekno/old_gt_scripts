4fb88a54ed08ca1f99f6b459a0007cc81049f964
Sjur N Moshagen <sjurnm@mac.com>
2012-09-18 06:29:06 +0000

    A simple shell script to sort a block of lexc entries, including out-commented entries, in alphabetical order, such that after sorting, also the out-commented entries appear in their correct alphabetical location. NB! It does NOT sort whole lexc FILES (at least not yet). Be careful with what and when you sort, and always tell others before you start sorting, to allow them to commit first.

---
+++ sort-lexc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Sort a selection of lexc entries such that commented entries are kept at
+# their alphabetical position. Spaces at the beginning of lines are removed.
+#
+# NB!!! This script is NOT meant to be used on whole lexc files. It must be used
+# semi-automatically at present:
+#
+# 1. open your lexc file in an editor
+# 2. copy-paste the block of lexc entries to be sorted to another file
+# 3. sort the temporary file with lexc entries
+# 4. copy-paste back the block of sorted entries over the original block
+# 5. save the now sorted original file
+# 6. repeat for every block of lines you need to sort
+#
+
+sed 's/^ *//' | sed 's/^\(\!\{1,\}\)\(.*\)/\2QQQQQ\1/' | sort -f | sed 's/\(.*\)QQQQQ\(.*\)/\2\1/'
