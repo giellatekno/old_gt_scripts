0773fa011d34e56c5cd7839dfa16ed7ac0080dca
Børre Gaup <albbas@gmail.com>
2012-10-03 10:48:53 +0000

    If a word only has +Der analyses, then print it

--- find-non-lexicalised-words.sh
+++ find-non-lexicalised-words.sh
@@ -7,18 +7,11 @@
 for word in `cat $1`
 do
     ok=0
-    analisys=`echo $word | lookup -flags mbTT $GTHOME/gt/sme/bin/sme.fst 2> /dev/null | grep -v "?" | grep + | cut -f2 | cut -f1 -d'+' | sort -u`
-    for line in $analisys
-    do
-        if [ "$line" == "$word" ]
-        then
-            ok=1
-            break
-        fi
-    done
+    outword=""
+    analisys=`echo $word | lookup -flags mbTT $GTHOME/gt/sme/bin/sme.fst 2> /dev/null | grep -v "?" | grep -v +Der`
     
-    if [ "$ok" == "0" ]
+    if [ `echo ${#analisys}` == "0" ]
     then
-        echo $word
+        echo "$word"
     fi
 done
