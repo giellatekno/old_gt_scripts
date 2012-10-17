0fccf2f20df4bcd8f90999f8a4c29e06a40f6123
Børre Gaup <albbas@gmail.com>
2012-10-17 13:41:53 +0000

    Look for both toktmx and tmx files

--- corpus/tmx2html.sh
+++ corpus/tmx2html.sh
@@ -1,6 +1,6 @@
 for dir in toktmx tmx
 do
-    for file in `find $GTFREE/prestable/$dir -name \*.tmx`
+    for file in `find $GTFREE/prestable/$dir -name \*tmx`
     do
         xsltproc $GTHOME/gt/script/corpus/tmx2html.xsl $file > $file.html
     done
