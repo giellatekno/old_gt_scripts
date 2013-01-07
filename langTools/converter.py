7ea06aa1dd5642cba1a9f4b95c7c36c6583c7203
Børre Gaup <albbas@gmail.com>
2013-01-07 11:55:58 +0000

    Inserted copyright info

--- langTools/converter.py
+++ langTools/converter.py
@@ -1,5 +1,25 @@
 # -*- coding: utf-8 -*-
 
+#
+#   This file contains routines to convert files to the giellatekno xml
+#   format.
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this file. If not, see <http://www.gnu.org/licenses/>.
+#
+#   Copyright 2012 Børre Gaup <borre.gaup@uit.no>
+#
+
 import os
 import unittest
 
@@ -223,4 +243,3 @@ class PlaintextConverter(Preconverter):
         intermediate.append(self.handleContent())
         
         return intermediate
-    
