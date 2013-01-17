676bdc9f0cb62f5b7295e0148c5fc921239f2179
Børre Gaup <albbas@gmail.com>
2013-01-17 12:44:28 +0000

    Test vislcg3. Send one sentence at a time into vislcg3.

---
+++ test-vislcg3.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import subprocess
+import os
+
+def run_visglcg3(vislcg3input):
+
+    vislcg3command = ['vislcg3', '-g', os.path.join(os.getenv('GTHOME'), 'gt/sme/src/sme-dis.rle')]
+
+    subp = subprocess.Popen(vislcg3command, stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    (output, error) = subp.communicate(vislcg3input)
+
+    if subp.returncode == 0:
+        print 'all well'
+    else:
+        print 'all hell'
+        print output
+        print error
+
+def main():
+    f = open(sys.argv[1])
+
+    lines = []
+    clbno = 0
+    for line in f:
+        lines.append(line)
+        if '"." CLB' in line:
+            clbno = clbno + 1
+            vislcg3input = ''.join(lines)
+            clbfile = open(str(clbno), 'w')
+            clbfile.write(vislcg3input)
+            clbfile.close()
+            sys.stdout.write(str(clbno) + ' ' + str(len(lines)) + ' ')
+            sys.stdout.flush()
+            run_visglcg3(vislcg3input)
+            lines = []
+
+if __name__ == "__main__":
+    main()
