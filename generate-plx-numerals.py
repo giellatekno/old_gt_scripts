239482c8bd40a083984fc1f18a00db862e3e4ec5
Tomi Pieski <>
2013-01-24 15:55:52 +0000

    Using the python script again to generate arabic numerals for speller.

--- generate-plx-numerals.py
+++ generate-plx-numerals.py
@@ -8,48 +8,62 @@ import string
 import sys
 
 def generate_nums(lang, max_limit):
-	plxfile = open(os.path.join(os.getcwd(),lang + "/polderland/generated_nums-plx.txt"),"w")
+#	plxfile = open(os.path.join(os.getcwd(),lang + "/polderland/generated_nums-plx.txt"),"w")
 
 #	if (lang == "sme"):
 	for x in range(0,max_limit + 1):
-		plxfile.write(`x` + '\t' + 'JIBO,NePE\n')
-		plxfile.write(`x` + '--\t' + 'JtBX,JO\n')
-#			plxfile.write(`x` + ':i' + '\t' + 'UI\n')
-#			plxfile.write(`x` + ':s' + '\t' + 'UI\n')
-#			plxfile.write(`x` + ':in' + '\t' + 'UI\n')
-#			plxfile.write(`x` + ':n' + '\t' + 'UI\n')
-#			plxfile.write(`x` + ':id' + '\t' + 'UI\n')
-#			plxfile.write(`x` + ':ide' + '\t' + 'UI\n')
-#			plxfile.write(`x` + ':iguin' + '\t' + 'UI\n')
+		print(str(x) + '\t' + 'UIBOEX')
+		print(str(x) + '--\t' + 'UIBOEX')
+		print(str(x) + ':t' + '\t' + 'UIE')
+		print(str(x) + ':sis' + '\t' + 'UIE')
+		print(str(x) + ':siin' + '\t' + 'UIE')
+		print(str(x) + ':sii' + '\t' + 'UIE')
+		print(str(x) + ':sa' + '\t' + 'UIE')
+		print(str(x) + ':s' + '\t' + 'UIE')
+		print(str(x) + ':n' + '\t' + 'UIE')
+		print(str(x) + ':in' + '\t' + 'UIE')
+		print(str(x) + ':id' + '\t' + 'UIE')
+		print(str(x) + ':i-guin' + '\t' + 'UIE')
+		print(str(x) + ':i-de' + '\t' + 'UIE')
+		print(str(x) + ':i' + '\t' + 'UIE')
+		print(str(x) + ':dis' + '\t' + 'UIE')
+		print(str(x) + ':din' + '\t' + 'UIE')
+		print(str(x) + ':diin' + '\t' + 'UIE')
+		print(str(x) + ':diid-da' + '\t' + 'UIE')
+		print(str(x) + ':diid' + '\t' + 'UIE')
+		print(str(x) + ':dii' + '\t' + 'UIE')
+		print(str(x) + ':di-guin' + '\t' + 'UIE')
+		print(str(x) + ':dat' + '\t' + 'UIE')
+		print(str(x) + ':da' + '\t' + 'UIE')
 #	else:
 #		for x in range(1,max_limit + 1):
-#			plxfile.write(`x` + '--\t' + 'NL\n')
-#			plxfile.write(`x` + ':n' + '\t' + 'UI\n')
-#			plxfile.write(`x` + ':v' + '\t' + 'UI\n')
-#			plxfile.write(`x` + ':jv' + '\t' + 'UI\n')
-#			plxfile.write(`x` + ':jt' + '\t' + 'UI\n')
-#			plxfile.write(`x` + ':j' + '\t' + 'UI\n')
-#			plxfile.write(`x` + ':jda' + '\t' + 'UI\n')
-#			plxfile.write(`x` + ':js' + '\t' + 'UI\n')
-#			plxfile.write(`x` + ':jn' + '\t' + 'UI\n')
+#			print(str(x) + '--\t' + 'NL\n')
+#			print(str(x) + ':n' + '\t' + 'UIE\n')
+#			print(str(x) + ':v' + '\t' + 'UIE\n')
+#			print(str(x) + ':jv' + '\t' + 'UIE\n')
+#			print(str(x) + ':jt' + '\t' + 'UIE\n')
+#			print(str(x) + ':j' + '\t' + 'UIE\n')
+#			print(str(x) + ':jda' + '\t' + 'UIE\n')
+#			print(str(x) + ':js' + '\t' + 'UIE\n')
+#			print(str(x) + ':jn' + '\t' + 'UIE\n')
 
 
-def usage():
-	print "This script generates numerals for compounding, like '10-feet'."
-	print "It needs one arguments, the upper limit for the numerals"
-	print "The result is printed to the file $HOME/gt/$LANG/polderland/generated_nums-plx.txt"
-	print "Example:"
-	print '\t', "generate-plx-numerals.py smj 1000000"
+#def usage():
+#	print "This script generates numerals for compounding, like 10-feet."
+#	print "It needs one arguments, the upper limit for the numerals"
+#	print "The result is printed to the file $HOME/gt/$LANG/polderland/generated_nums-plx.txt"
+#	print "Example:"
+#	print '\t', "generate-plx-numerals.py smj 1000000"
 
 
 def main():
 	if (len(sys.argv) != 3):
 		# print help information and exit:
-		print "\nToo few arguments\n"
-		usage()
+		print ("\nToo few arguments\n")
+#		usage()
 		sys.exit(2)
 	else:
-		generate_nums(sys.argv[1], string.atoi(sys.argv[2]))
+		generate_nums(sys.argv[1], int(sys.argv[2]))
    
 if __name__ == "__main__":
     main()
