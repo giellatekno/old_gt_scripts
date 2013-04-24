c6cc2dc78052e7a30451db7f71f4c0b6e0c97267
Linda <32739027+lynnda-hill@users.noreply.github.com>
2013-04-24 15:00:37 +0000

    same script for undisambiguated text

---
+++ ct_nodis_homonymies.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3.2
+# -*- encoding: utf-8 -*-
+import sys ;
+ 
+wordpairs = [
+    "áibat,V,áibbašit,V",
+    "áitit,V,áiti,N",
+    "bassat,V,bassit,V",
+    "beassat,V,besset,V",
+    "čielgat,V,čilget,V",
+    "dassat,V,dat,Pron",
+    "doaibmat,V,doaibma,N",
+    "dustet,V,duostat,V",
+    "eavttuhit,V,eaktu,N",
+    "gierdat,V,girdit,V",
+    "gullat,V,gullet,V",
+    "justit,V,juste,Adv",
+    "lihkkat,V,lihkku,N",
+    "lihkkat,V,lihkká,Adv",
+    "loaktit,V,loktit,V",
+    "loaktit,V,loktet,V",
+    "mánnat,V,mánná,N",
+    "mannet,V,mannat,V",
+    "miehtat,V,miehtá,Adv",
+    "nuorrat,V,nuorra,A",
+    "ollit,V,ollu,Adv",
+    "ollásit,V,ollásit,Adv",
+    "salastit,V,salla,N",
+    "soahpat,V,sohpat,V",
+    "vuhttot,V,vuohttut,V",
+    "vuhttot,V,vuohttit,V"
+]
+ 
+counter = {}
+analysisCounter = {}
+ 
+# "<čilget>"         "čielgat" V IV Ind Prs Pl3 @+FMAINV          "čilget" V TV Ind Prs Sg2 @+FMAINV  
+ 
+ 
+# First we read through every line in the standard input.
+ 
+for line in sys.stdin.readlines(): #{
+ 
+        # For each of the wordpairs in the wordpair list
+        for wordpair in wordpairs: #{
+                row = wordpair.split(',');     
+ 
+                # Form the two analyses we are looking for.
+                analysis1 = row[0] + '+' + row[1]; # e.g. vuhttot+V
+                analysis2 = row[2] + '+' + row[3]; # e.g. vuohttit+V
+ 
+                # If we find both analyses in the same line
+                if line.count(analysis1) > 0 and line.count(analysis2) > 0: #{
+ 
+                        # If we haven't already seen this wordpair, then initialise the counter
+                        if wordpair not in counter: #{
+                                counter[wordpair] = 0;
+                        #}
+                       
+                        # Increment the counter by 1
+                        counter[wordpair] = counter[wordpair] + 1;
+                #}
+                
+                if analysis1 not in analysisCounter:
+                    analysisCounter[analysis1] = 0
+                    
+                analysisCounter[analysis1] = analysisCounter[analysis1] + line.count(analysis1)
+       
+                if analysis2 not in analysisCounter:
+                    analysisCounter[analysis2] = 0
+                    
+                analysisCounter[analysis2] = analysisCounter[analysis2] + line.count(analysis2)
+       
+        #}
+#}
+ 
+# For all the wordpairs we have seen, print out their values
+for wordpair in counter: #{
+ 
+        print(counter[wordpair], wordpair);
+#}
+for analysis in analysisCounter: #{
+ 
+        print(analysisCounter[analysis], analysis);
