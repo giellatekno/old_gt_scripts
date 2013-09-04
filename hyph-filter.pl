c7a5b788054bebf9b95d51ec3939942249eaa146
Børre Gaup <albbas@gmail.com>
2013-09-04 08:33:18 +0000

    White space changes

--- hyph-filter.pl
+++ hyph-filter.pl
@@ -4,7 +4,7 @@ use strict;
 
 use utf8;
 
-# hyph-filter.pl 
+# hyph-filter.pl
 # Perls script for cleaning the hyphenator output.
 # - reads one cohort at the time
 # - compares the hyphenated word to the orignal, disregarding ^ and #:
@@ -20,62 +20,62 @@ $/ = "";
 
 # Read while not eol
 INPUT:
-	while(<>) {	
-	
-		chomp;
-		my %lines;
-		@lines{split(/\n/, $_)} = ();
-		
-		my %forms;
-		
-		my $word;
-		my $hyph;
+    while(<>) {
 
-		for my $line (keys %lines) {
-			chomp;
+        chomp;
+        my %lines;
+        @lines{split(/\n/, $_)} = ();
 
-			($word, $hyph) = split(/\t/, $line);
-			next if (! $hyph);
+        my %forms;
 
-			if (scalar(keys %lines) == 1) { # and $word eq $hyph && $line !~ /\+\?/) {
-				$line =~ s/\s*\+\?//;
-				print $line; 
-				print "\n\n"; 
-				next INPUT; 
-			}
+        my $word;
+        my $hyph;
 
-			#remove word boundaries
-			$hyph =~ tr/\#//;
+        for my $line (keys %lines) {
+            chomp;
 
-			# take the upper case and lower case forms.
-			my $uc = ucfirst($hyph);
-			my $lc = lcfirst($hyph);
+            ($word, $hyph) = split(/\t/, $line);
+            next if (! $hyph);
 
-			# Take hyphenation point count.
-			my $count = ($hyph =~ tr/^//);
+            if (scalar(keys %lines) == 1) { # and $word eq $hyph && $line !~ /\+\?/) {
+                $line =~ s/\s*\+\?//;
+                print $line;
+                print "\n\n";
+                next INPUT;
+            }
 
-			# remove all the other wb:s
-			( my $cleaned = $hyph ) =~ tr/[^]//d;
-			
-		  TEST: {
-			  if ($word eq $cleaned) { $forms{$hyph} = $count; last TEST; }
-			  
-			  ( my $ucfirst = $uc ) =~ tr/[^]//d;
-			  if ($ucfirst eq $word) { $forms{$uc} = $count; last TEST; }
+            #remove word boundaries
+            $hyph =~ tr/\#//;
 
-			  ( my $lcfirst = $lc ) =~ tr/[^]//d;
-			  if ($lcfirst eq $word) { $forms{$lc} = $count; last TEST; }
-			  
-			  #If gone this far, then move forward.
-			  next;
-		  }
-		}
-		unless( %forms) { print STDERR "No forms for $word\n"; }
-		
-		# Print the output.
-		for my $key (sort { $forms{$b} <=> $forms{$a}} keys %forms) {
-			print "$word\t$key\n";
-			last;
-		}
-		print "\n";
-	} # while
+            # take the upper case and lower case forms.
+            my $uc = ucfirst($hyph);
+            my $lc = lcfirst($hyph);
+
+            # Take hyphenation point count.
+            my $count = ($hyph =~ tr/^//);
+
+            # remove all the other wb:s
+            ( my $cleaned = $hyph ) =~ tr/[^]//d;
+
+          TEST: {
+              if ($word eq $cleaned) { $forms{$hyph} = $count; last TEST; }
+
+              ( my $ucfirst = $uc ) =~ tr/[^]//d;
+              if ($ucfirst eq $word) { $forms{$uc} = $count; last TEST; }
+
+              ( my $lcfirst = $lc ) =~ tr/[^]//d;
+              if ($lcfirst eq $word) { $forms{$lc} = $count; last TEST; }
+
+              #If gone this far, then move forward.
+              next;
+          }
+        }
+        unless( %forms) { print STDERR "No forms for $word\n"; }
+
+        # Print the output.
+        for my $key (sort { $forms{$b} <=> $forms{$a}} keys %forms) {
+            print "$word\t$key\n";
+            last;
+        }
+        print "\n";
+    } # while
