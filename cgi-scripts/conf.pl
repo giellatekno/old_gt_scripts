2f41bd85742af223e18f8d8dafd831347bfb56ce
Trond Trosterud <trond.trosterud@uit.no>
2013-11-14 21:51:15 +0000

    Exchanged  varible with its value.

--- cgi-scripts/conf.pl
+++ cgi-scripts/conf.pl
@@ -202,7 +202,8 @@ sub init_variables {
 	if ($lang eq "sme") {
 	    $disamb = "$preprocess | $utilitydir/lookup $fstflags $fst | $bindir/lookup2cg | $bindir/vislcg3 -g $dis_bin -C UTF-8 | $bindir/vislcg3 -g $syn_rle -C UTF-8"; 
 	}
-	else { $disamb = "$analyze | $bindir/lookup2cg | $bindir/vislcg3 -g $dis_bin -C UTF-8";  }
+	else { $disamb = "$preprocess | $utilitydir/lookup $fstflags $fst | $bindir/lookup2cg | $bindir/vislcg3 -g $dis_bin -C UTF-8";  }
+#	else { $disamb = "$analyze | $bindir/lookup2cg | $bindir/vislcg3 -g $dis_bin -C UTF-8";  }
 
 	if ($lang eq "sme") {
 	    $dependency = "$preprocess | $utilitydir/lookup $fstflags $fst | $bindir/lookup2cg | $bindir/vislcg3 -g $dis_bin -C UTF-8 | $bindir/vislcg3 -g $syn_rle -C UTF-8 | $bindir/vislcg3 -g $dep_bin -C UTF-8"; 
