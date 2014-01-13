76922a5c11d37c6e95913c5a6e5be50f9cec888f
Børre Gaup <albbas@gmail.com>
2014-01-13 13:50:25 +0000

    Use new infra for building sme Analysed files are now placed in $CORPUS/analysed in the same style as converted ones, setup commands up accordingly

--- corpus/analyse.sh
+++ corpus/analyse.sh
@@ -15,40 +15,34 @@ module load automake/1.13.1
 cd $GTHOME
 svn up
 
-cd $GTHOME/gt
-make GTLANG=sme
-make GTLANG=sme abbr
-
-for lang in sma smj
+for lang in sma sme smj
 do
 	cd $GTHOME/langs/$lang
 	./autogen.sh
 	./configure
 	make
-done
 
-for corpus in $GTFREE $GTBOUND
-do
-	cd $corpus
-	svn up
-	make clean
-	make_corpus.py orig
+	for corpus in $GTFREE $GTBOUND
+	do
+		cd $corpus
+		svn up orig/$lang
+		make_corpus.py orig/$lang
+		time analyse_corpus.py --lang $lang converted/$lang 2> $HOME/$lang.log
+	done
 done
 
 for lang in sma sme smj
 do
-	ccatter.py $HOME/ccats/ $lang $GTFREE/converted/$lang $GTBOUND/converted/$lang
-	time analyse_corpus.py --lang $lang --analysisdir $HOME/analysed $GTFREE/converted/$lang $GTBOUND/converted/$lang 2> $HOME/$lang.log
+	#ccatter.py $HOME/ccats/ $lang $GTFREE/converted/$lang $GTBOUND/converted/$lang
 done
 
-find $GTFREE/converted/ -type f \! -name \*.xml | xargs rm
-find $GTBOUND/converted/ -type f \! -name \*.xml | xargs rm
 
-ssh boerre@divvun.no "rm -rf /Users/hoavda/Public/corp/boundcorpus/converted"
-ssh boerre@divvun.no "rm -rf /Users/hoavda/Public/corp/freecorpus/converted"
+# rsync -az $HOME/ccats boerre@divvun.no:/Users/hoavda/Public/corp/.
+for xmltype in converted analysed
+do
+	ssh boerre@divvun.no "rm -rf /Users/hoavda/Public/corp/boundcorpus/$xmltype"
+	rsync -az $GTFREE/$xmltype boerre@divvun.no:/Users/hoavda/Public/corp/freecorpus/.
 
-rsync -az $GTFREE/converted boerre@divvun.no:/Users/hoavda/Public/corp/freecorpus/.
-rsync -az $GTBOUND/converted boerre@divvun.no:/Users/hoavda/Public/corp/boundcorpus/.
-rsync -az $HOME/ccats boerre@divvun.no:/Users/hoavda/Public/corp/.
-rsync -az $HOME/analysed/* boerre@divvun.no:/Users/hoavda/Public/corp/analysed/.
-
+	ssh boerre@divvun.no "rm -rf /Users/hoavda/Public/corp/freecorpus/$xmltype"
+	rsync -az $GTBOUND/$xmltype boerre@divvun.no:/Users/hoavda/Public/corp/boundcorpus/.
+done
