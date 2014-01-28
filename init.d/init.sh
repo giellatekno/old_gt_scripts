4669ef18d4e504201124d3ba6bab39d90c7b7e14
Børre Gaup <albbas@gmail.com>
2014-01-28 10:53:10 +0000

    Add more paths. These are needed by CorpusTools on OS X

--- init.d/init.sh
+++ init.d/init.sh
@@ -103,6 +103,7 @@ else
   if [ -d /opt/local ]; then
     prepend_path PATH /opt/local/sbin
     prepend_path PATH /opt/local/bin
+    prepend_path PATH /opt/local/Library/Frameworks/Python.framework/Versions/2.7/bin
     export MANPATH=/opt/local/share/man:${MANPATH}
     export INFOPATH=/opt/local/share/info:${INFOPATH}
     export CPATH=/opt/local/include:/usr/local/include:/usr/include:${CPATH}
@@ -113,6 +114,7 @@ else
 
   PATH=/usr/local/bin:$PATH
   prepend_path PATH $HOME/.local/bin
+  prepend_path PATH $HOME/Library/Python/2.7/bin
   prepend_path PATH $HOME/bin
   prepend_path PATH $GTHOME/gt/script
   prepend_path PATH $GTHOME/gt/script/corpus
