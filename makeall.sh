05745519c5d6c341b82b81838e3e18d8a450c58a
Trond Trosterud <trond.trosterud@uit.no>
2014-04-01 07:44:40 +0000

    updated the makeall languages script.

--- makeall.sh
+++ makeall.sh
@@ -9,24 +9,28 @@ echo ""
 echo "===>     All the newinfra languages      <==="
 pushd $GTHOME/langs
 
+echo " -------------------------------------------------------------------------------------- bxr, Buriad "
+cd   bxr && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
 echo " -------------------------------------------------------------------------------------- ciw, Ojibwe "
-cd   ciw && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
-echo " -------------------------------------------------------------------------------------- crk, Plains Cree "
-cd ../crk && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
+cd ../ciw && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
 echo " -------------------------------------------------------------------------------------- cor, Cornish "
-cd ../cor && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
+cd ../cor && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
+echo " -------------------------------------------------------------------------------------- crk, Plains Cree "
+cd ../crk && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
 echo " -------------------------------------------------------------------------------------- est, Estonian "
-cd ../est && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
+cd ../est && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
 echo " -------------------------------------------------------------------------------------- fao, Faroese "
-cd ../fao && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
+cd ../fao && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
 echo " -------------------------------------------------------------------------------------- fin, Finnish "
-cd ../fin && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
-echo " -------------------------------------------------------------------------------------- fkv, Kven "
-cd ../fkv && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
+cd ../fin && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
+echo " -------------------------------------------------------------------------------------- hdn, Northern Haida "
+cd ../hdn && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
+echo " -------------------------------------------------------------------------------------- cor, Cornish "
+cd ../cor && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
 echo " -------------------------------------------------------------------------------------- ipk, Iñupiaq "
-cd ../ipk && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
+cd ../ipk && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
 echo " -------------------------------------------------------------------------------------- izh, Izhorian "
-cd ../izh && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
+cd ../izh && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
 echo " -------------------------------------------------------------------------------------- kal, Greenlandic "
 echo " "
 echo " No, sorry, we skip Greenlandic. It takes to long time to compile."
@@ -34,56 +38,68 @@ echo " In order to compile Greenlandic, do this:"
 echo " cd $GTHOME/langs/kal/"
 echo " ./autogen.sh -l && ./configure && make "
 echo " "
-#cd ../kal && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
+#cd ../kal && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
 echo " -------------------------------------------------------------------------------------- kca, Khanty "
-cd ../kca && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
+cd ../kca && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
 echo " -------------------------------------------------------------------------------------- kpv, Komi Zyryan "
-cd ../kpv && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
+cd ../kpv && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
 echo " -------------------------------------------------------------------------------------- liv, Livonian "
-cd ../liv && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
+cd ../liv && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
+echo " -------------------------------------------------------------------------------------- lut, Lushootseed "
+cd ../lut && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
 echo " -------------------------------------------------------------------------------------- mdf, Moksha "
-cd ../mdf && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
+cd ../mdf && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
 echo " -------------------------------------------------------------------------------------- mhr, Meadow Mari "
-cd ../mhr && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
+cd ../mhr && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
+echo " -------------------------------------------------------------------------------------- mns, Northern Mansi "
+cd ../mns && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
 echo " -------------------------------------------------------------------------------------- mrj, Hill Mari "
-cd ../mrj && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
+cd ../mrj && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
 echo " -------------------------------------------------------------------------------------- myv, Erzya "
-cd ../myv && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
+cd ../myv && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
 echo " -------------------------------------------------------------------------------------- ndl, Ndolo "
-cd ../ndl && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
+cd ../ndl && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
 echo " -------------------------------------------------------------------------------------- nio, Nganasan "
-cd ../nio && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
+cd ../nio && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
 echo " -------------------------------------------------------------------------------------- nob, Norwegian Bokmål "
-cd ../nob && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
+cd ../nob && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
 echo " -------------------------------------------------------------------------------------- olo, Olonetsian "
-cd ../olo && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
+cd ../olo && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
 echo " -------------------------------------------------------------------------------------- ron, Rumanian "
-cd ../ron && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
+cd ../ron && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
+echo " -------------------------------------------------------------------------------------- rup, Arumanian "
+cd ../rup && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
+echo " -------------------------------------------------------------------------------------- rus, Russian "
+cd ../rus && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
 echo " -------------------------------------------------------------------------------------- sjd, Kildin Saami "
-cd ../sjd && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
+cd ../sjd && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
 echo " -------------------------------------------------------------------------------------- sje, Pite Saami "
-cd ../sje && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
+cd ../sje && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
 echo " -------------------------------------------------------------------------------------- sma, South Saami "
-cd ../sma && pwd && nice ./autogen.sh -l && nice ./configure --enable-oahpa --with-hfst && nice time make   # adjectives.lexc
-#cd ../sma && pwd && nice ./autogen.sh -l && nice ./configure --enable-oahpa --with-hfst && nice time make  # adjectives-oahpa.lexc
+cd ../sma && pwd && nice ./autogen.sh -l && nice ./configure --enable-oahpa --with-hfst && nice time make HFST_LEXC_FLAGS=-F   # adjectives.lexc
+#cd ../sma && pwd && nice ./autogen.sh -l && nice ./configure --enable-oahpa --with-hfst && nice time make HFST_LEXC_FLAGS=-F  # adjectives-oahpa.lexc
 echo " -------------------------------------------------------------------------------------- smj, Lule Saami "
-cd ../smj && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
+cd ../smj && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
 echo " -------------------------------------------------------------------------------------- smn, Inari Saami "
-cd ../smn && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
+cd ../smn && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
 echo " -------------------------------------------------------------------------------------- sms, Skolt Saami "
-cd ../sms && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
+cd ../sms && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
 echo " -------------------------------------------------------------------------------------- som, Somali "
-cd ../som && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
+cd ../som && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
+echo " -------------------------------------------------------------------------------------- tat, Tatar "
+cd ../tat && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
+echo " -------------------------------------------------------------------------------------- tku, Upper Nekaxa Totonac "
+cd ../tku && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
 echo " -------------------------------------------------------------------------------------- udm, Udmurt "
-cd ../udm && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
+cd ../udm && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
 echo " -------------------------------------------------------------------------------------- vep, Veps "
-cd ../vep && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
+cd ../vep && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
 echo " -------------------------------------------------------------------------------------- vro, Võro "
-cd ../vro && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
+cd ../vro && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
 echo " -------------------------------------------------------------------------------------- yrk, Tundra Nenets "
-cd ../yrk && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
+cd ../yrk && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
 echo " -------------------------------------------------------------------------------------- zul, Zulu "
-cd ../zul && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
+cd ../zul && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
 
 
 # File to run all our compilation
@@ -206,9 +222,9 @@ nice time make
 
 
 # echo " -------------------------------------------------------------------------------------- tlh, Klingon "
-# cd ../tlh && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
+# cd ../tlh && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
 # echo " -------------------------------------------------------------------------------------- tuv, Turkana "
-# cd ../tuv && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
+# cd ../tuv && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make HFST_LEXC_FLAGS=-F 
 
 
 popd
