e57f424e7a64abeca3400874704fe52e7bdd7ba5
Børre Gaup <albbas@gmail.com>
2014-10-07 12:09:24 +0000

    Split apache log reader and report writer in two separate classe Replace getopt with argparse as commandline parser

--- read_logfiles.py
+++ read_logfiles.py
@@ -2,17 +2,14 @@
 # -*- coding: utf-8 -*
 """
 This script parses apache log files found in a given directory.
-It outputs a .jspwiki file, readable by forrest
-Run it like this:
-./read_logfiles.py <directory where apache log files are found> <outputfile>
+It outputs an xml file, readable by forrest
 """
-import sys
 import os
-import getopt
 import glob
 import gzip
 import datetime
 import lxml.etree as etree
+import argparse
 
 import GeoIP
 from logsparser.lognormalizer import LogNormalizer
@@ -20,7 +17,7 @@ normalizer = LogNormalizer('/usr/share/logsparser/normalizers')
 
 
 class DivvunApacheLogParser:
-    def __init__(self, args):
+    def __init__(self, log_directory, our_targets):
         """
         Initialize the variables the class needs to handle:
         self.log_directory: Where the log files are
@@ -31,16 +28,10 @@ class DivvunApacheLogParser:
         self.found_lists: List of lines that fill the criteria lined out
         further down
         """
-        self.log_directory, self.outfile = args
-        self.our_targets = {
-            'DivvunInstaller.exe': 'MSOffice/Windows',
-            'sami-proofing-tools.dmg': 'MSOffice/Mac',
-            'indesign-divvuntools.dmg': 'InDesign/Mac',
-            'smi-pack.zip': 'OpenOffice.org, pre 3.0',
-            'smi.oxt': 'OpenOffice.org 3.0',
-            'hunspell-se.tar.gz': 'Hunspell/Unix, Northern Sami',
-            'hunspell-smj.tar.gz': 'Hunspell/Unix, Lule Sami',
-            'smi.zip': 'Hunspell/Generic'}
+        self.found_lists = {}
+        for target in our_targets:
+            self.found_lists[target] = []
+        self.log_directory = log_directory
         self.bots = [
             'Yanga WorldSearch',
             'Yahoo! Slurp',
@@ -126,11 +117,87 @@ class DivvunApacheLogParser:
             'Browserlet',
             'Purebot'
             ]
-        self.found_lists = {}
-        for key in self.our_targets.keys():
-            self.found_lists[key] = []
-        self.get_max_and_min_date()
+
+    def set_mindate(self, date):
+        self.mindate = date
+
+    def set_maxdate(self, date):
+        self.maxdate = date
+
+    def is_bot(self, line):
+        """Check if the line contains one of the bots in self.bots
+        Tried to implement this check as a regex, but runtime
+        on a 100 000 line test access log went up to 18 seconds
+        from about 2.5 seconds with this implementation on my
+        test machine.
+        """
+        for bot in self.bots:
+            if bot in line:
+                return True
+
+        return False
+
+    def get_infile(self, filename):
+        if filename[-3:] == '.gz':
+            return gzip.open(filename)
+        else:
+            return open(filename)
+
+    def set_date(self, date):
+        if date < self.mindate:
+            self.set_mindate(date)
+        if date > self.maxdate:
+            self.set_maxdate(date)
+
+    def is_target(self, line):
+        for target in self.found_lists.keys():
+            if target in line:
+                return target
+
+    def parse_apachelog(self, filename):
+        for line in self.get_infile(filename):
+            target = self.is_target(line)
+            if (target is not None and self.is_bot(line) is False and
+                    ' 200 ' in line):
+                l = {'raw': line,
+                     'body': line}
+                normalizer.normalize(l)
+                self.found_lists[target].append(l)
+                self.set_date(l['date'])
+
+    def parse_apachelogs(self):
+        """
+        Go through all the access log files in a given directory. Pick out
+        the lines that has one our download goals, which has been fully fetched
+        and which hasn't been downloaded by a bot.
+        """
+        for access_file in glob.glob(
+                os.path.join(self.log_directory, '*access*')):
+            self.parse_apachelog(access_file)
+
+    def debug_input(self):
+        with open('debugfile', 'w') as debugfile:
+            for key in self.found_lists.keys():
+                [debugfile.write(line['raw'])
+                 for line in self.found_lists[key]]
+
+
+class DivvunLogHandler(object):
+    def __init__(self, log_directory, outfile):
+        self.outfile = outfile
+        self.our_targets = {
+            'DivvunInstaller.exe': 'MSOffice/Windows',
+            'sami-proofing-tools.dmg': 'MSOffice/Mac',
+            'indesign-divvuntools.dmg': 'InDesign/Mac',
+            'smi-pack.zip': 'OpenOffice.org, pre 3.0',
+            'smi.oxt': 'OpenOffice.org 3.0',
+            'hunspell-se.tar.gz': 'Hunspell/Unix, Northern Sami',
+            'hunspell-smj.tar.gz': 'Hunspell/Unix, Lule Sami',
+            'smi.zip': 'Hunspell/Generic'}
         self.report_file = etree.Element('document')
+        self.logparser = DivvunApacheLogParser(log_directory,
+                                          self.our_targets.keys())
+        self.get_max_and_min_date()
 
     def get_max_and_min_date(self):
         '''Set max and min date
@@ -139,13 +206,26 @@ class DivvunApacheLogParser:
         '''
         try:
             doc = etree.parse(self.outfile)
-            self.mindate = datetime.datetime.strptime(
-                doc.find('//em[@id="mindate"]').text, '%Y-%m-%d %H:%M:%S')
-            self.maxdate = datetime.datetime.strptime(
-                doc.find('//em[@id="maxdate"]').text,  '%Y-%m-%d %H:%M:%S')
+            self.logparser.set_mindate(datetime.datetime.strptime(
+                doc.find('//em[@id="mindate"]').text, '%Y-%m-%d %H:%M:%S'))
+            self.logparser.set_maxdate(datetime.datetime.strptime(
+                doc.find('//em[@id="maxdate"]').text,  '%Y-%m-%d %H:%M:%S'))
         except (IOError, etree.XMLSyntaxError, AttributeError):
-            self.mindate = datetime.datetime(2100, 1, 1, 0, 0, 0)
-            self.maxdate = datetime.datetime(2000, 1, 1, 0, 0, 0)
+            self.logparser.set_mindate(datetime.datetime(2100, 1, 1, 0, 0, 0))
+            self.logparser.set_maxdate(datetime.datetime(2000, 1, 1, 0, 0, 0))
+
+    def generate_report(self):
+        self.report_file.append(self.write_header())
+        self.report_file.append(self.write_body())
+        o = open(self.outfile, 'w')
+        o.write(etree.tostring(
+            self.report_file,
+            encoding=u'utf-8',
+            pretty_print=True,
+            xml_declaration=True,
+            doctype='<!DOCTYPE document PUBLIC \
+            "-//APACHE//DTD Documentation V2.0//EN" \
+            "http://forrest.apache.org/dtd/document-v20.dtd">'))
 
     def write_header(self):
         header = etree.Element('header')
@@ -168,7 +248,8 @@ class DivvunApacheLogParser:
         '''Sum up the number of lines found
         '''
         return sum([len(
-            self.found_lists[key]) for key in self.found_lists.keys()])
+            self.logparser.found_lists[key]) for key in
+            self.logparser.found_lists.keys()])
 
     def make_p(self):
         p = etree.Element('p')
@@ -177,12 +258,12 @@ class DivvunApacheLogParser:
 
         em_min = etree.SubElement(p, 'em')
         em_min.set('id', 'mindate')
-        em_min.text = str(self.mindate)
+        em_min.text = str(self.logparser.mindate)
         em_min.tail = u' and '
 
         em_max = etree.SubElement(p, 'em')
         em_max.set('id', 'maxdate')
-        em_max.text = str(self.maxdate)
+        em_max.text = str(self.logparser.maxdate)
 
         return p
 
@@ -193,7 +274,7 @@ class DivvunApacheLogParser:
             ul.append(li)
             li.text = self.our_targets[target] + \
                 ' has been downloaded ' + \
-                str(len(self.found_lists[target])) + ' times'
+                str(len(self.logparser.found_lists[target])) + ' times'
 
         return ul
 
@@ -217,7 +298,7 @@ class DivvunApacheLogParser:
             table.append(self.make_table_row(['Year', 'Count'], 'th'))
 
             year_dict = {}
-            for found_line in self.found_lists[target]:
+            for found_line in self.logparser.found_lists[target]:
                 year = found_line['date'].year
                 if year in year_dict:
                     year_dict[year] += 1
@@ -259,7 +340,7 @@ class DivvunApacheLogParser:
             table.append(self.make_table_row(['Useragent', 'Count'], 'th'))
 
             year_dict = {}
-            for found_line in self.found_lists[target]:
+            for found_line in self.logparser.found_lists[target]:
                 year = found_line['useragent']
                 if year in year_dict:
                     year_dict[year] = year_dict[year] + 1
@@ -283,7 +364,7 @@ class DivvunApacheLogParser:
             table.append(self.make_table_row(['Country', 'Count'], 'th'))
 
             counted_countries = {}
-            for found_line in self.found_lists[target]:
+            for found_line in self.logparser.found_lists[target]:
                 country = locator.country_name_by_addr(
                     found_line['source_ip'])
 
@@ -301,97 +382,31 @@ class DivvunApacheLogParser:
 
         return section
 
-    def is_bot(self, line):
-        """Check if the line contains one of the bots in self.bots
-        Tried to implement this check as a regex, but runtime
-        on a 100 000 line test access log went up to 18 seconds
-        from about 2.5 seconds with this implementation on my
-        test machine.
-        """
-        for bot in self.bots:
-            if bot in line:
-                return True
-
-        return False
-
-    def get_infile(self, filename):
-        if filename[-3:] == '.gz':
-            return gzip.open(filename)
-        else:
-            return open(filename)
-
-    def set_date(self, date):
-        if date < self.mindate:
-            self.mindate = date
-        if date > self.maxdate:
-            self.maxdate = date
-
-    def is_target(self, line):
-        for target in self.our_targets.keys():
-            if target in line:
-                return target
-
-    def gather_lines(self, filename):
-        for line in self.get_infile(filename):
-            target = self.is_target(line)
-            if (target is not None and self.is_bot(line) is False and
-                    ' 200 ' in line):
-                l = {'raw': line,
-                     'body': line}
-                normalizer.normalize(l)
-                self.found_lists[target].append(l)
-                self.set_date(l['date'])
-
-    def find_lines(self):
-        """
-        Go through all the access log files in a given directory. Pick out
-        the lines that has one our download goals, which has been fully fetched
-        and which hasn't been downloaded by a bot.
-        """
-        for access_file in glob.glob(
-                os.path.join(self.log_directory, '*access*')):
-            self.gather_lines(access_file)
-
-    def generate_report(self):
-        self.report_file.append(self.write_header())
-        self.report_file.append(self.write_body())
-        o = open(self.outfile, 'w')
-        o.write(etree.tostring(
-            self.report_file,
-            encoding=u'utf-8',
-            pretty_print=True,
-            xml_declaration=True,
-            doctype='<!DOCTYPE document PUBLIC \
-            "-//APACHE//DTD Documentation V2.0//EN" \
-            "http://forrest.apache.org/dtd/document-v20.dtd">'))
+    def parse_apachelogs(self):
+        self.get_max_and_min_date()
+        self.logparser.parse_apachelogs()
 
     def debug_input(self):
-        with open('debugfile', 'w') as debugfile:
-            for key in self.found_lists.keys():
-                [debugfile.write(line['raw']) for line in self.found_lists[key]]
+        self.logparser.debug_input()
+
+
+def parse_options():
+    parser = argparse.ArgumentParser(
+        description=__doc__)
+    parser.add_argument('log_directory',
+                        help='Directory where the apache log files are found')
+    parser.add_argument('xmlfile',
+                        help='The file where the report is read from and \
+                        written to')
+
+    return parser.parse_args()
 
 
 def main():
-    if len(sys.argv) != 3:
-        print __doc__
-        sys.exit(0)
-    # parse command line options
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "h", ["help"])
-    except getopt.error, msg:
-        print msg
-        print "for help use --help"
-        sys.exit(2)
-    # process options
-    for o, a in opts:
-        if o in ("-h", "--help"):
-            print __doc__
-            sys.exit(0)
+    args = parse_options()
 
-    args = sys.argv[1:]
-
-    divvun_parser = DivvunApacheLogParser(args)
-    divvun_parser.find_lines()
+    divvun_parser = DivvunLogHandler(args.log_directory, args.xmlfile)
+    divvun_parser.parse_apachelogs()
     divvun_parser.generate_report()
     divvun_parser.debug_input()
 
