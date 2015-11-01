fd8307de627cffef92b0e79b699dc462e68abb98
Børre Gaup <albbas@gmail.com>
2015-11-01 23:27:01 +0000

    Script to test show which engines and languages php-enchant provides
    If se_NO exists, check the version of it

---
+++ prooftools/test-enchant.php
@@ -0,0 +1,25 @@
+<?php
+$r = enchant_broker_init();
+$bprovides = enchant_broker_describe($r);
+echo "Current broker provides the following backend(s):\n";
+print_r($bprovides);
+
+$dicts = enchant_broker_list_dicts($r);
+print_r($dicts);
+
+$tag = 'se_NO';
+if (enchant_broker_dict_exists($r,$tag)) {
+    $d = enchant_broker_request_dict($r, $tag);
+    $dprovides = enchant_dict_describe($d);
+    echo "dictionary $tag provides:\n";
+    $wordcorrect = enchant_dict_check($d, "nuvviDspeller");
+    print_r($dprovides);
+    if (!$wordcorrect) {
+        $suggs = enchant_dict_suggest($d, "nuvviDspeller");
+        echo "Suggestions for 'nuvviDspeller':";
+        print_r($suggs);
+    }
+    enchant_broker_free_dict($d);
+}
+enchant_broker_free($r);
+?>
