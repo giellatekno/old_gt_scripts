0d486d8e9807498e34f2e1fbb76df597e83413d1
Børre Gaup <albbas@gmail.com>
2016-04-21 15:31:08 +0000

    /me prefers single quotes

--- static-divvun.py
+++ static-divvun.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""This script builds a multilingual forrest site.
+'''This script builds a multilingual forrest site.
 --destination (-d) an ssh destination
 --sitehome (-s) where sd and techdoc lives
-"""
+'''
 
 from __future__ import absolute_import
 from __future__ import print_function
@@ -20,18 +20,18 @@ import time
 
 
 class StaticSiteBuilder(object):
-    """Class to build a multilingual static version of the divvun site."""
+    '''Class to build a multilingual static version of the divvun site.'''
 
     def __init__(self, builddir, destination, langs):
-        """
+        '''
             builddir: The directory where the forrest site is
             destination: where the built site is copied (using ssh)
             langs: list of langs to be built
 
             Revert files that might be changed
             Clean up the build directory of the forrest site
-        """
-        print("Setting up...")
+        '''
+        print('Setting up...')
         if builddir.endswith('/'):
             builddir = builddir[:-1]
         self.builddir = builddir
@@ -42,43 +42,43 @@ class StaticSiteBuilder(object):
 
         self.logfile = open(
             os.path.join(self.builddir,
-                         "buildlog" + time.strftime("%Y-%m-%d-%H-%M",
+                         'buildlog' + time.strftime('%Y-%m-%d-%H-%M',
                                                     time.localtime())), 'w')
         os.chdir(self.builddir)
-        subp = subprocess.Popen(["forrest", "clean"],
+        subp = subprocess.Popen(['forrest', 'clean'],
                                 stdout=self.logfile, stderr=self.logfile)
         subp.wait()
 
         if subp.returncode != 0:
-            print("forrest clean failed", file=sys.stderr)
+            print('forrest clean failed', file=sys.stderr)
 
-        if os.path.isdir(os.path.join(self.builddir, "built")):
-            shutil.rmtree(os.path.join(self.builddir, "built"))
+        if os.path.isdir(os.path.join(self.builddir, 'built')):
+            shutil.rmtree(os.path.join(self.builddir, 'built'))
 
-        os.mkdir(os.path.join(self.builddir, "built"))
+        os.mkdir(os.path.join(self.builddir, 'built'))
 
     def __del__(self):
-        """Close the logfile"""
+        '''Close the logfile'''
         self.logfile.close()
 
     def validate(self):
         '''Run forrest validate'''
-        print("Validating...")
+        print('Validating...')
         os.chdir(self.builddir)
-        subp = subprocess.Popen(["forrest", "validate"],
+        subp = subprocess.Popen(['forrest', 'validate'],
                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         (output, error) = subp.communicate()
         self.logfile.writelines(output)
         self.logfile.writelines(error)
 
         if subp.returncode != 0:
-            if "Could not validate document" in error:
-                print("\n\nCould not validate doc\n\n", file=sys.stderr)
+            if 'Could not validate document' in error:
+                print('\n\nCould not validate doc\n\n', file=sys.stderr)
                 raise SystemExit(subp.returncode)
 
     def set_forrest_lang(self, lang):
         for line in fileinput.FileInput(
-            os.path.join(self.builddir, "forrest.properties"),
+            os.path.join(self.builddir, 'forrest.properties'),
                 inplace=1):
             if 'forrest.jvmargs' in line:
                 line = (
@@ -90,30 +90,30 @@ class StaticSiteBuilder(object):
             print(line.rstrip())
 
     def buildsite(self, lang):
-        """Builds a site in the specified language
+        '''Builds a site in the specified language
 
         Clean up the build files
         Validate files. If they don't validate, exit program
         Build site. stdout and stderr are stored in output and error,
         respectively.
         If we aren't able to rename the built site, exit program
-        """
+        '''
         # This ensures that the build directory is build/site/en
-        os.environ['LC_ALL'] = "C"
+        os.environ['LC_ALL'] = 'C'
 
         os.chdir(self.builddir)
         self.set_forrest_lang(lang)
-        print("Building", lang, "...")
-        subp = subprocess.Popen(["forrest", "site"],
+        print('Building', lang, '...')
+        subp = subprocess.Popen(['forrest', 'site'],
                                 stdout=self.logfile, stderr=self.logfile)
         subp.wait()
         if subp.returncode != 0:
-            print("Linking errors detected when building", lang, file=sys.stderr)
+            print('Linking errors detected when building', lang, file=sys.stderr)
 
-        print("Done building ")
+        print('Done building ')
 
     def add_language_changer(self, lang):
-        builddir = os.path.join(self.builddir, "build/site/en")
+        builddir = os.path.join(self.builddir, 'build/site/en')
 
         for root, dirs, files in os.walk(builddir):
             for f in files:
@@ -122,21 +122,21 @@ class StaticSiteBuilder(object):
                     f2b.convert(lang, self.langs, builddir)
 
     def rename_site_files(self, lang):
-        """Search for files ending with html and pdf in the build site.
+        '''Search for files ending with html and pdf in the build site.
 
         Give all these files the ending '.lang'.
         Move them to the 'built' dir
-        """
+        '''
 
-        builddir = os.path.join(self.builddir, "build/site/en")
-        builtdir = os.path.join(self.builddir, "built")
+        builddir = os.path.join(self.builddir, 'build/site/en')
+        builtdir = os.path.join(self.builddir, 'built')
 
         if len(self.langs) == 1:
             for item in glob.glob(builddir + '/*'):
                 shutil.move(item, builtdir)
         else:
             for root, dirs, files in os.walk(builddir):
-                goal_dir = root.replace("build/site/en", "built")
+                goal_dir = root.replace('build/site/en', 'built')
 
                 if not os.path.exists(goal_dir):
                     os.mkdir(goal_dir)
@@ -162,10 +162,10 @@ class StaticSiteBuilder(object):
             self.rename_site_files(lang)
 
     def copy_to_site(self):
-        """Copy the entire site to self.destination"""
-        builtdir = os.path.join(self.builddir, "built/")
+        '''Copy the entire site to self.destination'''
+        builtdir = os.path.join(self.builddir, 'built/')
         subp = subprocess.Popen(
-            ["rsync", "-avz", "-e", "ssh", builtdir, self.destination],
+            ['rsync', '-avz', '-e', 'ssh', builtdir, self.destination],
             stdout=self.logfile, stderr=self.logfile)
         subp.wait()
 
@@ -187,15 +187,15 @@ class LanguageAdder(object):
 
     def add_lang_info(self, lang, langs, builddir):
         body = self.getelement('body')
-        my_nav_bar = body.find('.//div[@id="myNavbar"]',
+        my_nav_bar = body.find('.//div[@id='myNavbar']',
                                namespaces=self.namespace)
         my_nav_bar.append(self.make_lang_menu(lang, langs, builddir))
 
     def make_lang_menu(self, this_lang, langs, builddir):
-        trlangs = {u"fi": u"Suomeksi", u"no": u"På norsk",
-                   u"sma": u"Åarjelsaemien", u"se": u"Davvisámegillii",
-                   u"smj": u"Julevsábmáj", u"sv": u"På svenska",
-                   u"en": u"In English"}
+        trlangs = {u'fi': u'Suomeksi', u'no': u'På norsk',
+                   u'sma': u'Åarjelsaemien', u'se': u'Davvisámegillii',
+                   u'smj': u'Julevsábmáj', u'sv': u'På svenska',
+                   u'en': u'In English'}
 
         right_menu = etree.Element('ul')
         right_menu.set('class', 'nav navbar-nav navbar-right')
@@ -208,7 +208,7 @@ class LanguageAdder(object):
         dropdown_toggle.set('class', 'dropdown-toggle')
         dropdown_toggle.set('data-toggle', 'dropdown')
         dropdown_toggle.set('href', '#')
-        dropdown_toggle.text = u"Change language"
+        dropdown_toggle.text = u'Change language'
         dropdown.append(dropdown_toggle)
 
         span = etree.Element('span')
@@ -242,12 +242,12 @@ def parse_options():
     parser = argparse.ArgumentParser(
         description='This script builds a multilingual forrest site.')
     parser.add_argument('--destination', '-d',
-                        help="an ssh destination",
+                        help='an ssh destination',
                         required=True)
     parser.add_argument('--sitehome', '-s',
-                        help="where the forrest site lives",
+                        help='where the forrest site lives',
                         required=True)
-    parser.add_argument('langs', help="list of languages",
+    parser.add_argument('langs', help='list of languages',
                         nargs='+')
 
     args = parser.parse_args()
@@ -263,5 +263,5 @@ def main():
     builder.copy_to_site()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
