2ae7c527b5ae1c7914043ae36ba058195d9ca780
Børre Gaup <albbas@gmail.com>
2016-09-29 15:10:26 +0000

    SC2086: Double quote to prevent globbing and word splitting.

--- corpus/sync_analysed.sh
+++ corpus/sync_analysed.sh
@@ -5,7 +5,7 @@
 #
 
 # Read environment variables that are needed
-source $HOME/.bash_profile
+source "$HOME/.bash_profile"
 
 DATE=$(date +%Y-%m-%d)
 
@@ -20,13 +20,13 @@ do
     for lang in sma sme smj fkv smn sms
     do
         # count how many files are potentially convertible
-        xsls=$(find $CORPUS/orig/$lang -name \*.xsl|wc -l)
+        xsls=$(find "$CORPUS/orig/$lang" -name \*.xsl|wc -l)
 
         # count how many files that really got converted
-        cxmls=$(find $CORPUS/converted/$lang -name \*.xml|wc -l)
+        cxmls=$(find "$CORPUS/converted/$lang" -name \*.xml|wc -l)
 
         # count how many files that really got analysed
-        axmls=$(find $CORPUS/analysed/$lang -name \*.xml|wc -l)
+        axmls=$(find "$CORPUS/analysed/$lang" -name \*.xml|wc -l)
 
         # print the facts
         echo "$lang xsls $xsls cxml $cxmls axml $axmls $CORPUS"
@@ -37,9 +37,9 @@ do
     # Make sure the directory exists in divvun.no
     ssh boerre@divvun.no "if [ -d \"$DIRECTORY\" ]; then echo \"$DIRECTORY exists\"; else mkdir $DIRECTORY;fi"
     # sync the file to freecorpus
-    rsync -qaz $CORPUS/analysed/ boerre@divvun.no:$DIRECTORY
+    rsync -qaz "$CORPUS/analysed/" boerre@divvun.no:"$DIRECTORY"
     # remove analysed files. If the analysis job is not run during the night,
     # this assures we won't get stale data synced to divvun.no
-    rm -rf $CORPUS/analysed
+    rm -rf "$CORPUS/analysed"
 
 done
