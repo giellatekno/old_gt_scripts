7a179cc231792ff8f67f50524f16146360b4326e
Børre Gaup <albbas@gmail.com>
2017-01-19 01:32:51 +0000

    Improve output

--- character_frequency_counter.py
+++ character_frequency_counter.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 # show the frequency of characters in a string
 # Python24 and higher by HAB
@@ -8,6 +8,7 @@
 
 import sys
 
+char_num = 0
 cf_dic = {}
 fname = sys.argv[1]
 if (fname == '-'):
@@ -15,20 +16,18 @@ if (fname == '-'):
 else:
     infile = open(fname)
 for line in infile:
-    line = unicode(line, 'utf8')
     # create a character:frequency dictionary
+    for char in line:
+        if char not in u" \n\t0123456789.¶,-–:«»!()?[]/'’…<>=;\"&+@*":
+            char_num += 1
+            cf_dic[char] = cf_dic.get(char, 0) + 1
 
-    for char in line.lower():
-        cf_dic[char] = cf_dic.get(char, 0) + 1
 
-
-print "Characters sorted by frequency:"
+print('Characters sorted by frequency:')
 # convert cf_dic to list of (k, v) tuples with cf_dic.items()
 # flip tuple elements to (v, k) using list comprehension
 # then sort list of tuples (order is v,k), highest v first
 value_key = sorted([(v, k) for k, v in cf_dic.items()], reverse=True)
-for vk in value_key:
+for frequency, character in value_key:
     # don't show space and newline
-    if vk[1] not in u" \n0123456789.¶,-–:«»!()?[]/'’…":
-        c = unicode(vk[1]).encode('utf8')
-        print c, vk[0]
+    print('{}\t{:>10}\t{:>6.2f}%'.format(character, frequency, frequency/char_num*100))
