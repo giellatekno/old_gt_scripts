43cb1c6a77cb483ba3f374e4e0062adde1b982b4
Børre Gaup <albbas@gmail.com>
2017-03-20 13:04:26 +0000

    Move tmx2html.xsl to CorpusTools, where it belongs

--- corpus/tmx2html.sh
+++ corpus/tmx2html.sh
@@ -1,4 +1,4 @@
 for file in $(find $GTFREE/prestable/tmx -name '*.tmx' -type f)
 do
-    xsltproc $GTHOME/gt/script/corpus/tmx2html.xsl $file > $file.html
+    xsltproc $GTHOME/tools/CorpusTools/corpustools/xslt/tmx2html.xsl $file > $file.html
 done
