c0d2fd8f4e173d949e7ec4556b2489d8a4333143
Trond Trosterud <trond.trosterud@uit.no>
2017-08-17 06:36:06 +0000

    Also hfst, and goodbye to old infra compilation commands.

--- makeall.sh
+++ makeall.sh
@@ -10,171 +10,100 @@ echo "===>     All the newinfra languages      <==="
 pushd $GTHOME/langs
 
 echo " -------------------------------------------------------------------------------------- bxr, Buriad "
-cd   bxr && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd   bxr && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- ciw, Ojibwe "
-cd ../ciw && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../ciw && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- cor, Cornish "
-cd ../cor && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../cor && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- crk, Plains Cree "
-cd ../crk && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../crk && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- est, Estonian "
-cd ../est && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../est && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- fao, Faroese "
-cd ../fao && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../fao && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- fin, Finnish "
-cd ../fin && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../fin && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- hdn, Northern Haida "
-cd ../hdn && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../hdn && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- cor, Cornish "
-cd ../cor && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../cor && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- ipk, Iñupiaq "
-cd ../ipk && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../ipk && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- izh, Izhorian "
-cd ../izh && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../izh && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- kal, Greenlandic "
 echo " "
 echo " No, sorry, we skip Greenlandic. It takes to long time to compile."
 echo " In order to compile Greenlandic, do this:"
 echo " cd $GTHOME/langs/kal/"
-echo " ./autogen.sh -l && ./configure && make "
+echo " ./autogen.sh -l && ./configure --with-hfst && make "
 echo " "
-#cd ../kal && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../kal && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- kca, Khanty "
-cd ../kca && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../kca && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- kpv, Komi Zyryan "
-cd ../kpv && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../kpv && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- liv, Livonian "
-cd ../liv && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../liv && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- lut, Lushootseed "
-cd ../lut && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../lut && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- mdf, Moksha "
-cd ../mdf && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../mdf && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- mhr, Meadow Mari "
 cd ../mhr && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst --without-xfst && nice time make 
 echo " -------------------------------------------------------------------------------------- mns, Northern Mansi "
-cd ../mns && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../mns && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- mrj, Hill Mari "
-cd ../mrj && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../mrj && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- myv, Erzya "
-cd ../myv && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../myv && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- ndl, Ndolo "
-cd ../ndl && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../ndl && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- nio, Nganasan "
-cd ../nio && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../nio && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- nob, Norwegian Bokmål "
-cd ../nob && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../nob && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- olo, Olonetsian "
-cd ../olo && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../olo && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- ron, Rumanian "
-cd ../ron && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../ron && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- rup, Arumanian "
-cd ../rup && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../rup && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- rus, Russian "
-cd ../rus && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../rus && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- sjd, Kildin Saami "
-cd ../sjd && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../sjd && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- sje, Pite Saami "
-cd ../sje && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../sje && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- sma, South Saami "
 cd ../sma && pwd && nice ./autogen.sh -l && nice ./configure --enable-oahpa && nice time make   # adjectives.lexc
 #cd ../sma && pwd && nice ./autogen.sh -l && nice ./configure --enable-oahpa && nice time make  # adjectives-oahpa.lexc
 echo " -------------------------------------------------------------------------------------- smj, Lule Saami "
-cd ../smj && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../smj && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- smn, Inari Saami "
-cd ../smn && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../smn && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- sms, Skolt Saami "
-cd ../sms && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../sms && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- som, Somali "
-cd ../som && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../som && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- tat, Tatar "
-cd ../tat && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../tat && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- tku, Upper Nekaxa Totonac "
-cd ../tku && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../tku && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- udm, Udmurt "
-cd ../udm && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../udm && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- vep, Veps "
-cd ../vep && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../vep && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- vro, Võro "
-cd ../vro && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../vro && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- yrk, Tundra Nenets "
-cd ../yrk && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+cd ../yrk && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 echo " -------------------------------------------------------------------------------------- zul, Zulu "
-cd ../zul && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
-
-
-# File to run all our compilation
-# processes in xerox.
-echo ""
-echo " This script will compile all our analysers and lexica with xerox"
-echo ""
-pushd $GTHOME/gt
-echo "===>                    <===" 
-echo "===>   Sámi languages   <===" 
-echo "===>                    <===" 
-echo ""
-echo "===>     North Sámi      <==="
-nice time make GTLANG=sme all
-echo ""
-echo "===>                    <===" 
-echo "===>   Other languages  <===" 
-echo "===>                    <===" 
-echo ""
-echo "===>    Nynorsk       <===" 
-cd $GTHOME/st/nno/src
-nice time make
-echo ""
-echo "===>    Old Norse     <===" 
-cd $GTHOME/st/non/src
-nice time make
-echo ""
-echo "===>    Czech     <===" 
-cd $GTHOME/st/ces/src
-nice time make
-echo ""
-echo "===>    German    <===" 
-cd $GTHOME/st/deu/src
-nice time make
-echo ""
-echo "===>    Guaraní    <===" 
-cd $GTHOME/st/grn/src
-nice time make
-echo ""
-echo "===>    Amharic   <===" 
-cd $GTHOME/st/amh/src
-nice time make
-echo ""
-echo "===>    English      <===" 
-cd $GTHOME/st/eng/src
-nice time make
-echo ""
-echo "===>    Cornish   <===" 
-cd $GTHOME/st/cor/src
-nice time make
-echo ""
-echo "===>     Iñupiaq     <===" 
-cd $GTHOME/st/ipk/src
-nice time make
-echo ""
-echo "===>     Inuktitut     <===" 
-cd $GTHOME/st/iku/src
-nice time make
-echo ""
-echo "===>    Kurdish   <===" 
-cd $GTHOME/st/kmr/src
-nice time make
+cd ../zul && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 
 
 
 
-
-
-
-echo "===>                    <===" 
-echo "===>  Bilingual transd. <===" 
-echo "===>                    <===" 
-echo "===>   North Sámi - Lule Sámi  <===" 
-cd $GTHOME/words/dicts/smesmj/src
-nice time make
 
 echo ""
 echo "===>     Dictionaries   <===" 
@@ -222,9 +151,9 @@ nice time make
 
 
 # echo " -------------------------------------------------------------------------------------- tlh, Klingon "
-# cd ../tlh && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+# cd ../tlh && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 # echo " -------------------------------------------------------------------------------------- tuv, Turkana "
-# cd ../tuv && pwd && nice ./autogen.sh -l && nice ./configure && nice time make 
+# cd ../tuv && pwd && nice ./autogen.sh -l && nice ./configure --with-hfst && nice time make 
 
 
 popd
